059f07e9c6a17cd5f97a2b0911ff4b82ad29786b
Anton Arbus <anton.arbus@macbookpro>
2022-07-25 23:41:52 +0300

    env file and package

--- server/api/login/login.ts
+++ server/api/login/login.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 
 const router = express.Router()
 
-mongoose.connect('mongodb://xxx')
+// mongoose.connect('mongodb://xxx')
+console.log('process.env.MONGO_DB_USER_NAME', process.env.MONGO_DB_USER_NAME)
 
 router.post('/', (req: ReqType, res: ResType) => {
   console.log(req.body)
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,8 @@
+import 'dotenv/config'
 import express, { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
 import login from './api/login'
+
+console.log(process.env.MONGO_DB_USER_NAME)
 
 const app = express()
 app.use(express.json()) // parses incoming requests with JSON because we use lots of json, let it be default
