e7652d402f5521fcd1c49f248b2250a3ecbd4e62
Anton Arbus <anton.arbus@macbookpro>
2022-08-14 22:24:20 +0300

    small update

--- server/api/loginRouter.ts
+++ server/api/loginRouter.ts
@@ -1,13 +1,9 @@
 import express, { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
-import mongoose from 'mongoose'
+import { connectToDb } from '../db/connectToDb'
 import { UserModel } from '../db/models/user.model'
+connectToDb()
 
 export const loginRouter = express.Router()
-
-const mongo = process.env.MONGO_DB_CONNECTION_STRING
-const db = 'q'
-mongoose.connect(`${mongo}/${db}` as string)
-
 loginRouter.post('/', async (req: ReqType, res: ResType) => {
   console.log(req.body)
   const user = await UserModel.findOne({
--- server/api/registerRouter.ts
+++ server/api/registerRouter.ts
@@ -1,15 +1,9 @@
 import express, { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
-// import mongoose from 'mongoose'
 import { connectToDb } from '../db/connectToDb'
 import { UserModel } from '../db/models/user.model'
-
-export const registerRouter = express.Router()
-
-// const mongo = process.env.MONGO_DB_CONNECTION_STRING
-// const db = 'q'
-// mongoose.connect(`${mongo}/${db}` as string)
 connectToDb()
 
+export const registerRouter = express.Router()
 registerRouter.post('/', async (req: ReqType, res: ResType) => {
   console.log('req.body', req.body)
   try {
