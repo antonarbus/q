f7dc8df9f483780eb4afa827f49a9945ab1b18f5
Anton Arbus <anton.arbus@macbookpro>
2022-08-17 00:30:52 +0300

    small update

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config'
 import express, { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
+import morgan from 'morgan'
 import { loginRouter } from './api/loginRouter'
 import { registerRouter } from './api/registerRouter'
 
-console.log(process.env.MONGO_DB_USER_NAME)
-
 const app = express()
+app.use(morgan('dev')) // http logs in terminal
 app.use(express.json()) // parses incoming requests with JSON because we use lots of json, let it be default
 app.get('/', (req: ReqType, res:ResType) => {
   res.send('This is from express.js')
