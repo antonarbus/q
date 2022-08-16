4a5bbb7596da9737b0fb33dcf7dc21ce0a5be3d6
Anton Arbus <anton.arbus@macbookpro>
2022-08-17 00:30:52 +0300

    small update

--- server/api/loginRouter.ts
+++ server/api/loginRouter.ts
@@ -1,16 +1,26 @@
 import express, { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
 import { connectToDb } from '../db/connectToDb'
 import { UserModel } from '../db/models/user.model'
+import { sign } from 'jsonwebtoken'
+// const jwt = require('jsonwebtoken')
 connectToDb()
 
 export const loginRouter = express.Router()
 loginRouter.post('/', async (req: ReqType, res: ResType) => {
   console.log(req.body)
-  const user = await UserModel.findOne({
-    email: req.body.email,
-    password: req.body.password
-  })
+  const { email, password } = req.body
 
-  if (user) return res.json({ status: 'ok', user: true })
-  return res.json({ status: 'error', user: false })
+  try {
+    const user = await UserModel.findOne({ email, password })
+    if (user) {
+      const { email } = user
+      const jwtToken = sign(email, process.env.JWT_SALT as string)
+      res.json({ status: 'user logged in', user, jwtToken })
+    } else {
+      res.json({ status: 'error during logging in' })
+    }
+  } catch (error: any) {
+    const { message, number, trace, name } = error
+    res.json({ status: 'error', message, number, trace, name })
+  }
 })
--- server/db/models/user.model.ts
+++ server/db/models/user.model.ts
@@ -2,8 +2,15 @@ import mongoose from 'mongoose'
 
 const User = new mongoose.Schema(
   {
-    email: { type: String, required: true },
-    password: { type: String, required: true, unique: true }
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'email is required']
+    }
   }
 )
 
