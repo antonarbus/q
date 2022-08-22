cb9e321838cc7cab80759abb2b1386b6646cd319
Anton Arbus <anton.arbus@macbookpro>
2022-08-22 17:42:24 +0300

    Access-JWT-Token lowered

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -5,6 +5,6 @@ export const baseURL = 'http://localhost:3009'
 export const axiosWithAuth = axios.create({ withCredentials: true, baseURL })
 
 axiosWithAuth.interceptors.request.use((config) => {
-  config.headers['Access-JWT-Token'] = localStorage.getItem('accessJwtToken') || ''
+  config.headers['['access-jwt-token']'] = localStorage.getItem('accessJwtToken') || ''
   return config
 })
