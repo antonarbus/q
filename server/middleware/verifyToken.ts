d53c14cce0783c9e7af7169e1f27cd24f2895ee3
Anton Arbus <anton.arbus@macbookpro>
2022-08-22 17:44:47 +0300

    xxx

--- server/middleware/verifyToken.ts
+++ server/middleware/verifyToken.ts
@@ -3,10 +3,10 @@ import jwt from 'jsonwebtoken'
 import type { JwtPayload } from 'jsonwebtoken'
 
 export function verifyToken(req: any, res: any, next: NextType) {
-  const accessJwtToken = req.headers['['access-jwt-token']'] as string
-  console.log('accessJwtToken', accessJwtToken)
-  console.log('req', req)
-  console.log('req.headers', req.headers)
+  const accessJwtToken = req.headers['access-jwt-token'] as string
+  // console.log('accessJwtToken', accessJwtToken)
+  // console.log('req', req)
+  // console.log('req.headers', req.headers)
   if (!accessJwtToken) return res.status(401).send('Not authorized')
   try {
     const { email } = jwt.verify(accessJwtToken, process.env.JWT_ACCESS_SECRET as string) as JwtPayload
