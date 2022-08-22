7beda8358692a15dcb96ed7c36b68812c9d4d935
Anton Arbus <anton.arbus@macbookpro>
2022-08-22 18:20:01 +0300

    almost works

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -8,3 +8,26 @@ axiosWithAuth.interceptors.request.use((config) => {
   config.headers['access-jwt-token'] = localStorage.getItem('accessJwtToken') || ''
   return config
 })
+
+axiosWithAuth.interceptors.response.use(
+  (config) => {
+    return config
+  },
+  async (error) => {
+    const originalRequest = error.config
+    console.log(666)
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+      try {
+        originalRequest._isRetry = true
+        const response = await axios.get(`${baseURL}/api/refresh`, { withCredentials: true })
+        if (!response.data.accessJwtToken) return
+        const accessJwtToken = response.data.accessJwtToken
+        localStorage.setItem('accessJwtToken', accessJwtToken)
+        return axiosWithAuth.request(originalRequest)
+      } catch (error) {
+        console.log('not authorized')
+      }
+    }
+    throw error
+  }
+)
