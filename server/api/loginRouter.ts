967523209b65ab4ebbbb983dedf4eca72a335c5c
Anton Arbus <anton.arbus@macbookpro>
2022-08-22 12:00:13 +0300

    small update

--- server/api/loginRouter.ts
+++ server/api/loginRouter.ts
@@ -4,29 +4,31 @@ import { UserModel } from '../db/models/user.model'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 
-const refreshTokenExpirationDays = 30
-
 export const loginRouter = express.Router()
 loginRouter.post('/', async (req: ReqType, res: ResType, next: NextType) => {
-  console.log(req.body)
-  let { email, password } = req.body
   try {
-    // await connectToDb()
+    // get mail & password from body
+    let { email, password } = req.body
     email = email.toLowerCase()
+
+    // check email & password
     const user = await UserModel.findOne({ email })
-    if (!user) return res.json({ status: 'error', message: 'invalid email' })
+    const isPasswordValid = user && await bcrypt.compare(password, user.password)
+    if (!user || !isPasswordValid) return res.json({ status: 'error', message: 'invalid credentials' })
+
+    // check if account is activated
     if (!user.isActivated) return res.json({ status: 'error', message: 'account is not activated' })
-    const isPasswordValid = await bcrypt.compare(password, user.password)
-    if (!isPasswordValid) return res.json({ status: 'error', message: 'invalid password' })
-    // send token
+
+    // get and send jwt tokens
+    const refreshJwtTokenExpirationDays = 30
     const accessJwtToken = jwt.sign({ email }, process.env.JWT_ACCESS_SECRET as string, { expiresIn: '8h' })
-    const refreshJwtToken = jwt.sign({ email }, process.env.JWT_REFRESH_SECRET as string, { expiresIn: `${refreshTokenExpirationDays}d` })
-    // add refresh token into cookie
-    res.cookie('refreshToken', refreshJwtToken, { maxAge: refreshTokenExpirationDays * 24 * 60 * 60 * 1000, httpOnly: true })
+    const refreshJwtToken = jwt.sign({ email }, process.env.JWT_REFRESH_SECRET as string, { expiresIn: `${refreshJwtTokenExpirationDays}d` })
+    res.cookie('refreshJwtToken', refreshJwtToken, { maxAge: refreshJwtTokenExpirationDays * 24 * 60 * 60 * 1000, httpOnly: true })
     res.json({ status: 'ok', message: `user with email: ${email} logged in`, accessJwtToken })
-    // update logging date and refresh token
+
+    // save login date and refresh token in db
     const filter = { email }
-    const update = { loggedAt: new Date(), refreshToken: refreshJwtToken }
+    const update = { loggedAt: new Date(), refreshJwtToken }
     await UserModel.findOneAndUpdate(filter, update)
   } catch (error: any) {
     next(error)
