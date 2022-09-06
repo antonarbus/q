99a9f8fab25ba6d6c48ebfcaf8e7bdd51904ab8d
Anton Arbus <anton.arbus@macbookpro>
2022-09-07 02:58:13 +0300

    httpStatus icons in buttons

--- src/components/Credentials/Reset.tsx
+++ src/components/Credentials/Reset.tsx
@@ -1,40 +1,51 @@
 import { EventType } from '@src/types'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button, TextField } from '@mui/material'
 import { Backdrop } from '@components/Common/Backdrop'
 import { Card } from '@components/Common/Card'
 import { useSlideElement } from '@functions/useSlideElement'
+import { useEffectOnce } from 'react-use'
+import { ButtonWithSuccess } from '@components/Common/ButtonWithSuccess'
 
 export function Reset() {
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const cardRef = useRef() as React.MutableRefObject<HTMLDivElement>
   const inputRef = useRef() as React.MutableRefObject<HTMLDivElement>
+  type httpStatusType = 'loading' | 'error' | 'success' | ''
+  const [httpStatus, setHttpStatus] = useState<httpStatusType>('')
 
   async function mailResetPasswordLink(e: EventType) {
     e.preventDefault()
-    alert('mail reset password link')
-    const method = 'POST'
-    const headers = { 'Content-Type': 'application/json' }
-    const body = JSON.stringify({ email })
-    const options = { method, headers, body }
-    // todo: for error messages make a separate route with a sliding card
-    const res = await fetch('/api/reset', options)
-    const data = await res.json()
-    console.log(data)
-    if (data.status === 'error') {
-      alert(data.message)
-      return localStorage.removeItem('accessJwtToken')
+    setHttpStatus('loading')
+    console.log('mail reset password link')
+    try {
+      const method = 'POST'
+      const headers = { 'Content-Type': 'application/json' }
+      const body = JSON.stringify({ email })
+      const options = { method, headers, body }
+      const res = await fetch('/api/reset', options)
+      const data = await res.json()
+      console.log(data)
+      if (data.status === 'error') {
+        alert(data.message)
+        setHttpStatus('error')
+        return localStorage.removeItem('accessJwtToken')
+      }
+      alert('check your mail box')
+      setHttpStatus('success')
+    } catch (err) {
+      console.log(err)
+      setHttpStatus('error')
     }
-    alert('check your mail box')
   }
 
-  useEffect(() => useSlideElement({ intoView: true, element: cardRef.current, cb: () => inputRef.current.focus() }), [])
+  useEffectOnce(() => useSlideElement({ intoView: true, element: cardRef.current, cb: () => inputRef.current.focus() }))
 
   return (
     <Backdrop
-      onMouseDown={() => useSlideElement({ intoView: false, element: cardRef.current, cb: () => navigate('/') })}
+      onMouseDown={() => useSlideElement({ element: cardRef.current, cb: () => navigate('/') })}
     >
       <Card
         reference={cardRef}
@@ -57,8 +68,13 @@ export function Reset() {
             variant='contained'
             fullWidth
             sx={{ mt: 3, mb: 2, alignSelf: 'center', padding: '10px' }}
-            // todo: put rotating icon inside button while waiting for the api call response
             children='Reset'
+          />
+          <ButtonWithSuccess
+            type='submit'
+            content='Reset'
+            httpStatus={httpStatus}
+            setHttpStatus={setHttpStatus}
           />
         </form>
       </Card>
