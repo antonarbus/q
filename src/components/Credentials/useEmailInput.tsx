a6eff123451cfda76420f9a80ff00ef0a9715393
Anton Arbus <anton.arbus@macbookpro>
2022-09-10 17:47:08 +0300

    EmailInput

---
+++ src/components/Credentials/useEmailInput.tsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { useUpdateEffect } from 'react-use'
+import mailcheck from 'mailcheck'
+import { InputAdornment, TextField } from '@mui/material'
+import { Person } from '@mui/icons-material'
+import { theme } from '@src/theme'
+import { EventType } from '@src/types'
+
+const isEmailPatternOk = (email: string) => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+
+type Props = {
+  email: string
+  setEmail: (value: string) => void
+}
+
+export function EmailInput({ email, setEmail }: Props) {
+  // input value
+  const handleEmailChange = (e: EventType) => {
+    const target = (e.target as HTMLInputElement)
+    setEmail(target.value)
+  }
+
+  // input focused out ones (show validation msg only after first focus out)
+  const [inputFocusedOutOnes, setInputFocusedOutOnes] = useState(false)
+
+  // is email pattern ok
+  const [isEmailOk, setIsEmailOk] = useState(false)
+  useUpdateEffect(function checkIfEmailPatternIsOk() {
+    isEmailPatternOk(email) ? setIsEmailOk(true) : setIsEmailOk(false)
+  }, [email])
+
+  // label msg
+  const initEmailLabel = 'Email'
+  const [emailLabel, setEmailLabel] = useState(initEmailLabel)
+  useUpdateEffect(function setLabelMsgBasedOnValidation() {
+    (inputFocusedOutOnes && email !== '' && !isEmailOk) ? setEmailLabel('Check email pattern') : setEmailLabel(initEmailLabel)
+  }, [email, inputFocusedOutOnes, isEmailOk])
+
+  // email suggestion
+  const [emailSuggestion, setEmailSuggestion] = useState('')
+
+  function suggestEmail(emailVal?: string) {
+    mailcheck.run({
+      email: emailVal || email,
+      suggested: (suggestion: any) => setEmailSuggestion(suggestion.full),
+      empty: () => setEmailSuggestion('')
+    })
+  }
+
+  return (
+    <div css={{ position: 'relative' }}>
+      <TextField
+        fullWidth
+        id="email"
+        label={emailLabel}
+        name="email"
+        autoComplete="email"
+        placeholder='Email'
+        value={email}
+        onChange={handleEmailChange}
+        onBlur={() => {
+          setInputFocusedOutOnes(true)
+          suggestEmail()
+        }}
+        autoFocus
+        InputProps={{
+          startAdornment: <InputAdornment position="start"><Person /></InputAdornment>
+        }}
+        sx={{
+          '& .MuiInputLabel-shrink': {
+            color: (emailLabel !== initEmailLabel) ? theme.colors.red : ''
+          },
+          mb: 2
+        }}
+      />
+      {emailSuggestion && (
+        <div css={{ position: 'absolute', bottom: '18px', right: '5px', fontSize: '12px', color: theme.colors.red }} >
+          Did you mean? {' '}
+          <a
+            style={{ textDecoration: 'underline' }}
+            onClick={(e) => {
+              e.preventDefault()
+              setEmail(emailSuggestion)
+              suggestEmail(emailSuggestion)
+            }}
+          >
+            {emailSuggestion}
+          </a>
+        </div>
+      )}
+    </div>
+  )
+}
