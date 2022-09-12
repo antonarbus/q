ed640634b28ba20d366ce83d7a9239b8f3277a5f
Anton Arbus <anton.arbus@macbookpro>
2022-09-13 00:54:43 +0300

    maybe fixed logout

--- server/api/logoutRouter.ts
+++ server/api/logoutRouter.ts
@@ -7,25 +7,35 @@ import jwt_decode from 'jwt-decode'
 export const logoutRouter = express.Router()
 logoutRouter.get('/', async (req: ReqType, res: ResType, next: NextType) => {
   try {
+    console.log(1)
     // check refresh token
     const { refreshJwtToken } = req.cookies
-    if (!refreshJwtToken) res.json({ status: 'error', message: 'no refresh token in cookies, probably already logged out' })
+    if (!refreshJwtToken) res.json({ status: 'error', message: 'no refresh token in cookies' })
+    console.log(2)
 
     // get email from refresh token
     const { email }: { email: string } = jwt_decode(refreshJwtToken)
-    if (!email) res.json({ status: 'error', message: 'can not retrieve email from refresh token' })
+    if (!email) res.json({ status: 'error', message: 'no email in refresh token' })
+    console.log('email from token', email)
 
     // delete refreshJwtToken from cookie
     res.clearCookie('refreshJwtToken')
+    console.log(4)
 
     // delete token from db
     const user = await UserModel.findOne({ refreshJwtToken })
-    if (!user) return res.json({ status: 'error', message: 'no user find with such refresh token' })
+    console.log('user found by refresh token')
+    console.log(user)
+    if (!user) return res.json({ status: 'error', message: 'no user with such refresh token' })
     user.refreshJwtToken = undefined
     await user.save()
+    console.log('refresh token is deleted')
+    console.log(user)
 
     // send response
-    res.json({ status: 'ok', message: `user with email: ${email} logged out` })
+    res.json({ status: 'ok', message: `user with email: ${email} logged out`, email })
+    console.log('sending response, should contain email')
+    console.log(email)
   } catch (error) {
     next(error)
   }
