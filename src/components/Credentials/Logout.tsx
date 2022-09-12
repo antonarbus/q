ac88d88fbad4757382f75666c411153b3aa1ad50
Anton Arbus <anton.arbus@macbookpro>
2022-09-13 00:54:43 +0300

    maybe fixed logout

--- src/components/Credentials/Logout.tsx
+++ src/components/Credentials/Logout.tsx
@@ -8,6 +8,7 @@ import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
 import { ButtonCustom } from '@components/Common/ButtonCustom'
 import { httpStatusType } from '@src/types'
 import { slideElement } from '@functions/slideElement'
+import { notify } from '@components/Notifier/notify'
 
 // todo: store user data in redux
 // todo: complete Logout component
@@ -18,13 +19,29 @@ export function Logout() {
   const navigate = useNavigate()
   const [httpStatus, setHttpStatus] = useState<httpStatusType>('')
 
-  function logout() {
-    setHttpStatus('loading')
-    localStorage.removeItem('accessJwtToken')
+  async function logout() {
+    const method = 'GET'
+    const options = { method }
+    try {
+      setHttpStatus('loading')
+      const res = await fetch('/api/logout', options)
+      const data = await res.json()
+      const { status, message, email } = data
+      status === 'error' && setHttpStatus('error')
+      status === 'error' && message === 'no refresh token in cookies' && notify({ msg: 'Already logged out before', type: 'info', theme: 'light' })
+      status === 'error' && message === 'no email in refresh token' && notify({ msg: 'No email in refresh token, smth is wrong', type: 'info', theme: 'light' })
+      status === 'error' && message === 'no user with such refresh token' && notify({ msg: 'No user with such refresh token', type: 'info', theme: 'light' })
+      status === 'ok' && setHttpStatus('success')
+      status === 'ok' && notify({ msg: `User with ${email} is logged out`, type: 'success', theme: 'light' })
+    } catch (err) {
+      console.log(err)
+      setHttpStatus('error')
+      notify({ msg: 'Internal error', type: 'error', theme: 'light' })
+    } finally {
+      localStorage.removeItem('accessJwtToken')
+      setTimeout(() => slideElement({ element: cardRef.current, cb: () => navigate('/') }), 3000)
+    }
     // todo: logout from redux
-    // todo: logout does not work, token comes back
-    setHttpStatus('success')
-    setTimeout(() => slideElement({ element: cardRef.current, cb: () => navigate('/') }), 3000)
   }
 
   return (
