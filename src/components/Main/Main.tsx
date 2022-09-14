13ba99bea947e05feac9b512761efb0670394cdb
Anton Arbus <anton.arbus@macbookpro>
2022-09-14 11:36:51 +0300

    roll back

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,33 +1,11 @@
-import { notify } from '@components/Notifier/notify'
 import { Outlet } from 'react-router-dom'
 // eslint-disable-next-line camelcase
 import jwt_decode from 'jwt-decode'
 import axios from 'axios'
-import { axiosWithAuth } from '@src/axios/axios'
 import { useEffectOnce } from 'react-use'
 import { store } from '@redux/store'
 import { login } from '@redux/slices/userSlice'
-
-async function getEmailFromDb() {
-  try {
-    // if we just go in browser to http://localhost:3009/api/user or fetch it with just fetch or axios general instance
-    // we get msg "accessJwtToken is not verified, user is not authorized"
-    // but with axiosWithAuth we add access token to the header and check it in the middleware 'verifyToken '
-    const res = await axiosWithAuth('/api/user')
-    console.log(res)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function getUsersFromDb() {
-  try {
-    const res = await axiosWithAuth('/api/users')
-    console.log(res)
-  } catch (error) {
-    console.log(error)
-  }
-}
+import { Dummy } from './Dummy'
 
 export function Main() {
   useEffectOnce(() => {
@@ -50,7 +28,7 @@ export function Main() {
         localStorage.setItem('accessJwtToken', accessJwtToken)
         console.log(response)
         store.dispatch(login({ email, isLogged: true, role: 'viewer' }))
-        console.log(`tokens for user with email: ${email} are refreshed`)
+        console.log(`tokens for ${email} are refreshed`)
       } catch (error) {
         console.log(error)
       }
@@ -61,10 +39,7 @@ export function Main() {
   return (
     <main css={{ margin: '10px' }}>
       <Outlet />
-      <h3>Main component</h3>
-      <button onClick={() => notify({ msg: 'hi' })}>say hi in bottom popup</button>
-      <button onClick={getEmailFromDb}>get user's email from db</button>
-      <button onClick={getUsersFromDb}>get users from db</button>
+      <Dummy />
     </main>
   )
 }
---
+++ src/components/Test.tsx
@@ -0,0 +1,33 @@
+import { notify } from '@components/Notifier/notify'
+// eslint-disable-next-line camelcase
+import { axiosWithAuth } from '@src/axios/axios'
+
+async function getEmailFromDb() {
+  try {
+    // if we just go in browser to http://localhost:3009/api/user or fetch it with just fetch or axios general instance
+    // we get msg "accessJwtToken is not verified, user is not authorized"
+    // but with axiosWithAuth we add access token to the header and check it in the middleware 'verifyToken '
+    const res = await axiosWithAuth('/api/user')
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getUsersFromDb() {
+  try {
+    const res = await axiosWithAuth('/api/users')
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const Test = () => (
+  <>
+    <h3>Test</h3>
+    <button onClick={() => notify({ msg: 'hi' })}>say hi in bottom popup</button>
+    <button onClick={getEmailFromDb}>get user's email from db</button>
+    <button onClick={getUsersFromDb}>get users from db</button>
+  </>
+)
