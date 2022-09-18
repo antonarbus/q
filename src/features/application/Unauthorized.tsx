a2df03b70fa8551bcd6bd23bbde7f6b8439c3e88
Anton Arbus <anton.arbus@macbookpro>
2022-09-19 02:13:41 +0300

    routes inside Main

---
+++ src/features/application/Unauthorized.tsx
@@ -0,0 +1,13 @@
+import { theme } from '@src/theme'
+
+const css = {
+  height: '100vh',
+  marginTop: `-${theme.nav.fullHeight}px`,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '20vw',
+  color: 'grey'
+}
+
+export const Unauthorized = () => <div css={css}>Unauthorized</div>
