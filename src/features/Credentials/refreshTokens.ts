c2df255e78230685ce5c84d706de3867c301272d
Anton Arbus <anton.arbus@macbookpro>
2022-09-19 16:11:37 +0300

    window.atob

--- src/features/Credentials/refreshTokens.ts
+++ src/features/Credentials/refreshTokens.ts
@@ -9,14 +9,15 @@ export async function refreshTokens() {
     const existingAccessJwtToken = localStorage.getItem('accessJwtToken')
     if (!existingAccessJwtToken) return console.log('user is not logged in')
 
-    const exp = (JSON.parse(atob(existingAccessJwtToken.split('.')[1]))).exp // in seconds since 01 January 1970 GMT
+    // check when access token expires
+    const exp = (JSON.parse(window.atob(existingAccessJwtToken.split('.')[1]))).exp // in seconds since 01 January 1970 GMT
     const d = new Date('1970-01-01T00:00:00Z') // Thu Jan 01 1970 02:00:00 GMT+0200 (Eastern European Standard Time)
     const expiration = d.setUTCSeconds(exp) // 1663584953000 //the value of 'exp', note use UTC not setSeconds().
     const now = new Date().valueOf()
     const difference = expiration - now
     const oneMin = 1 * 60 * 1000
-    const expirationInMin = Math.abs(difference / oneMin)
-    if (expirationInMin > 5) return console.log('access token expires in more than 5 min, skip the refresh for now')
+    const expirationInMin = difference / oneMin
+    if (expirationInMin > 5) return console.log(`access token expires in ${expirationInMin} min, which is more than 5 min, so let's skip the refresh for now`)
 
     const response = await axios.get('/api/refresh', { withCredentials: true })
     const status = response.data.status
