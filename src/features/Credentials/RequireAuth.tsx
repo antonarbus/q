2ff990b679f5c8c4d898f517e2a6b8b056d75cc6
Anton Arbus <anton.arbus@macbookpro>
2022-09-26 12:54:41 +0300

    roles improved

--- src/features/Credentials/RequireAuth.tsx
+++ src/features/Credentials/RequireAuth.tsx
@@ -9,8 +9,9 @@ export function RequireAuth({ allowedRoles }: Props) {
   const location = useLocation()
   const isLogged = useSelector(state => state.credentials.isLogged)
   const roles = useSelector(state => state.credentials.roles)
-  console.log(777)
-  if (isLogged && allowedRoles.some(role => roles.includes(role))) return <Outlet />
-  if (isLogged && !allowedRoles.some(role => roles.includes(role))) return <Navigate to='/unauthorized' state={{ from: location }} replace />
+  const haveRequiredRole = allowedRoles.some(role => roles.includes(role))
+
+  if (isLogged && haveRequiredRole) return <Outlet />
+  if (isLogged && !haveRequiredRole) return <Navigate to='/unauthorized' state={{ from: location }} replace />
   return <Navigate to='/login' state={{ from: location }} replace />
 }
