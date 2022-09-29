1aadc32cecff9fa1382a5b940b14886948102b68
Anton Arbus <anton.arbus@macbookpro>
2022-09-29 14:14:16 +0300

    smth

--- src/globalObject.ts
+++ src/globalObject.ts
@@ -1,10 +1,8 @@
 type Props = {
   goUpInMenu: { () : void } | null
-  goDownInMenu: { (id: string): void } | null,
-  accessJwtToken: string
+  goDownInMenu: { (id: string): void } | null
 }
 export const globalObject: Props = {
   goUpInMenu: () => console.log('put function here for going up the menu, otherwise need to pass it in many props'),
-  goDownInMenu: (id) => console.log('same, but for going down the menu'),
-  accessJwtToken: '' // keep in memory, instead of localStorage
+  goDownInMenu: (id) => console.log('same, but for going down the menu')
 }
