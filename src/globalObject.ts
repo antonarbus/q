3634b89bce746dfbc89a7c04a6806856d099e33f
Anton Arbus <anton.arbus@macbookpro>
2022-10-01 11:07:02 +0300

    Persistent auth without local storage

--- src/globalObject.ts
+++ src/globalObject.ts
@@ -1,8 +1,10 @@
 type Props = {
   goUpInMenu: { () : void } | null
-  goDownInMenu: { (id: string): void } | null
+  goDownInMenu: { (id: string): void } | null,
+  accessJwtToken: string | null
 }
 export const globalObject: Props = {
   goUpInMenu: () => console.log('put function here for going up the menu, otherwise need to pass it in many props'),
-  goDownInMenu: (id) => console.log('same, but for going down the menu')
+  goDownInMenu: (id) => console.log('same, but for going down the menu'),
+  accessJwtToken: '' // keep in memory, instead of localStorage
 }
