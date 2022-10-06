d3740ea447dde1818cd22566e5c8b4d433b6e1e3
Anton Arbus <anton.arbus@macbookpro>
2022-10-06 23:01:08 +0300

    disable store and dev tools in production

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { App } from '@features/application/App'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
