bf235ab6379dfc49ef4d940328a87b6b0380d2fb
Anton Arbus <anton.arbus@heeros.com>
2022-12-04 13:45:48 +0200

    npm run start & front & back

--- src/features/application/Main.tsx
+++ src/features/application/Main.tsx
@@ -1,30 +1,25 @@
 import { CounterFromRedux } from '@features/counter'
-import { useRefreshTokens } from '@features/credentials/useRefreshTokens'
 import { Offer } from '@features/offer/Offer'
 import { Outlet, Route, Routes } from 'react-router-dom'
 import { Dummy } from '../temp/Dummy'
 
-export const Main = () => {
-  useRefreshTokens()
-
-  return (
-    <main
-      css={{
-        margin: '10px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '20px 0px'
-      }}
-    >
-      <Outlet />
-      <Routes>
-        <Route path="/*" element={<Offer />} />
-        <Route path="/dummy" element={<Dummy />} />
-        <Route path="counter" element={<CounterFromRedux />} />
-        <Route path="div1" element={<div>div1</div>} />
-      </Routes>
-    </main>
-  )
-}
+export const Main = () => (
+  <main
+    css={{
+      margin: '10px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: '20px 0px'
+    }}
+  >
+    <Outlet />
+    <Routes>
+      <Route path="/*" element={<Offer />} />
+      <Route path="/dummy" element={<Dummy />} />
+      <Route path="counter" element={<CounterFromRedux />} />
+      <Route path="div1" element={<div>div1</div>} />
+    </Routes>
+  </main>
+)
