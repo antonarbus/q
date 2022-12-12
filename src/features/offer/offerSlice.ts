b2007da09986ab53d44ce70b37bcddcc19962d13
Anton Arbus <anton.arbus@macbookpro>
2022-12-12 13:28:18 +0200

    structuredClone

--- src/features/offer/offerSlice.ts
+++ src/features/offer/offerSlice.ts
@@ -1,9 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { globalObject } from '@src/globalObject'
+import { OfferType } from './templateOffer'
 
 export const offerSlice = createSlice({
   name: 'offerSlice',
-  initialState: window.structuredClone(globalObject.currentOffer),
+  initialState: window.structuredClone(globalObject.currentOffer) as OfferType,
   reducers: {
     someAction: (state, action) => {}
   }
