c5dde2d478266ec52dc130ce93e0f6d0d3370ff9
Anton Arbus <anton.arbus@macbookpro>
2022-12-12 13:26:30 +0200

    structuredClone

--- src/features/offer/offerSlice.ts
+++ src/features/offer/offerSlice.ts
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { globalObject } from '@src/globalObject'
-import { templateOffer } from './templateOffer'
-
-globalObject.currentOffer = localStorage.getItem('currentOffer') === null ? { ...templateOffer } : JSON.parse(localStorage.getItem('currentOffer') || '')
 
 export const offerSlice = createSlice({
   name: 'offerSlice',
-  initialState: JSON.parse(JSON.stringify(globalObject.currentOffer)),
+  initialState: window.structuredClone(globalObject.currentOffer),
   reducers: {
     someAction: (state, action) => {}
   }
