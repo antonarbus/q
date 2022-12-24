a66c5aa04b6a96febfed31a3c806a4959bcf988c
Anton Arbus <anton.arbus@macbookpro>
2022-12-24 14:26:10 +0200

    store refactor

--- src/features/Credentials/credentialsSlice.ts
+++ src/features/Credentials/credentialsSlice.ts
@@ -6,7 +6,7 @@ const initialState = {
   roles: ['no role']
 }
 
-export const credentialsSlice = createSlice({
+const credentialsSlice = createSlice({
   name: 'credentialsSlice',
   initialState,
   reducers: {
@@ -18,21 +18,5 @@ export const credentialsSlice = createSlice({
   }
 })
 
-/*
-  *  here is the convention, which I do not like because of default export and not clear traceability
-  *
-  *  export default credentialsSlice.reducer
-  *  export const { rememberLoggedUser, forgetLoggedUser } = credentialsSlice.actions
-  *
-  *  I like to export the whole slice instead and take all data directly from it, even it is more code
-  *
-  *  1. access in store configuration as
-  *  const store = configureStore({
-  *    reducer: {
-  *      credentials: credentialsSlice.reducer,
-  *    }
-  *  })
-  *
-  *  2. access action object as
-  *  dispatch(credentialsSlice.actions.forgetLoggedUser())
-*/
+export default credentialsSlice.reducer
+export const { rememberLoggedUser, forgetLoggedUser } = credentialsSlice.actions
