5a1a7f83f88305243e8966b5067a23b6d14d76b7
Anton Arbus <anton.arbus@macbookpro>
2023-01-08 20:01:32 +0200

    added react-query

--- client/application/App.tsx
+++ client/application/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RequireAuth } from '@client/credentials/RequireAuth'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Nav } from '@client/nav'
@@ -15,26 +17,30 @@ import { theme } from '@client/theme'
 import { GlobalStyles } from '@client/GlobalStyles'
 
 export const App = () => (
-  <ThemeProvider theme={createTheme(theme as any)}>
-    <GlobalStyles />
-    <BrowserRouter>
-      <SpinnerFullPage />
-      <Nav />
-      <Routes>
-        <Route path="/*" element={<Main />}>
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="reset" element={<Reset />} />
-        </Route>
-        <Route element={<PersistentAuth />}>
-          <Route element={<RequireAuth allowedRoles={['user']} />}>
-            <Route path="profile" element={<Profile />} />
-            <Route path="settings" element={<div>Settings</div>} />
+  <QueryClientProvider client={new QueryClient()}>
+    <ThemeProvider theme={createTheme(theme as any)}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <SpinnerFullPage />
+        <Nav />
+        <Routes>
+          <Route path="/*" element={<Main />}>
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="reset" element={<Reset />} />
           </Route>
-        </Route>
-        <Route path="unauthorized" element={<Unauthorized />} />
-      </Routes>
-      <Notifier />
-    </BrowserRouter>
-  </ThemeProvider>
+          <Route element={<PersistentAuth />}>
+            <Route element={<RequireAuth allowedRoles={['user']} />}>
+              <Route path="profile" element={<Profile />} />
+              <Route path="settings" element={<div>Settings</div>} />
+            </Route>
+          </Route>
+          <Route path="unauthorized" element={<Unauthorized />} />
+        </Routes>
+        <Notifier />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ThemeProvider>
+  </QueryClientProvider>
+
 )
