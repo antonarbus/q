afce63769320fd708c33c95c015695c792eae6b3
Anton Arbus <anton.arbus@macbookpro>
2023-01-31 19:02:21 +0200

    items

---
+++ client/offer/Items.tsx
@@ -0,0 +1,38 @@
+import { ResizablePaper } from './ResizablePaper'
+import { store, useDispatchTyped, useSelectorTyped } from 'client/store'
+import parseHtml from 'html-react-parser'
+import { Draggable, DraggableItem, DragHandle } from './draggable'
+import { updateOrderAfterDrag } from './offerSlice'
+import { useLocalStorage } from 'react-use'
+import { arrayMoveImmutable } from 'array-move'
+import { ActionsContainer } from './ActionsContainer'
+import { CopyIcon } from '../copy/CopyIcon'
+
+export const Items = () => {
+  const [, setCurrentOfferAtLocalStorage] = useLocalStorage('currentOffer')
+  const dispatch = useDispatchTyped()
+  const { items } = useSelectorTyped(state => state.offer)
+
+  return (
+    <Draggable
+      useDragHandle
+      onSortEnd={({ oldIndex, newIndex }) => {
+        const sortedItems = arrayMoveImmutable(items, oldIndex, newIndex)
+        dispatch(updateOrderAfterDrag({ sortedItems }))
+        setCurrentOfferAtLocalStorage(store.getState().offer)
+      }}
+    >
+      {items.map((item, index) => (
+        <DraggableItem key={item.id} index={index} >
+          <ActionsContainer>
+            <DragHandle />
+            <CopyIcon />
+          </ActionsContainer>
+          <ResizablePaper key={item.id} id={item.id} width={item.width} index={index}>
+            {item.type === 'text' && parseHtml(item.innerHtml)}
+          </ResizablePaper>
+        </DraggableItem>
+      ))}
+    </Draggable>
+  )
+}
