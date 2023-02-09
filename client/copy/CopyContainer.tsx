7d5796c7044dcb15ad6127947e2fbd2d8965a43f
Anton Arbus <anton.arbus@macbookpro>
2023-02-09 23:06:12 +0200

    some improvements

--- client/copy/CopyContainer.tsx
+++ client/copy/CopyContainer.tsx
@@ -112,7 +112,7 @@ export const CopyContainer = () => {
                   css={{
                     height: item.height * scaleFactor,
                     width: item.width * scaleFactor,
-                    position: 'relative'
+                    position: 'relative',
                   }}
                 >
                   <div
@@ -129,14 +129,6 @@ export const CopyContainer = () => {
                   >
                     {parseHtml(item.innerHtml)}
                   </div>
-                  <div css={{
-                    position: 'absolute',
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    background: 'rgba(255, 255, 255, 0.8)',
-                  }}></div>
                 </div>
               )
             })}
