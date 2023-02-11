2ad108c4964515bf331179b7c174ab0ed2112f8c
Anton Arbus <anton.arbus@macbookpro>
2023-02-11 12:50:23 +0200

    esc

--- client/copy/PressEsc.tsx
+++ client/copy/PressEsc.tsx
@@ -1,26 +1,14 @@
 import { Key } from 'client/components/Key'
-import { HiCursorClick } from 'react-icons/hi'
 
-export const PressEsc = () => {
-  return (
-    <div
-        css={{
-          textAlign: 'center',
-          color: 'grey',
-          fontSize: '12px',
-          marginBottom: '10px'
-        }}
-      >
-      <HiCursorClick
-        css={{
-          verticalAlign: 'middle',
-          width: '18px',
-          height: 'auto',
-          position: 'relative',
-          top: -1
-        }}
-      />
-      {' '} to paste, <Key>Esc</Key> to exit
-      </div>
-  )
-}
+export const PressEsc = () => (
+  <div
+      css={{
+        textAlign: 'center',
+        color: 'grey',
+        fontSize: '12px',
+        marginBottom: '10px'
+      }}
+    >
+      <Key>Esc</Key> to exit
+    </div>
+)
