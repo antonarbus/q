082ec1799ea2fb2dc38cfd59f027bd091147b82b
Anton Arbus <anton.arbus@macbookpro>
2023-02-15 19:56:10 +0200

    wait db connection before starting the client

--- server/server.ts
+++ server/server.ts
@@ -14,30 +14,32 @@ import { refreshRouter } from './api/refreshRouter'
 import { errorHandler } from './middleware/errorHandler'
 import { usersRouter } from './api/usersRouter'
 
-const app = express()
-connectToDb()
-app.use(morgan('dev')) // http logs in terminal
-app.use(express.json()) // parses incoming requests with JSON because we use lots of json, let it be default
-app.use(cookieParser())
-app.use(cors())
-app.get('/', (req: ReqType, res:ResType) => res.send('This is from express.js'))
-app.get('/api', (req: ReqType, res:ResType) => res.json({ message: '/api' }))
+(async function () {
+  const app = express()
+  await connectToDb()
+  app.use(morgan('dev')) // http logs in terminal
+  app.use(express.json()) // parses incoming requests with JSON because we use lots of json, let it be default
+  app.use(cookieParser())
+  app.use(cors())
+  app.get('/', (req: ReqType, res:ResType) => res.send('This is from express.js'))
+  app.get('/api', (req: ReqType, res:ResType) => res.json({ message: '/api' }))
 
-// use router from separate file
-const hi = require('./api/hi')
-app.use('/api/hi', hi)
+  // use router from separate file
+  const hi = require('./api/hi')
+  app.use('/api/hi', hi)
 
-app.use('/api/register', registerRouter)
-app.use('/api/login', loginRouter)
-app.use('/api/logout', logoutRouter)
-app.use('/api/activate', activateRouter)
-app.use('/api/refresh', refreshRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/user', userDetailsRouter)
+  app.use('/api/register', registerRouter)
+  app.use('/api/login', loginRouter)
+  app.use('/api/logout', logoutRouter)
+  app.use('/api/activate', activateRouter)
+  app.use('/api/refresh', refreshRouter)
+  app.use('/api/users', usersRouter)
+  app.use('/api/user', userDetailsRouter)
 
-app.use(errorHandler)
+  app.use(errorHandler)
 
-const port = process.env.PORT_BACK_END
-const domain = process.env.DOMAIN
+  const port = process.env.PORT_BACK_END
+  const domain = process.env.DOMAIN
 
-app.listen(port, () => console.log(`server started at ${domain}:${port}`))
+  app.listen(port, () => console.log(`server started at ${domain}:${port}`))
+})()
