89cb8f053bd1f80d0e19333cc23277f6c04bfdeb
Anton Arbus <anton.arbus@macbookpro>
2023-02-21 23:55:01 +0200

    items animation

--- client/offer/items/PasteTextOnTopOrBottom.tsx
+++ client/offer/items/PasteTextOnTopOrBottom.tsx
@@ -2,14 +2,14 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 
 export const PasteTextOnTopOrBottom = () => {
-  const [isExitAnimated, setIsExitAnimated] = useState(true)
-  console.log('🚀 ~ file: PasteTextOnTopOrBottom.tsx:6 ~ PasteTextOnTopOrBottom ~ state', isExitAnimated)
+  const [isExitAnimated, setIsExitAnimated] = useState(true) // disable exit animation on click
 
   return (
     <motion.div
+      // onClick={() => setIsExitAnimated(false)}
       initial={{ height: 0, opacity: 0 }}
       animate={{ height: 'auto', opacity: 1 }}
-      exit={{ ...(isExitAnimated && { height: 0, opacity: 0 }) }} //! item is pasted immediately, but PasteText exits with animation, need to disable animation on before paste
+      exit={{ ...(isExitAnimated && { height: 0, opacity: 0 }) }}
       transition={{
         height: { duration: 0.3 },
         opacity: { duration: 0 },
@@ -22,7 +22,6 @@ export const PasteTextOnTopOrBottom = () => {
         cursor: 'default',
         userSelect: 'none',
       }}
-      onClick={() => setIsExitAnimated(false)}
     >
       Paste here
     </motion.div>
--- client/offer/items/draggable/DraggableItem.tsx
+++ client/offer/items/draggable/DraggableItem.tsx
@@ -1,4 +1,5 @@
 import { SortableElement, SortableElementProps } from 'react-sortable-hoc'
+import { motion } from 'framer-motion'
 
 type Props = {
   children: React.ReactNode
@@ -10,18 +11,29 @@ interface ISortableItem extends SortableElementProps {
 
 export const DraggableItem: React.ComponentClass<ISortableItem, any> = SortableElement(({ children }: Props) => {
   return (
-    <div
+    <motion.div
+      initial={{ height: 0, marginTop: 0, marginBottom: 0, opacity: 0, y: 1000 }}
+      animate={{ height: 'auto', marginTop: 10, marginBottom: 10, opacity: 1, y: 0 }}
+      exit={{ height: 0, marginTop: 0, marginBottom: 0, opacity: 0, x: 1000 }}
+      transition={{
+        height: { duration: 0.5 },
+        opacity: { duration: 0.5 },
+        y: { duration: 0.5 },
+        x: { duration: 0.5 },
+        marginTop: { duration: 0.5 },
+        marginBottom: { duration: 0.5 },
+      }}
       css={{
         display: 'flex',
         justifyContent: 'center',
         gap: '2px',
         position: 'relative',
         maxWidth: '100%',
-        marginTop: 10,
-        marginBottom: 10,
+        // marginTop: 10,
+        // marginBottom: 10,
       }}
     >
       {children}
-    </div>
+    </motion.div>
   )
 })
