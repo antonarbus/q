5172c0b9af43f61bb981c7b5ff9568e8ab116c95
Anton Arbus <anton.arbus@macbookpro>
2023-02-21 22:02:07 +0200

    dynamic exit

--- client/offer/items/PasteTextOnTopOrBottom.tsx
+++ client/offer/items/PasteTextOnTopOrBottom.tsx
@@ -1,23 +1,30 @@
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
-export const PasteTextOnTopOrBottom = () => (
-  <motion.div
-    initial={{ height: 0, opacity: 0 }}
-    animate={{ height: 'auto', opacity: 1 }}
-    exit={{ height: 0, opacity: 0 }}
-    transition={{
-      height: { duration: 0.3 },
-      opacity: { duration: 0 },
-    }}
-    css={{
-      display: 'grid',
-      placeItems: 'center',
-      fontWeight: 600,
-      color: '#b4b4b4',
-      cursor: 'default',
-      userSelect: 'none',
-    }}
-  >
-    Paste here
-  </motion.div>
-)
+export const PasteTextOnTopOrBottom = () => {
+  const [isExitAnimated, setIsExitAnimated] = useState(true)
+  console.log('🚀 ~ file: PasteTextOnTopOrBottom.tsx:6 ~ PasteTextOnTopOrBottom ~ state', isExitAnimated)
+
+  return (
+    <motion.div
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: 'auto', opacity: 1 }}
+      exit={{ ...(isExitAnimated && { height: 0, opacity: 0 }) }} //! item is pasted immediately, but PasteText exits with animation, need to disable animation on before paste
+      transition={{
+        height: { duration: 0.3 },
+        opacity: { duration: 0 },
+      }}
+      css={{
+        display: 'grid',
+        placeItems: 'center',
+        fontWeight: 600,
+        color: '#b4b4b4',
+        cursor: 'default',
+        userSelect: 'none',
+      }}
+      onClick={() => setIsExitAnimated(false)}
+    >
+      Paste here
+    </motion.div>
+  )
+}
