7620183d66100f29b5d3a7f84b32bc7c7bcb9dcb
Anton Arbus <anton.arbus@macbookpro>
2023-02-26 17:54:03 +0200

    same shadow

--- client/copy/FirstCopiedItem.tsx
+++ client/copy/FirstCopiedItem.tsx
@@ -52,17 +52,16 @@ export const FirstCopiedItem = () => {
           height: items[0].height * scaleFactorForFirstItem,
           width: items[0].width * scaleFactorForFirstItem,
           marginTop: 15,
-          marginBottom: itemMarginBottom
+          marginBottom: itemMarginBottom,
+          background: 'white',
+          borderRadius: 4,
+          boxShadow: '#00000033 0px 0px 12px 2px',
         }}
       >
         <div
           css={{
-            background: 'white',
-            borderRadius: 6,
-            // boxShadow: '#00000033 0px 0px 12px 2px',
-            boxShadow: '#00000033 0px 0px 10px 0px',
             padding: 20,
-            marginBottom: 5,
+            // marginBottom: 5,
             width: items[0].width,
             transformOrigin: 'left top',
             scale: `${scaleFactorForFirstItem}`,
