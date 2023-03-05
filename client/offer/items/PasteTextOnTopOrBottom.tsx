da984e6c782521e4963216dc0f1f5965a5f09787
Anton Arbus <anton.arbus@macbookpro>
2023-03-05 21:05:15 +0200

    fixed some small things

--- client/offer/items/PasteTextOnTopOrBottom.tsx
+++ client/offer/items/PasteTextOnTopOrBottom.tsx
@@ -1,15 +1,23 @@
 import { motion } from 'framer-motion'
-import { useState } from 'react'
 
 export const PasteTextOnTopOrBottom = () => {
-  const [isExitAnimated, setIsExitAnimated] = useState(true) // disable exit animation on click
-
   return (
     <motion.div
-      // onClick={() => setIsExitAnimated(false)}
-      initial={{ height: 0, opacity: 0 }}
-      animate={{ height: 'auto', opacity: 1 }}
-      exit={{ ...(isExitAnimated && { height: 0, opacity: 0 }) }}
+      initial={{
+        height: 0,
+        opacity: 0,
+        marginBottom: 0
+      }}
+      animate={{
+        height: 20,
+        opacity: 1,
+        marginBottom: 20
+      }}
+      exit={{
+        height: 0,
+        opacity: 0,
+        marginBottom: 0
+      }}
       transition={{
         height: { duration: 0.3 },
         opacity: { duration: 0 },
@@ -20,7 +28,7 @@ export const PasteTextOnTopOrBottom = () => {
         fontWeight: 600,
         color: '#b4b4b4',
         userSelect: 'none',
-        cursor: 'pointer'
+        cursor: 'pointer',
       }}
     >
       Paste here
--- client/offer/items/draggable/DraggableItem.tsx
+++ client/offer/items/draggable/DraggableItem.tsx
@@ -12,10 +12,27 @@ interface ISortableItem extends SortableElementProps {
 export const DraggableItem: React.ComponentClass<ISortableItem, any> = SortableElement(({ children }: Props) => {
   return (
     <motion.div
-      initial={{ height: 0, marginTop: 0, marginBottom: 0, opacity: 0, y: '100vh' }}
-      animate={{ height: 'auto', marginTop: 10, marginBottom: 10, opacity: 1, y: 0 }}
-      exit={{ height: 0, marginTop: 0, marginBottom: 0, opacity: 0, x: 'calc(100vw + 1000px)' }} // off the screen + a bit more, coz do not want to show height animation, but without height jump occurs
-      transition={{ duration: 0.5 }}
+      initial={{
+        height: 0,
+        marginBottom: 0,
+        opacity: 0,
+        y: '100vh'
+      }}
+      animate={{
+        height: 'auto',
+        marginBottom: 20,
+        opacity: 1,
+        y: 0
+      }}
+      exit={{
+        height: 0,
+        marginBottom: 0,
+        opacity: 0,
+        x: '200vw'
+      }}
+      transition={{
+        duration: 0.5
+      }}
       css={{
         display: 'flex',
         justifyContent: 'center',
