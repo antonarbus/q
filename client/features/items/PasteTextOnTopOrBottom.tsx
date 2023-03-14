e8fb529a32bfd57eb7dcf8c25dc27d359a526bbc
Anton Arbus <anton.arbus@macbookpro>
2023-03-14 23:26:10 +0200

    fixed strange jump at the last paste

--- client/features/items/PasteTextOnTopOrBottom.tsx
+++ client/features/items/PasteTextOnTopOrBottom.tsx
@@ -1,41 +1,34 @@
-import { useSelectorTyped } from 'client/store'
 import { motion } from 'framer-motion'
 
-export const PasteTextOnTopOrBottom = () => {
-  const isPasteTextShown = useSelectorTyped(state => state.copy.isPasteTextShown)
-
-  if (!isPasteTextShown) return null
-
-  return (
-    <motion.div
-      initial={{
-        height: 0,
-        opacity: 0,
-        marginBottom: 0
-      }}
-      animate={{
-        height: 20,
-        opacity: 1,
-        marginBottom: 20
-      }}
-      exit={{
-        height: 0,
-        opacity: 0,
-        marginBottom: 0
-      }}
-      transition={{
-        height: { duration: 0.3 },
-        opacity: { duration: 0 },
-      }}
-      css={{
-        display: 'grid',
-        placeItems: 'center',
-        fontWeight: 600,
-        color: '#b4b4b4',
-        userSelect: 'none',
-      }}
-    >
-      Paste here
-    </motion.div>
-  )
-}
+export const PasteTextOnTopOrBottom = () => (
+  <motion.div
+    initial={{
+      height: 0,
+      opacity: 0,
+      marginBottom: 0
+    }}
+    animate={{
+      height: 20,
+      opacity: 1,
+      marginBottom: 20
+    }}
+    exit={{
+      height: 0,
+      opacity: 0,
+      marginBottom: 0
+    }}
+    transition={{
+      height: { duration: 0.3 },
+      opacity: { duration: 0 },
+    }}
+    css={{
+      display: 'grid',
+      placeItems: 'center',
+      fontWeight: 600,
+      color: '#b4b4b4',
+      userSelect: 'none',
+    }}
+  >
+    Paste here
+  </motion.div>
+)
