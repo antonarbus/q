a722feab5ee2b7c4f2f52207f62c2d7a610efad2
Anton Arbus <anton.arbus@macbookpro>
2023-03-17 00:58:55 +0200

    init froala in useEffect

--- client/features/FroalaItem/FroalaItem.tsx
+++ client/features/FroalaItem/FroalaItem.tsx
@@ -1,12 +1,13 @@
 import parseHtml from 'html-react-parser'
 import { DraggableResizableItemWithActions } from 'client/components/DraggableResizableItemWithActions'
 import { ItemType } from '../items/types'
-import FroalaEditor from 'react-froala-wysiwyg'
+// import FroalaEditor from 'react-froala-wysiwyg'
 import 'froala-editor/js/froala_editor.pkgd.min.js'
 import 'froala-editor/js/plugins.pkgd.min.js'
 import 'froala-editor/css/froala_style.min.css'
 import 'froala-editor/css/froala_editor.pkgd.min.css'
 import 'froala-editor/css/plugins.pkgd.min.css'
+import { useEffectOnce } from 'react-use'
 // import 'font-awesome/css/font-awesome.css'
 
 type Props = {
@@ -14,45 +15,49 @@ type Props = {
   index: number
 }
 
+// todo: try to use basic js Froala and initiate it in useEffect
+// todo: also pass its object to DraggableItem to initiated only after animation
+// todo: or even better do it on click and destroy it on click away
+// todo: var editor = new FroalaEditor('#example');
+
 export const FroalaItem = ({ item, index }: Props) => {
+  useEffectOnce(() => {
+    const editor = new FroalaEditor(`#froala${item.id}`, {
+      toolbarInline: true,
+      fontSizeSelection: true,
+      charCounterCount: false,
+      tabSpaces: 4,
+      toolbarButtons: {
+        moreText: {
+          buttons: ['fontSize', 'textColor', 'backgroundColor', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'inlineClass', 'inlineStyle', 'clearFormatting'],
+          buttonsVisible: 3,
+        },
+        moreParagraph: {
+          buttons: ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote'],
+          buttonsVisible: 3,
+        },
+        moreRich: {
+          buttons: ['insertLink', 'insertTable', 'insertImage', 'insertVideo', 'emoticons', 'embedly', 'fontAwesome', 'specialCharacters', 'insertFile', 'insertHR', 'html'],
+          buttonsVisible: 4,
+        }
+      },
+      fontFamily: { Roboto: 'Roboto', Arial: 'Arial', Georgia: 'Georgia', Tahoma: 'Tahoma', Verdana: 'Verdana', Helvetica: 'Helvetica', 'Trebuchet MS': 'Trebuchet MS', 'Times New Roman': 'Times New Roman', Garamond: 'Garamond', 'Courier New': 'Courier New', 'Brush Script MT': 'Brush Script MT' },
+      placeholderText: 'Type text, insert links, drop files or pictures...',
+      tableInsertHelper: false,
+      tableInsertMaxSize: 12
+    })
+  })
+
   return (
     <DraggableResizableItemWithActions
       index={index}
       item={item}
     >
-      <FroalaEditor
-        config={{
-          initOnClick: true,
-          charCounterCount: false,
-          toolbarInline: true,
-          // initOnClick: true,
-          // toolbarVisibleWithoutSelection: true,
-          fontSizeSelection: true,
-          tabSpaces: 4,
-          // https://froala.com/wysiwyg-editor/docs/options/#toolbarButtons
-          toolbarButtons: {
-            moreText: {
-              buttons: ['fontSize', 'textColor', 'backgroundColor', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'inlineClass', 'inlineStyle', 'clearFormatting'],
-              buttonsVisible: 3,
-            },
-            moreParagraph: {
-              buttons: ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote'],
-              buttonsVisible: 3,
-            },
-            moreRich: {
-              buttons: ['insertLink', 'insertTable', 'insertImage', 'insertVideo', 'emoticons', 'embedly', 'fontAwesome', 'specialCharacters', 'insertFile', 'insertHR', 'html'],
-              buttonsVisible: 4,
-            }
-          },
-          fontFamily: { Roboto: 'Roboto', Arial: 'Arial', Georgia: 'Georgia', Tahoma: 'Tahoma', Verdana: 'Verdana', Helvetica: 'Helvetica', 'Trebuchet MS': 'Trebuchet MS', 'Times New Roman': 'Times New Roman', Garamond: 'Garamond', 'Courier New': 'Courier New', 'Brush Script MT': 'Brush Script MT' },
-          placeholderText: 'Type text, insert links, drop files or pictures...',
-          tableInsertHelper: false,
-          tableInsertMaxSize: 12,
-        }}
-        tag="div"
+      <div
+        id={'froala' + item.id}
       >
         {parseHtml(item.innerHtml)}
-      </FroalaEditor>
+      </div>
     </DraggableResizableItemWithActions>
   )
 }
