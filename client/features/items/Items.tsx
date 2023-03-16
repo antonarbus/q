3f40a148de684055d17230e5147f3cc805fb6075
Anton Arbus <anton.arbus@macbookpro>
2023-03-17 00:25:49 +0200

    some kind of works

--- client/features/items/Items.tsx
+++ client/features/items/Items.tsx
@@ -2,6 +2,7 @@ import { useSelectorTyped } from 'client/store'
 import { ItemsContainer } from './ItemsContainer'
 import { PasteText } from '../copy/PasteText'
 import { TextItem } from './TextItem'
+import { FroalaItem } from '../FroalaItem'
 
 export const Items = () => {
   const items = useSelectorTyped(state => state.items)
@@ -9,7 +10,8 @@ export const Items = () => {
   return (
     <ItemsContainer>
       {items.map((item, index) => {
-        if (item.type === 'text') return <TextItem key={item.id} item={item} index={index} />
+        // if (item.type === 'text') return <TextItem key={item.id} item={item} index={index} />
+        if (item.type === 'text') return <FroalaItem key={item.id} item={item} index={index} />
         if (item.type === 'paste') return <PasteText key={item.id} />
         return null
       })}
