7b40798e34a056caef0ea837b2c6506f99eae0ef
Anton Arbus <anton.arbus@macbookpro>
2023-03-20 09:33:15 +0200

    useFroala

---
+++ client/features/FroalaItem/useFroala.tsx
@@ -0,0 +1,92 @@
+// // @ts-nocheck
+import { useDispatchTyped } from 'client/store'
+import { useRef } from 'react'
+import { useEffectOnce } from 'react-use'
+
+export const useFroala = ({ initHtml }) => {
+  const froalaRef = useRef() as React.MutableRefObject<HTMLDivElement>
+  const dispatch = useDispatchTyped()
+  const editorRef = useRef()
+
+  useEffectOnce(() => {
+    editorRef.current = new FroalaEditor(
+      froalaRef.current,
+      {
+        toolbarInline: true,
+        fontSizeSelection: true,
+        charCounterCount: false,
+        tabSpaces: 4,
+        toolbarButtons: {
+          moreText: {
+            buttons: ['fontSize', 'textColor', 'backgroundColor', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'inlineClass', 'inlineStyle', 'clearFormatting'],
+            buttonsVisible: 3,
+          },
+          moreParagraph: {
+            buttons: ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote'],
+            buttonsVisible: 3,
+          },
+          moreRich: {
+            buttons: ['insertLink', 'insertTable', 'insertImage', 'insertVideo', 'emoticons', 'embedly', 'fontAwesome', 'specialCharacters', 'insertFile', 'insertHR', 'html'],
+            buttonsVisible: 4,
+          }
+        },
+        fontFamily: { Roboto: 'Roboto', Arial: 'Arial', Georgia: 'Georgia', Tahoma: 'Tahoma', Verdana: 'Verdana', Helvetica: 'Helvetica', 'Trebuchet MS': 'Trebuchet MS', 'Times New Roman': 'Times New Roman', Garamond: 'Garamond', 'Courier New': 'Courier New', 'Brush Script MT': 'Brush Script MT' },
+        placeholderText: 'Your text, links, files & images go here...',
+        tableInsertHelper: false,
+        tableInsertMaxSize: 12,
+        // https://froala.com/wysiwyg-editor/docs/concepts/image/upload/
+        // imageUploadURL: './../phps/upload_image.php',
+        // fileUploadURL: './../phps/upload_file.php',
+        // videoUploadURL: './../phps/upload_video.php',
+        fileMaxSize: 1024 * 1024 * 30,
+        // https://froala.com/wysiwyg-editor/docs/events/
+        events: {
+        // https://froala.com/wysiwyg-editor/docs/events/#image.removed
+          'image.removed': function ($img) {
+          // Do something here.
+            console.log(this)
+            console.log($img.attr('src'))
+          },
+          'paste.before': function () {
+            console.log('paste before - froala')
+          // addToUndoArr()
+          },
+          // https://froala.com/wysiwyg-editor/docs/events/#paste.after
+          'paste.after': function () {
+            console.log('paste after - froala')
+          // addToUndoArr()
+          },
+          initialized: function () {
+          // $('a[href*="froala"]').parent().remove()
+          },
+          'toolbar.show': function () {
+          // console.log('toolbar on')
+
+          },
+          'toolbar.hide': function () {
+          // console.log('toolbar off')
+          },
+        // 'html.get': function (xxx) {
+        //   // https://froala.com/wysiwyg-editor/docs/events/#html.get
+        //   console.log(this)
+        //   const innerHTML = editor.current.html.get()
+        //   // console.log('🚀 ~ file: FroalaItem.tsx:36 ~ saveText ~ innerHTML:', innerHTML)
+        //   // dispatch(updateItemText({ index, innerHTML }))
+        //   // saveItemsIntoLocalStorage()
+        // }
+        },
+        key:
+        'AVB8B-21D4B3B2E1F1G1uB-33B-21cyoF-10yB-7G-7gB-22zzE2wkA-7gC7B7D6B4E4F3D2I3H2C5==',
+      },
+      function () {
+        this.html.set(initHtml)
+      }
+    )
+
+    return () => {
+      editorRef.current.destroy()
+    }
+  })
+
+  return { froalaRef, editorRef }
+}
