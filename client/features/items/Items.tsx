d91af1b7bf47fdc67e577c8dba68b26b71b153b2
Anton Arbus <anton.arbus@macbookpro>
2023-03-22 12:47:02 +0200

    text editable

--- client/features/items/Items.tsx
+++ client/features/items/Items.tsx
@@ -2,6 +2,7 @@ import { useSelectorTyped } from 'client/store'
 import { ItemsContainer } from './ItemsContainer'
 import { PasteText } from '../copy/PasteText'
 import { FroalaItem } from '../FroalaItem'
+import { TextItem } from './TextItem'
 
 export const Items = () => {
   const items = useSelectorTyped(state => state.items)
@@ -9,7 +10,8 @@ export const Items = () => {
   return (
     <ItemsContainer>
       {items.map((item, index) => {
-        if (item.type === 'text') return <FroalaItem key={item.id} item={item} index={index} />
+        if (item.type === 'text') return <TextItem key={item.id} item={item} index={index} />
+        if (item.type === 'text editable') return <FroalaItem key={item.id} item={item} index={index} />
         if (item.type === 'paste') return <PasteText key={item.id} />
         return null
       })}
--- client/features/items/types.ts
+++ client/features/items/types.ts
@@ -1,2 +1,2 @@
-export type ItemType = { id: string, type: 'text' | 'paste', width: number, height: number, innerHtml: string }
+export type ItemType = { id: string, type: 'text' | 'text editable' | 'paste', width: number, height: number, innerHtml: string }
 export type ItemsType = ItemType[]
