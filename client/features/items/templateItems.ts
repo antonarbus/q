8c256f4e2986cdd8355611f6b6dbbc487d579cb3
Anton Arbus <anton.arbus@macbookpro>
2023-03-22 12:47:02 +0200

    text editable

--- client/features/items/templateItems.ts
+++ client/features/items/templateItems.ts
@@ -3,21 +3,21 @@ import { ItemsType } from './types'
 export const templateItems: ItemsType = [
   {
     id: 'id0',
-    type: 'text',
+    type: 'text editable',
     width: 800,
     height: 136,
     innerHtml: '<div>0</div><div>0</div><div>0</div><div>0</div>'
   },
   {
     id: 'id1',
-    type: 'text',
+    type: 'text editable',
     width: 700,
     height: 88,
     innerHtml: '<div>1</div><div>1</div>'
   },
   {
     id: 'id2',
-    type: 'text',
+    type: 'text editable',
     width: 600,
     height: 73,
     innerHtml: '<div>2</div>'
