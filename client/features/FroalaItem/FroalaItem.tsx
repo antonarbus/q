54c898f3928b337dd639d89d3c804991bf97a543
Anton Arbus <anton.arbus@macbookpro>
2023-03-22 09:08:29 +0200

    put padding into theme

--- client/features/FroalaItem/FroalaItem.tsx
+++ client/features/FroalaItem/FroalaItem.tsx
@@ -8,6 +8,7 @@ import { useEffectOnce } from 'react-use'
 import { useDispatchTyped } from 'client/store'
 import { updateItemText } from '../items/itemsSlice'
 import { saveItemsIntoLocalStorage } from 'client/modules/localStorage'
+import { theme } from 'client/theme'
 
 type Props = {
   item: ItemType
@@ -59,7 +60,9 @@ export const FroalaItem = ({ item, index }: Props) => {
             }
           },
         }}
-        style={{ height: (item.height && item.height - 40) || 'auto' }}
+        // at first we have fixed height to avoid height change when froala converts html into text on initialization
+        // when froala is initialized we make height: 'auto' to let it adjust when new text is added from the keyboard
+        style={{ height: item.height - 2 * theme.item.padding }}
       >
         {/* {parseHtml(item.innerHtml)} */}
       </div>
