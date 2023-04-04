a2bfb41c0c919c6c8ebf5d29b2bec751652936f7
Anton Arbus <anton.arbus@macbookpro>
2023-04-04 11:12:23 +0300

    msg animation

--- client/components/DraggableResizableItemWithActions/Msg.tsx
+++ client/components/DraggableResizableItemWithActions/Msg.tsx
@@ -1,4 +1,5 @@
 import { useSelectorTyped } from 'client/store'
+import { motion, AnimatePresence } from 'framer-motion'
 
 type Props = {
   index: number
@@ -7,21 +8,26 @@ type Props = {
 export const Msg = ({ index }: Props) => {
   const msg = useSelectorTyped(state => state.items[index]?.msg)
 
-  if (!msg) return null
-
   return (
-    <span
-      css={{
-        position: 'absolute',
-        top: 5,
-        right: 10,
-        fontSize: 10,
-        color: '#b7b7b7',
-        fontWeight: 500,
-        userSelect: 'none'
-      }}
-    >
-      {msg}
-    </span>
+    <AnimatePresence>
+      {msg && (
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          css={{
+            position: 'absolute',
+            top: 5,
+            right: 10,
+            fontSize: 10,
+            color: '#b7b7b7',
+            fontWeight: 500,
+            userSelect: 'none'
+          }}
+        >
+          {msg}
+        </motion.span>
+      )}
+    </AnimatePresence>
   )
 }
