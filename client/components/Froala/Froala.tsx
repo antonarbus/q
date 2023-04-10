8964c714064cb119777bd78b5acbe2a49aaa8ac8
Anton Arbus <anton.arbus@macbookpro>
2023-04-11 00:28:56 +0300

    removeProperty

--- client/components/Froala/Froala.tsx
+++ client/components/Froala/Froala.tsx
@@ -14,8 +14,8 @@ type Props = {
 * @param initHeight may need to preserve height until froala is not instantiated
 * @param initHtml initHtml for initial Froala render
 * @param onClickAwayIfHtmChanged can be used to save current html in redux or sent it somewhere
-* @param froalaElementRef can be used to get an access to the Froala element externally
-* @param editorRef can be used to get an access to the Froala methods externally
+* @param froalaElementRef can be used to get an access to the Froala element
+* @param editorRef can be used to get an access to the Froala methods
 */
 
 export const Froala = ({ padding, initHeight, initHtml, onClickAwayIfHtmChanged, froalaElementRef, editorRef }: Props) => {
