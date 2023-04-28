f993bfd2f2724a72d3c8499da5a9dea1953cdea5
Anton Arbus <anton.arbus@macbookpro>
2023-04-28 13:44:19 +0300

    comment

--- client/ReactQueryDevtoolsProductionHidden.tsx
+++ client/ReactQueryDevtoolsProductionHidden.tsx
@@ -35,6 +35,7 @@ export const ReactQueryDevtoolsProductionHidden = () => {
     window.addEventListener('keydown', activateReactQueryDevTools)
     window.addEventListener('keyup', emptyKeysPressedArray)
 
+    // ? probably this part is not needed as it is not that good idea
     const oneMin = 60 * 1000
     setTimeout(() => {
       window.removeEventListener('keydown', activateReactQueryDevTools)
