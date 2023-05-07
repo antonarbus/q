13f8bed0c754d3c2675007a7a7f8b361f13f8067
Anton Arbus <anton.arbus@macbookpro>
2023-05-07 23:37:16 +0300

    comments

--- client/features/items/FixHeightForElementAnimation.tsx
+++ client/features/items/FixHeightForElementAnimation.tsx
@@ -8,28 +8,28 @@ type TProps = {
   children: TChildren
 }
 
-//* we have slide in item animation after pasting
+//* we have slide-in item animation after paste
 //* height is animated from 0 to 'auto'
-//* if item children not initiated yet or not available immediately
+//* if item children not initiated yet or not available immediately or smth
 //* then auto height will be smaller then it should be
-//* we will see height jump after animation when height is calculated
-//* to avoid it we always keep saving item height value
-//* we wrap our content into a this wrapper which at the beginning has this height
-//* and we remove it after animation is supposed to finnish
-
-//* do not like 'setTimeout', but let it be for now
-//* we maybe can use onAnimationComplete callback, but need to find a way to pass it, fuck that
+//* we will see height jump when height is calculated
+//* to avoid it we save item height value in redux
+//* we wrap our item content into this wrapper which at the beginning has height from redux
+//* and we remove it when animation is supposed to finnish, in 0.5s
+//* do not 'setTimeout' approach, but let it be for now
+//* we maybe can use onAnimationComplete callback from framer-motion, but need to find a way to pass it, not sure
 
 // todo: check if same will work for EditableText and small Forala elements
 // todo: make overflow: hidden on animation, and then remove it, probably right here!
 // todo: itemRef is not actually an item, it is somewhere inside, check that
 export const FixHeightForElementAnimation = ({ height, children }: TProps) => {
   const ref = useRef() as TRefDiv
+  const delayMs = 1000 * theme.item.animationDuration
 
   useEffectOnce(() => {
     setTimeout(() => {
       ref.current.style.removeProperty('height')
-    }, 1000 * theme.item.animationDuration)
+    }, delayMs)
   })
 
   return (
