733d82186d3caaf3ce6991fca52164ff59c037c0
Anton Arbus <anton.arbus@macbookpro>
2023-05-07 15:24:51 +0300

    boq height

--- client/features/boq/BoqItem.tsx
+++ client/features/boq/BoqItem.tsx
@@ -1,23 +1,35 @@
 import { DraggableResizableItemWithActions } from 'client/components/DraggableResizableItemWithActions'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { BoqHeader } from './boqHeader'
-import { RefResizableType } from 'client/types'
+import { RefDivType, RefResizableType } from 'client/types'
 import { BoqTable } from './boqHeader/boqTable'
+import { store } from 'client/store'
 
 type Props = {
   index: number
 }
 
 export const BoqItem = ({ index }: Props) => {
+  const ref = useRef() as RefDivType
   const itemRef = useRef() as RefResizableType
+  const height = store.getState().items[index].height
+
+  useEffect(() => {
+    ref.current.style.removeProperty('height')
+  }, [])
 
   return (
     <DraggableResizableItemWithActions
       index={index}
       itemRef={itemRef}
     >
-      <BoqHeader index={index} itemRef={itemRef} />
-      <BoqTable index={index} itemRef={itemRef} />
+      <div
+        ref={ref}
+        style={{ height }}
+      >
+        <BoqHeader index={index} itemRef={itemRef} />
+        <BoqTable index={index} itemRef={itemRef} />
+      </div>
     </DraggableResizableItemWithActions>
   )
 }
