e06ac8ba433f09d5aabbc69675f7eba923663484
Anton Arbus <anton.arbus@macbookpro>
2023-05-12 10:54:35 +0300

    comment update

--- client/features/items/FixHeightBeforePasteAnimation.tsx
+++ client/features/items/FixHeightBeforePasteAnimation.tsx
@@ -9,15 +9,15 @@ type TProps = {
 }
 
 //* we have slide-in item animation after paste
-//* height is animated from 0 to 'auto'
-//* if item children not initiated yet or not available immediately or smth
-//* then auto height will be smaller then it should be
+//* item height is animated from 0 to 'auto'
+//* if item's children not initiated by Froala or AG-grid yet or not available immediately or smth
+//* then height 'auto' will be smaller then it should be
 //* we will see height jump when height is calculated
-//* to avoid it we save item height value in redux
-//* we wrap our item content into this wrapper which at the beginning has height from redux
-//* and we remove it when animation is supposed to finnish, in 0.5s
-//* do not 'setTimeout' approach, but let it be for now
-//* we maybe can use onAnimationComplete callback from framer-motion, but need to find a way to pass it, not sure
+//* to avoid it we save an item height value in redux
+//* we wrap our item content into this wrapper which at the beginning has fixed height from redux
+//* and we remove fixed height when animation is supposed to finnish, in 0.5s
+//* do not like 'setTimeout' approach, but let it be for now
+//* we probably can use onAnimationComplete callback from 'framer-motion', but need to find a way to pass it, not sure
 
 // todo: move FixHeightBeforePasteAnimation in common folder to the
 // todo: instead of set item heights in default object, let's put them on init load and then they will be saved in local storage
