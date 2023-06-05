fd2ddbbd4f7c390244b3bde1cf8784482b7714f0
Anton Arbus <anton.arbus@macbookpro>
2023-06-05 09:39:19 +0300

    color grey on hover for editable text

--- client/components/Froala/Froala.tsx
+++ client/components/Froala/Froala.tsx
@@ -25,8 +25,8 @@ export const Froala = ({ index, editorRef, froalaElementRef, initHtml, onClickAw
         padding: padding || 0,
         wordBreak: 'break-word',
         '& .fr-element:hover:not(:focus)': {
-          textShadow: '0px 0px 0.8px'
-          // color: 'grey'
+          // textShadow: '0px 0px 0.8px',
+          color: 'grey'
         },
         ...sx
       }}
