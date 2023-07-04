59513aabb50d0fb6acbfb5aced05ca659b022181
Anton Arbus <anton.arbus@macbookpro>
2023-07-04 22:46:12 +0300

    FroalaForCopyMode

---
+++ client/components/Froala/FroalaForCopyMode.tsx
@@ -0,0 +1,38 @@
+
+import { Box, SxProps } from '@mui/material'
+import { useRef } from 'react'
+import { useEffectOnce } from 'react-use'
+
+type TProps = {
+  padding?: number | string
+  html: string
+  additionalStyle?: SxProps
+}
+
+export const FroalaForCopyMode = ({ html: initHtml, padding, additionalStyle }: TProps) => {
+  const ref = useRef<HTMLDivElement>()
+
+  useEffectOnce(function insertHtmlIntoElement() {
+    if (ref.current) {
+      ref.current.innerHTML = initHtml
+    }
+  })
+
+  return (
+    <Box
+      ref={ref}
+      className='not-editable-froala'
+      style={{
+        padding: padding || 0,
+      }}
+      sx={{
+        wordBreak: 'break-word',
+        '& .fr-element:hover:not(:focus)': {
+          textShadow: '0px 0px 0.8px',
+        },
+        ...additionalStyle,
+      }}
+    >
+    </Box>
+  )
+}
