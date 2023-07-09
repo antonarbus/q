aeb85628fdaa9a4164bb0254633667376042a55d
Anton Arbus <anton.arbus@macbookpro>
2023-07-09 21:56:24 +0300

    usePutCaretAtTheEndOfText

---
+++ client/components/Froala/usePutCaretAtTheEndOfText.tsx
@@ -0,0 +1,39 @@
+import { TRefAny, TRefDiv } from 'client/types'
+import { useEffect } from 'react'
+
+type TProps = {
+  index: number
+  froalaElementRef: TRefDiv
+  editorRef: TRefAny
+  isCopyMode: boolean
+}
+
+export const usePutCaretAtTheEndOfText = ({ index, isCopyMode, editorRef, froalaElementRef }: TProps) => {
+  useEffect(() => {
+    if (isCopyMode) return
+    function focusOnTextIfCellOrPaddingAreClicked(e: MouseEvent) {
+      // https://stackoverflow.com/a/35191761/7239778
+      const clickedElement = e.target as HTMLElement
+
+      if (clickedElement.matches('.fr-box')) {
+        editorRef.current.selection.setAtEnd(editorRef.current.$el.get(0))
+      }
+
+      if (clickedElement.matches('.ag-cell')) {
+        // editorRef.current.selection.setAtStart(editorRef.current.$el.get(0))
+        editorRef.current.selection.setAtEnd(editorRef.current.$el.get(0))
+      }
+
+      editorRef.current.selection.restore()
+    }
+
+    froalaElementRef?.current?.addEventListener('click', focusOnTextIfCellOrPaddingAreClicked)
+    const tableCell = froalaElementRef?.current.closest('.ag-cell') as HTMLElement
+    tableCell?.addEventListener('click', focusOnTextIfCellOrPaddingAreClicked)
+
+    return () => {
+      froalaElementRef?.current?.removeEventListener('click', focusOnTextIfCellOrPaddingAreClicked)
+      tableCell?.removeEventListener('click', focusOnTextIfCellOrPaddingAreClicked)
+    }
+  }, [index, isCopyMode])
+}
