64f994127d40ed50926ec138b32b204a88e76cba
Anton Arbus <anton.arbus@macbookpro>
2023-07-14 10:26:40 +0300

    saveItemHeight on width change

--- client/components/Froala/useSetHeightBackToAuto.tsx
+++ client/components/Froala/useSetHeightBackToAuto.tsx
@@ -9,6 +9,8 @@ type TProps = {
 export function useSetHeightBackToAuto({ froalaElementRef }: TProps) {
   useEffectOnce(() => {
     setTimeout(() => {
+      // todo: now it acts weirdly
+      // todo: on animation completion go up and remove height for all froala elements
       froalaElementRef.current.style.removeProperty('height')
     }, 1000 * theme.item.animationDuration + 500)
   })
