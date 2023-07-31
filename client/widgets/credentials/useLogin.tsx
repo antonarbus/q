e73b1161874f796a84e6a13a173244fac7b06249
Anton Arbus <anton.arbus@macbookpro>
2023-07-31 23:00:13 +0300

    fixed some types

--- client/widgets/credentials/useLogin.tsx
+++ client/widgets/credentials/useLogin.tsx
@@ -1,22 +1,22 @@
 import { notify } from 'client/shared/ui/topMsg/notify'
 import { slideElement } from 'utils/slideElement'
 import { useDispatchTyped } from 'client/shared/hooks'
-import { Event } from 'client/types'
+import type { Event } from 'client/types'
 import { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { navUpdate } from './navUpdate'
 import { token } from '../../shared/auth/token'
-import { HttpStatusType } from './types'
+import type { HttpStatusType } from './types'
 import { rememberLoggedUser } from 'client/entities/user'
 
 export function useLogin() {
   const [httpStatus, setHttpStatus] = useState<HttpStatusType>('')
   const navigate = useNavigate()
   const location = useLocation()
-  const from = (location.state as any)?.from?.pathname || '/'
+  const from = location.state?.from?.pathname || '/'
   const dispatch = useDispatchTyped()
 
-  type Props = {
+  interface Props {
     e: Event
     email: string
     password: string
@@ -57,28 +57,28 @@ export function useLogin() {
         dispatch(rememberLoggedUser({ email, isLogged: true, roles }))
         // notify({ msg: 'Logged in!', theme: 'light', closeAfterMs: 3000 })
         navUpdate.login()
-        setTimeout(
-          () =>
-            slideElement({
-              element: cardElement,
-              cb: () => navigate(from, { replace: true }),
-            }),
-          2000
-        )
+        setTimeout(() => {
+          slideElement({
+            element: cardElement,
+            cb: () => {
+              navigate(from, { replace: true })
+            },
+          })
+        }, 2000)
       }
       console.log(data)
     } catch (err) {
       setHttpStatus('error')
       console.log(err)
       notify({ msg: 'Internal error', type: 'error', theme: 'light' })
-      setTimeout(
-        () =>
-          slideElement({
-            element: cardElement,
-            cb: () => navigate(from, { replace: true }),
-          }),
-        2000
-      )
+      setTimeout(() => {
+        slideElement({
+          element: cardElement,
+          cb: () => {
+            navigate(from, { replace: true })
+          },
+        })
+      }, 2000)
     }
   }
 
