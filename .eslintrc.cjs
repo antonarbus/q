b7f6959531363f9adb90766d69bfd325efd941da
Anton Arbus <anton.arbus@macbookpro>
2023-07-31 23:19:51 +0300

    tsconfigRootDir

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
   },
   rules: {
     'react-refresh/only-export-components': [
