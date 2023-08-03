f1651d6934b02e294bf7b922b66afb432465733d
Anton Arbus <anton.arbus@macbookpro>
2023-08-04 00:44:05 +0300

    some ts fixes

--- client/widgets/nav/NavList/NavList.tsx
+++ client/widgets/nav/NavList/NavList.tsx
@@ -1,12 +1,9 @@
-import type { TMenuItem } from 'client/entities/nav'
 import { NavItem } from './NavItem'
 import { Burger } from './NavItem/Burger'
 import { useSelectorTyped } from 'client/shared/hooks'
 
-export function NavList(): React.ReactNode {
+export function NavList(): JSX.Element {
   const navStructure = useSelectorTyped((state) => state.nav.navStructure)
-
-  if (navStructure[0] === undefined) return null
 
   return (
     <ul
@@ -16,11 +13,9 @@ export function NavList(): React.ReactNode {
         justifyContent: 'flex-end',
       }}
     >
-      {navStructure[0]
-        .menuItems?.filter((navItem) => !navItem.isHidden)
-        .map((navItem: TMenuItem) => (
-          <NavItem id={navItem.id} key={navItem.id} />
-        ))}
+      {navStructure[0]?.menuItems?.filter((navItem) => !navItem.isHidden).map((navItem) => (
+        <NavItem id={navItem.id} key={navItem.id} />
+      ))}
       <NavItem id={'burger'} key={'burger'}>
         <Burger />
       </NavItem>
