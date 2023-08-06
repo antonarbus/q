d0672697293ad9db7f882b394fc7e236eed0c251
Anton Arbus <anton.arbus@macbookpro>
2023-08-07 00:59:37 +0300

    moved DraggableItemsContainer

--- client/widgets/items/DraggableItemsContainer.tsx
+++ client/widgets/items/DraggableItemsContainer.tsx
@@ -5,7 +5,9 @@ import { SortableContainer } from 'react-sortable-hoc'
 
 // example with TypeScript
 // https://codesandbox.io/s/odfrontendeveloper-react-sortable-hoc-example-t96d8x?file=/src/examples/Items.tsx:518-635
-interface IProps { children: ReactNode }
+interface IProps {
+  children: ReactNode
+}
 interface ISortableContainer extends SortableContainerProps, IProps { }
 
 export const DraggableItemsContainer: React.ComponentClass<ISortableContainer> =
