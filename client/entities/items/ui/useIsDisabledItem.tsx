b2897eed2e7de1644886877cce3b98b0f11f76c5
Anton Arbus <anton.arbus@macbookpro>
2023-08-06 20:50:30 +0300

    renamed a bit

--- client/entities/items/ui/useIsDisabledItem.tsx
+++ client/entities/items/ui/useIsDisabledItem.tsx
@@ -1,7 +1,7 @@
 import { selectIsLastItem } from 'client/entities/items'
 import { useSelectorTyped } from 'client/shared/hooks'
 
-export const useIsDisabledItem = () => {
+export const useIsDisabledItem = (): boolean => {
   const isCopyMode = useSelectorTyped((state) => state.copy.isCopyMode)
   const isLastItem = useSelectorTyped(selectIsLastItem)
   const isDisabled = isCopyMode || isLastItem
