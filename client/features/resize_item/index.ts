aafe9d458cd8a7d927b5b8e01b501b457e83ab0a
Anton Arbus <anton.arbus@macbookpro>
2023-08-07 10:04:10 +0300

    onItemResizeStop

---
+++ client/features/resize_item/index.ts
@@ -0,0 +1,1 @@
+export { onItemResizeStop } from './onItemResizeStop'
---
+++ client/features/resize_item/onItemResizeStop.ts
@@ -0,0 +1,17 @@
+import { saveItemWidth } from 'client/entities/items'
+import { store } from 'client/shared/clients'
+import { saveItemsLocally } from 'client/shared/lib'
+import type { ResizeCallback } from 're-resizable'
+
+interface IProps {
+  index: number
+}
+
+type TResize = ResizeCallback | undefined
+
+export const onItemResizeStop = ({ index }: IProps): TResize =>
+  (e, direction, refToElement): void => {
+    const width = parseInt(refToElement.style.width)
+    store.dispatch(saveItemWidth({ index, width }))
+    saveItemsLocally({ msgAboveItemWithIndex: index })
+  }
