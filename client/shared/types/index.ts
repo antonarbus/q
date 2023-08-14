827361387586afb1c786753d3ba9432344471d13
Anton Arbus <anton.arbus@macbookpro>
2023-08-14 23:22:53 +0300

    removed some types

--- client/shared/types/index.ts
+++ client/shared/types/index.ts
@@ -8,10 +8,5 @@ export type { TCopyItem } from './Copy'
 
 export type { TOnFroalaContentChange } from './froala'
 
-import type { Resizable } from 're-resizable'
-
 export type Event = KeyboardEvent | MouseEvent | React.FormEvent | React.KeyboardEvent | React.MouseEvent
 export type RefDiv = React.MutableRefObject<HTMLDivElement>
-export type RefSpan = React.MutableRefObject<HTMLSpanElement>
-export type RefString = React.MutableRefObject<string>
-export type RefResizable = React.MutableRefObject<Resizable>
