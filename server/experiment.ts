c4437ddb3f71ad95b779f827a657ae11244537f8
Anton Arbus <anton.arbus@macbookpro>
2023-08-16 00:51:44 +0300

    TRouteHandler

--- server/experiment.ts
+++
@@ -1,3 +0,0 @@
-export default function showGreetings(greetings: any) {
-  return greetings?.birthday?.mom || 'no such property'
-}
--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,17 +1,9 @@
-import express, {
-  Request as ReqType,
-  Response as ResType,
-  NextFunction as NextType,
-} from 'express'
+import type { Request as ReqType, Response as ResType, NextFunction as NextType } from 'express'
+import express from 'express'
 
-export function errorHandler(
-  error: any,
-  req: ReqType,
-  res: ResType,
-  next: NextType
-) {
+export const errorHandler = (error: Error, req: ReqType, res: ResType, next: NextType): void => {
   console.log(error)
   const { message, name, stack } = error
-  res.json({ name, message, stack })
-  // save error into database
+  return void res.json({ name, message, stack })
+  // todo: save error into database
 }
