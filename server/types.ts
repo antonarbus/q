7834e3b3940be124f6d080069d3961117d0b4aa4
Anton Arbus <anton.arbus@macbookpro>
2023-08-16 19:06:20 +0300

    types for express

--- server/types.ts
+++ server/types.ts
@@ -1,6 +1,12 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { Query, Send } from 'express-serve-static-core'
 
+export type TReq = Request
+export type TRes = Response
+export type TNext = NextFunction
+
+export type TRouteHandler = (req: Request, res: Response, next: NextFunction) => void
+export type TRouteHandlerAsync = (req: Request, res: Response, next: NextFunction) => Promise<void>
 
 // https://plainenglish.io/blog/typed-express-request-and-response-with-typescript
 export interface TReqWithBody<TBodyObject> extends Request {
@@ -9,19 +15,13 @@ export interface TReqWithBody<TBodyObject> extends Request {
 export interface TReqWithQuery<TQueryObject extends Query> extends Request {
   query: TQueryObject
 }
+export type TReqExtended<TOtherProps> = Request & TOtherProps
 
 export interface TReqWithBodyAndQuery<T extends Query, U> extends Request {
   body: U,
   query: T
 }
 
-export interface TResWithBody<ResBody> extends Express.Response {
+export interface TResWithBody<ResBody> extends Response {
   json: Send<ResBody, this>;
 }
-
-export type TReq = Request
-export type TRes = Response
-export type TNext = NextFunction
-
-export type TRouteHandler = (req: Request, res: Response, next: NextFunction) => void
-export type TRouteHandlerAsync = (req: Request, res: Response, next: NextFunction) => Promise<void>
