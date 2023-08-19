8d813521fe321ca98128fa67b380a2cc67d729a5
Anton Arbus <anton.arbus@macbookpro>
2023-08-20 01:01:05 +0300

    didTextChange

--- client/features/on_item_text_content_change/onItemTextContentChange.ts
+++ client/features/on_item_text_content_change/onItemTextContentChange.ts
@@ -4,6 +4,11 @@ import { saveItemsLocally } from 'client/shared/lib'
 import type { TOnFroalaContentChange } from 'client/shared/types'
 
 export const onItemTextContentChange: TOnFroalaContentChange = ({ html, index, rowIndex }) => {
+  const item = store.getState().items[index]
+  if (item?.type !== 'text') return
+  const prevHtml = item.text.html
+  const didTextChange = prevHtml !== html
+  if (!didTextChange) return
   store.dispatch(saveText({ index, html, rowIndex }))
   saveItemsLocally({ msgAboveItemWithIndex: index })
 }
