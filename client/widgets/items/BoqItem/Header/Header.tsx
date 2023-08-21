55fe948b354090cc5fcadfb5dff77290d5d48cb8
Anton Arbus <anton.arbus@macbookpro>
2023-08-21 23:47:33 +0300

    comment

--- client/widgets/items/BoqItem/Header/Header.tsx
+++ client/widgets/items/BoqItem/Header/Header.tsx
@@ -8,6 +8,14 @@ interface Props {
   index: number
 }
 
+// todo: now we have a placeholder for price and currency
+// todo: what if we do not want currency to be shown at all
+// todo: if we delete it, we still have some space reserved, which is not good, it should disappear completely
+// todo: integrate currency in price directly and create a logic to update only price part
+// todo: something like I did in quotation.org originally
+
+// todo: also maybe we do not need placeholder for Title and Subtotal
+
 export const Header = ({ index }: Props): JSX.Element => {
   return (
     <BoqHeaderLayout
@@ -15,7 +23,6 @@ export const Header = ({ index }: Props): JSX.Element => {
       subtotalText={<SubtotalText index={index} />}
       price={<Price index={index} />}
       currency={<Currency index={index} />}
-      outlinedForDevPurposes
     />
   )
 }
