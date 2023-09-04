824097c43242fc4c26edb90da291e5f60545718e
Anton Arbus <anton.arbus@macbookpro>
2023-09-04 22:38:26 +0300

    default width

---
+++ client/shared/layouts/BoqTableLayout.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@mui/material'
+import type { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export const BoqTableLayout = ({ children }: Props): JSX.Element => {
+  return (
+    <Box
+      className='boq-table-container-with-paddings'
+      sx={{ p: '10px' }}
+    >
+      <Box
+        className='boq-table-container'
+        sx={{
+          overflow: 'auto',
+          '& *': {
+            background: '#ff00001b',
+          },
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  )
+}
