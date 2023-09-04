3bc5362d7955aa5344c4089df760c44d37d9c3f2
Anton Arbus <anton.arbus@macbookpro>
2023-09-04 12:18:37 +0300

    BoqRowLayout

--- client/widgets/items/BoqItem/Table/boq_rows/BoqRow.tsx
+++ client/widgets/items/BoqItem/Table/boq_rows/BoqRow.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@mui/material'
 import type { BoqRow as BoqRowType } from 'client/shared/types'
 import { IconsCell } from './IconsCell'
 import { NumberCell } from './NumberCell'
@@ -6,6 +5,7 @@ import { DescriptionCell } from './DescriptionCell'
 import { ItemCell } from './ItemCell'
 import { QtyCell } from './QtyCell'
 import { PriceCell } from './PriceCell'
+import { BoqRowLayout } from 'client/shared/layouts/BoqRowLayout'
 
 interface Props {
   index: number
@@ -15,21 +15,13 @@ interface Props {
 
 export const BoqRow = ({ boqRow, index, rowIndex }: Props): JSX.Element => {
   return (
-    <Box
-      className='tr'
-      sx={{
-        display: 'flex',
-        minHeight: '50px',
-        alignItems: 'flex-end',
-        position: 'relative',
-      }}
-    >
+    <BoqRowLayout>
       <IconsCell />
       <NumberCell index={index} boqRow={boqRow} rowIndex={rowIndex} />
       <DescriptionCell index={index} boqRow={boqRow} rowIndex={rowIndex} />
       <ItemCell index={index} boqRow={boqRow} rowIndex={rowIndex} />
       <QtyCell index={index} boqRow={boqRow} rowIndex={rowIndex} />
       <PriceCell index={index} boqRow={boqRow} rowIndex={rowIndex} />
-    </Box>
+    </BoqRowLayout>
   )
 }
