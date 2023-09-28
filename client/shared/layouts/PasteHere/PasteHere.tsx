d91d7179db2dba1cd3170a4e0b9f695ab5a4bc81
Anton Arbus <anton.arbus@macbookpro>
2023-09-29 00:14:45 +0300

    paste here

--- client/shared/layouts/PasteHere/PasteHere.tsx
+++ client/shared/layouts/PasteHere/PasteHere.tsx
@@ -32,7 +32,6 @@ export const PasteHere = ({ children, id }: Props): JSX.Element => {
             placeItems: 'center',
             zIndex: 2,
             userSelect: 'none',
-            // fontSize: '14px',
           }}
         >
           Paste here
