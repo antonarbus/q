2b62365c96ba7618102fefd1464283ade1054c39
Anton Arbus <anton.arbus@macbookpro>
2023-10-09 21:06:44 +0300

    paste text is shows when approach from bottom

--- client/features/move_paste_text/movePasteTextForBoqRow.ts
+++ client/features/move_paste_text/movePasteTextForBoqRow.ts
@@ -1,39 +1,50 @@
-import { dispatch, getState } from 'client/shared/clients'
 import isEqual from 'lodash.isequal'
+import { dispatch, getState } from 'client/shared/clients'
 import { copySlice } from 'client/entities/copy'
 import { className } from 'client/shared/className'
 import { itemsSlice } from 'client/entities/items'
 import { getPastePlace } from 'client/shared/lib'
+import { type BoqItem } from 'client/shared/types'
 
 export const movePasteTextForBoqRow = (e: MouseEvent): void => {
   if (!(e.target instanceof Element)) return
 
   const prevPlace = getState().copy.place
   const isPasteTextShown = getState().copy.isPasteTextShown
-
+  console.log('🚀  isPasteTextShown:', isPasteTextShown)
   const boqRowsElement = e.target.closest('.boq-rows')
-  console.log('🚀  boqRowsElement:', boqRowsElement)
+  const isBoqPasteItem = (getState().items
+    .filter(item => item.type === 'boq') as BoqItem[])
+    .flatMap(item => item.boq.rows)
+    .some(boqRow => boqRow.type === 'boq paste')
 
   if (!boqRowsElement) {
-    if (!isPasteTextShown) return
-    dispatch(copySlice.actions.hidePasteText())
-    dispatch(itemsSlice.actions.removePasteItem())
+    if (isPasteTextShown) {
+      dispatch(copySlice.actions.hidePasteText())
+    }
+    if (isBoqPasteItem) {
+      dispatch(itemsSlice.actions.removePasteItem())
+    }
     return
   }
 
   const elementsUnderCursor = document.elementsFromPoint(e.x, e.y)
-  const isOverActionsContainer = elementsUnderCursor.some(element => element.classList.contains(className.actionsContainer))
+  const isCursorOverActionsContainer = elementsUnderCursor.some(element => element.classList.contains(className.actionsContainer))
 
-  if (isOverActionsContainer && isPasteTextShown) {
-    dispatch(copySlice.actions.hidePasteText())
-    dispatch(itemsSlice.actions.removePasteItem())
+  if (isCursorOverActionsContainer && isPasteTextShown) {
+    if (isPasteTextShown) {
+      dispatch(copySlice.actions.hidePasteText())
+    }
+    if (isBoqPasteItem) {
+      dispatch(itemsSlice.actions.removePasteItem())
+    }
     return
   }
 
-  if (!isOverActionsContainer && !isPasteTextShown) {
-    dispatch(copySlice.actions.showPasteText())
-    return
-  }
+  // if (!isCursorOverActionsContainer && !isPasteTextShown) {
+  //   dispatch(copySlice.actions.showPasteText())
+  //   return
+  // }
 
   const isPastable = getState().copy.isPastable
 
@@ -44,11 +55,13 @@ export const movePasteTextForBoqRow = (e: MouseEvent): void => {
   }
 
   const boqRowElement = e.target.closest(`.${className.boqRow}`)
+
   if (!boqRowElement) return
 
   const pastePlace = getPastePlace({ item: boqRowElement, e, distanceToEdge: 10 })
+  console.log('🚀  pastePlace:', pastePlace)
 
-  if (isEqual(pastePlace, prevPlace)) return
+  if (isEqual(pastePlace, prevPlace) && isPasteTextShown) return
 
   dispatch(copySlice.actions.updatePastePos(pastePlace))
   dispatch(copySlice.actions.showPasteText())
