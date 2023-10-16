39d8b763c66377e5bf2bd5b84b8c241330a1186b
Anton Arbus <anton.arbus@macbookpro>
2023-10-17 01:06:49 +0300

    passiveSupport

--- client/shared/lib/nonPassiveEventErrorFix.ts
+++ client/shared/lib/nonPassiveEventErrorFix.ts
@@ -1,5 +1,7 @@
 import { passiveSupport } from 'passive-events-support/src/utils'
 
+// https://stackoverflow.com/questions/59282213/react-warning-non-passive-event-listener-to-a-scroll-blocking-touchstart
+
 passiveSupport({
   debug: false,
   listeners: [
@@ -10,6 +12,15 @@ passiveSupport({
     },
     {
       element: '#items',
+      event: 'touchmove',
+    },
+    {
+      element: '#boq-rows',
+      event: 'touchstart',
+      // prevented: true, // (optional) will force { passive: false }
+    },
+    {
+      element: '#boq-rows',
       event: 'touchmove',
     },
   ],
