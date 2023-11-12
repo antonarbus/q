688ab2ad1e3bc151b09422bbbf25af509aa5b01c
Anton Arbus <anton.arbus@macbookpro>
2023-11-12 12:59:27 +0200

    froala for other cells

--- client/widgets/items/boq/table/rows/row/cells/ItemCell.tsx
+++ client/widgets/items/boq/table/rows/row/cells/ItemCell.tsx
@@ -1,7 +1,11 @@
 import { Box } from '@mui/material'
-import { selectColumnWidth } from 'client/entities/items'
+import { boqCellHtmlGetter, selectColumnWidth } from 'client/entities/items'
+import { changeBoqCell } from 'client/features/change_text'
 import { useSelectorTyped } from 'client/shared/hooks'
 import type { BoqRow } from 'client/shared/types'
+import { Froala } from 'client/shared/ui/froala'
+import type FroalaEditor from 'froala-editor'
+import { useRef } from 'react'
 
 type Props = {
   itemIndex: number
@@ -12,6 +16,8 @@ type Props = {
 const boqColumnKey = 'item'
 
 export const ItemCell = ({ itemIndex, boqRow, rowIndex }: Props): JSX.Element => {
+  const froalaElementRef = useRef<HTMLDivElement>(null)
+  const editorRef = useRef<FroalaEditor | null>(null)
   const itemColWidth = useSelectorTyped(selectColumnWidth({ itemIndex, boqColKey: boqColumnKey }))
   const isItemColWidthSetManually = itemColWidth !== undefined
 
@@ -26,7 +32,21 @@ export const ItemCell = ({ itemIndex, boqRow, rowIndex }: Props): JSX.Element =>
         minWidth: '100px',
       }}
     >
-      {boqRow[boqColumnKey].html}
+      <Froala
+        itemIndex={itemIndex}
+        editorRef={editorRef}
+        froalaElementRef={froalaElementRef}
+        placeholder={boqColumnKey + '...'}
+        initHtmlGetter={() => boqCellHtmlGetter({ itemIndex, rowIndex, boqColumnKey })}
+        onContentChange={() => {
+          if (editorRef.current === null) return
+          const html = editorRef.current.html.get()
+          changeBoqCell({ itemIndex, rowIndex, boqColumnKey, html })
+        }}
+        additionalStyle={{
+          flexGrow: 1,
+        }}
+      />
     </Box>
   )
 }
