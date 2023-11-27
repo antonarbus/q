31da868ea4ec4070ee97ef733f1d45e113e0b358
Anton Arbus <anton.arbus@macbookpro>
2023-11-27 22:09:50 +0200

    better resetItems

--- client/features/reset_items/resetItems.ts
+++ client/features/reset_items/resetItems.ts
@@ -1,10 +1,15 @@
-import { dispatch } from 'client/shared/clients'
+import { dispatch, theme } from 'client/shared/clients'
 import { defaultItems, itemsSlice } from 'client/entities/items'
 import { offerSlice } from 'client/entities/offer'
 import { saveItemsLocally } from 'client/shared/lib'
+import { copySlice } from 'client/entities/copy'
 
 export const resetItems = (): void => {
+  dispatch(copySlice.actions.enterIntoCopyMode())
   saveItemsLocally({ items: defaultItems })
   dispatch(itemsSlice.actions.resetItemsToDefault())
   dispatch(offerSlice.actions.reloadOffer())
+  setTimeout(() => {
+    dispatch(copySlice.actions.exitFromCopyMode())
+  }, 1000 * theme.item.animationDuration)
 }
