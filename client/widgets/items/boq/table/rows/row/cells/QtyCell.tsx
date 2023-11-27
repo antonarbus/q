4058eb96e2149e288b974e30a3c199cbbf1215c5
Anton Arbus <anton.arbus@macbookpro>
2023-11-27 20:17:10 +0200

    placeholder position

--- client/widgets/items/boq/table/rows/row/cells/QtyCell.tsx
+++ client/widgets/items/boq/table/rows/row/cells/QtyCell.tsx
@@ -46,6 +46,9 @@ export const QtyCell = ({ itemIndex, rowIndex }: Props): JSX.Element => {
         additionalStyle={{
           flexGrow: 1,
           textAlign: 'center',
+          '.fr-placeholder': {
+            left: '15px',
+          },
         }}
       />
     </Box>
