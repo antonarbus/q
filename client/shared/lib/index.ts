5458423750f153df02b86e227217f35e3b087654
Anton Arbus <anton.arbus@macbookpro>
2023-12-26 19:01:39 +0200

    getNumberFromString

--- client/shared/lib/index.ts
+++ client/shared/lib/index.ts
@@ -1,3 +1,4 @@
 export { saveItemsLocally } from './save_items_locally/saveItemsLocally'
 export { getPastePlace } from './getPastePlace'
 export { getTextContentFromHtml } from './getTextContentFromHtml'
+export { getNumberFromString } from './getNumberFromString'
