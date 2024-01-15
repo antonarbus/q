41ed010d82a2f8e72f768bed6c9fb27f008eac92
Anton Arbus <anton.arbus@macbookpro>
2024-01-16 01:02:35 +0200

    hook for styles

--- client/widgets/items/boq/table/rows/row/cells/NumberCell.tsx
+++ client/widgets/items/boq/table/rows/row/cells/NumberCell.tsx
@@ -1,30 +1,20 @@
-import { Box } from '@mui/material'
-import { getNumberOfBoqItemsAbove, selectColumnWidth, useItem, useRow } from 'client/entities/items'
-import { useSelectorTyped } from 'client/shared/hooks'
+import { getNumberOfBoqItemsAbove, useItem, useRow } from 'client/entities/items'
+import { useStylesForResizableCell } from './useStylesForResizableCell'
 import type { BoqColumnKey } from 'client/shared/types'
+import { Box } from '@mui/material'
 
 const boqColumnKey: BoqColumnKey = 'number'
 
 export const NumberCell = (): JSX.Element => {
   const { itemIndex } = useItem()
   const { rowIndex } = useRow()
-  const numberColWidth = useSelectorTyped(selectColumnWidth({ itemIndex, boqColumnKey }))
-  const isNumberColWidthSetManually = numberColWidth !== undefined
-  const width = isNumberColWidthSetManually ? numberColWidth : 'auto'
-  const minWidth = '30px'
-  const maxWidth = width === 'auto' ? minWidth : width
+  const { stylesForResizableCell } = useStylesForResizableCell({ itemIndex, boqColumnKey, minWidth: '30px' })
 
   return (
     <Box
       className={`td ${boqColumnKey}`}
       sx={{
-        display: isNumberColWidthSetManually ? 'block' : 'flex',
-        position: 'relative',
-        flexGrow: 0,
-        flexShrink: 0,
-        width,
-        maxWidth,
-        minWidth,
+        ...stylesForResizableCell,
         fontSize: '10px',
         color: 'grey',
         paddingBottom: '6px',
