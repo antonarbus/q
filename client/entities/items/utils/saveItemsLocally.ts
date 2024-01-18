b360c299a8c1d920d6b70dd7d4bee29a62b2ee50
Anton Arbus <anton.arbus@macbookpro>
2024-01-18 10:12:46 +0200

    6 fsd issues left

--- client/entities/items/utils/saveItemsLocally.ts
+++ client/entities/items/utils/saveItemsLocally.ts
@@ -1,6 +1,5 @@
 import { tellItemsSavedLocally } from '@entities/bottom_msg' // todo: not good
 import { dispatch, getState } from '@shared/clients'
-import { saveItemsIntoLocalStorage } from '@shared/lib/saveItemsIntoLocalStorage'
 import type { Item } from '@shared/types'
 import { itemsSlice } from '../redux/itemsSlice'
 
@@ -13,8 +12,9 @@ export const saveItemsLocally = ({
   items = getState().items,
   msgAboveItemWithIndex,
 }: Props = {}): void => {
-  saveItemsIntoLocalStorage({ items })
+  localStorage.setItem('items', JSON.stringify(items))
   tellItemsSavedLocally()
+
   if (msgAboveItemWithIndex !== undefined) {
     dispatch(itemsSlice.actions.tellItemSavedLocallyReducer({ itemIndex: msgAboveItemWithIndex }))
   }
