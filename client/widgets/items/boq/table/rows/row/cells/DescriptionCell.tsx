ee8fe259c79b3f363a681b506516c37d31efeae5
Anton Arbus <anton.arbus@macbookpro>
2024-01-22 00:45:49 +0200

    something working

--- client/widgets/items/boq/table/rows/row/cells/DescriptionCell.tsx
+++ client/widgets/items/boq/table/rows/row/cells/DescriptionCell.tsx
@@ -1,4 +1,3 @@
-import { Box } from '@mui/material'
 import type FroalaEditor from 'froala-editor'
 import { useRef } from 'react'
 import { updateDescriptionCell } from '@features/update_cell'
@@ -14,28 +13,16 @@ export const DescriptionCell = (): JSX.Element => {
   const { stylesForResizableCell } = useStylesForResizableCell({ itemIndex, boqColumnKey, minWidth: '200px' })
 
   return (
-    <Box
-    className={`td ${boqColumnKey}`}
-    sx={{ ...stylesForResizableCell, background: 'yellow' }}
-    onClick={() => {
-      // todo: to set cursor and select on dbClick we need to put also
-      // todo: descriptionCellEditorRef into RowProvider
-      // todo: also we may need to remove that logic from froala, maybe just move those hooks here
-      console.log(666)
-    }}
-    >
       <Froala
+        className={`td ${boqColumnKey}`}
         editorRef={editorRef}
         placeholder='Description...'
         htmlGetter={() => getBoqCellHtmlFromStore({ itemIndex, rowIndex, boqColumnKey })}
         onContentChange={() => {
           updateDescriptionCell({ editorRef, itemIndex, rowIndex, boqColumnKey })
         }}
-        onClick={() => {
-          console.log(555)
-        }}
-        additionalStyle={{ ...descriptionCellStyle, background: 'red' }}
+        wrapperStyles={stylesForResizableCell}
+        additionalStyle={descriptionCellStyle}
       />
-    </Box>
   )
 }
