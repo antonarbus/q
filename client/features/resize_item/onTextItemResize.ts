03332a7a2e73d93dc750611fff63369db44b5e6e
Anton Arbus <anton.arbus@macbookpro>
2024-01-27 21:39:54 +0200

    no froala disable on text resize

--- client/features/resize_item/onTextItemResize.ts
+++ client/features/resize_item/onTextItemResize.ts
@@ -3,20 +3,20 @@ import { itemsSlice, saveItemsLocally } from '@entities/items'
 import type { OnItemResize, OnItemResizeStart, OnItemResizeStop } from '@shared/types'
 
 export const onTextItemResizeStart: OnItemResizeStart = ({ itemIndex, e, dir, elementRef }) => {
-  dispatch(itemsSlice.actions.disableFroalaReducer({ itemIndex }))
+  // dispatch(itemsSlice.actions.disableFroalaReducer({ itemIndex }))
 }
 
 export const onTextItemResize: OnItemResize = ({ itemIndex, e, direction, elementRef, delta }) => {
-  const width = parseInt(elementRef.style.width)
-  const prevItemWidth = getState().items[itemIndex]?.width
-  if (width === prevItemWidth) return
-  dispatch(itemsSlice.actions.updateItemWidthReducer({ itemIndex, width }))
+  // const width = parseInt(elementRef.style.width)
+  // const prevItemWidth = getState().items[itemIndex]?.width
+  // if (width === prevItemWidth) return
+  // dispatch(itemsSlice.actions.updateItemWidthReducer({ itemIndex, width }))
 }
 
 export const onTextItemResizeStop: OnItemResizeStop = ({ itemIndex, e, direction, elementRef, delta }) => {
   const width = parseInt(elementRef.style.width)
   const prevItemWidth = getState().items[itemIndex]?.width
-  dispatch(itemsSlice.actions.enableFroalaReducer({ itemIndex }))
+  // dispatch(itemsSlice.actions.enableFroalaReducer({ itemIndex }))
 
   if (width === prevItemWidth) return
 
