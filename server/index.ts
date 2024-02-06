b88f6f3008b5e1c319fd316cb791634032a69648
Anton Arbus <anton.arbus@macbookpro>
2024-02-06 03:20:25 +0200

    upload files into the bucket

--- server/index.ts
+++ server/index.ts
@@ -3,18 +3,22 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
+import multer from 'multer'
 import { activateRouter } from './api/activateRouter'
 import { hiRouter } from './api/hiRouter'
 import { loginRouter } from './api/loginRouter'
 import { logoutRouter } from './api/logoutRouter'
 import { refreshRouter } from './api/refreshRouter'
 import { registerRouter } from './api/registerRouter'
+import { uploadRouter } from './api/uploadRouter'
 import { userEmailRouter } from './api/userEmailRouter'
 import { usersRouter } from './api/usersRouter'
 import { apiUrl } from './apiUrls'
 import { connectToDb } from './db/connectToDb'
 import { errorHandlerMiddleware } from './middleware/errorHandlerMiddleware'
 import type { Req, Res } from './types'
+
+const multerMid = multer({ storage: multer.memoryStorage(), limits: { fileSize: 20 * 1024 * 1024 } })
 
 void (async (): Promise<void> => {
   const app = express()
@@ -24,6 +28,8 @@ void (async (): Promise<void> => {
   app.use(express.json()) // parses incoming requests with JSON because we use lots of json, let it be default
   app.use(cookieParser())
   app.use(cors())
+  app.use(multerMid.single('file'))
+  app.set('trust proxy', true) // for app engine
 
   app.get(apiUrl.root, (_req: Req, res: Res) => res.send('i am express.js'))
   app.get(apiUrl.api, (_req: Req, res: Res) => res.json({ message: '/api' }))
@@ -35,6 +41,7 @@ void (async (): Promise<void> => {
   app.use(apiUrl.refresh, refreshRouter)
   app.use(apiUrl.users, usersRouter)
   app.use(apiUrl.user, userEmailRouter)
+  app.use(apiUrl.upload, uploadRouter)
 
   app.use(errorHandlerMiddleware)
 
