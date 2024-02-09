2a1ba104f2ca2819e02f826d71e57954655d0226
Anton Arbus <anton.arbus@macbookpro>
2024-02-09 04:04:58 +0200

    froala 3 works

--- public/froala.js
+++ public/froala.js
@@ -1,11 +1,3 @@
-/*!
- * froala_editor v3.2.0 (https://www.froala.com/wysiwyg-editor)
- * License https://froala.com/wysiwyg-editor/terms/
- * Copyright 2014-2020 Froala Labs
- */
-
-console.log(666)
-
 !function(e, t) {
   "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : e.FroalaEditor = t()
 }(this, function() {
