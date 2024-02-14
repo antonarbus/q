bb4502d4f326a3e85d8a67aa67a2ba44207d9953
Anton Arbus <anton.arbus@macbookpro>
2024-02-14 20:56:31 +0200

    better pdf

--- client/features/download_pdf/downloadPdf.ts
+++ client/features/download_pdf/downloadPdf.ts
@@ -1,38 +1,36 @@
 import { dispatch } from '@lib_instances/store'
-import html2canvas from 'html2canvas'
+import { domToPng } from 'modern-screenshot'
 import { navSlice } from '@entities/nav'
 import { className } from '@shared/consts/className'
 
 export const downloadPdf = async (): Promise<void> => {
   dispatch(navSlice.actions.showLoadingIcon({ id: 'pdf' }))
+
   const itemsElement = document.querySelector(`.${className.items}`)
   if (!(itemsElement instanceof HTMLElement)) return
-  const worker = new Worker(new URL('./pdfWorker', import.meta.url), {
-    type: 'module',
-  })
 
-  const width = itemsElement.clientWidth
-  const height = itemsElement.clientHeight
-  const canvas = await html2canvas(itemsElement, {
-    allowTaint: true,
-    useCORS: true,
-    ignoreElements: (element) => {
-      if (element.classList.contains(className.actionsContainer)) return true
-      return false
-    },
-    onclone: (document: Document, element: HTMLElement) => {
-      const paperElements = element.querySelectorAll(`.${className.paper}`)
-      paperElements.forEach(paperElement => {
-        if (!(paperElement instanceof HTMLElement)) return
-        paperElement.style.border = '1px solid grey'
+  const screenshot = await domToPng(itemsElement, {
+    backgroundColor: 'grey',
+    onCloneNode: (node) => {
+      if (!(node instanceof HTMLElement)) return
+      const actionsElements = node.querySelectorAll(`.${className.actionsContainer}`)
+      actionsElements.forEach((element) => {
+        element.remove()
       })
     },
   })
-  const base64image = canvas.toDataURL('image/png')
-  worker.postMessage({ base64image, width, height })
+
+  const worker = new Worker(new URL('./pdfWorker', import.meta.url), { type: 'module' })
+
+  worker.postMessage({
+    imageData: screenshot,
+    width: itemsElement.clientWidth,
+    height: itemsElement.clientHeight,
+  })
 
   worker.onmessage = function (event: MessageEvent<Blob>) {
-    const pdfDataUrl = URL.createObjectURL(event.data)
+    const blobImage = event.data
+    const pdfDataUrl = URL.createObjectURL(blobImage)
     const downloadLink = document.createElement('a')
     downloadLink.href = pdfDataUrl
     downloadLink.download = 'quotation.pdf'
@@ -40,6 +38,7 @@ export const downloadPdf = async (): Promise<void> => {
     downloadLink.click()
     document.body.removeChild(downloadLink)
     URL.revokeObjectURL(pdfDataUrl) // Revoke the data URL to free up resources
+
     dispatch(navSlice.actions.hideLoadingIcon({ id: 'pdf' }))
   }
 }
--- client/features/download_pdf/pdfWorker.ts
+++ client/features/download_pdf/pdfWorker.ts
@@ -1,25 +1,18 @@
 import { jsPDF } from 'jspdf'
 
-type Props = {
-  base64image: string
+self.onmessage = async function (event: MessageEvent<{
+  imageData: string
   width: number
   height: number
-}
-
-self.onmessage = async function (event) {
-  const { base64image, width, height } = event.data
-  const result = performImageProcessing({ base64image, width, height })
-  self.postMessage(result)
-}
-
-function performImageProcessing({ base64image, width, height }: Props): Blob {
+}>) {
+  const { imageData, width, height } = event.data
   // eslint-disable-next-line new-cap
   const pdf = new jsPDF({
     orientation: width > height ? 'landscape' : 'portrait',
     unit: 'px',
     format: [width, height],
   })
-  pdf.addImage(base64image, 'PNG', 0, 0, width, height, undefined, 'FAST')
+  pdf.addImage(imageData, 'PNG', 0, 0, width, height, undefined, 'FAST')
   const blob = pdf.output('blob')
-  return blob
+  self.postMessage(blob)
 }
