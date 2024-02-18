126c338ba907c88be6d2f56d9cf7bc7764c8c82d
Anton Arbus <anton.arbus@macbookpro>
2024-02-18 22:04:15 +0200

    main layout

--- client/app/Main.tsx
+++ client/app/Main.tsx
@@ -1,3 +1,4 @@
+import { type ReactNode } from 'react'
 import { Outlet, Route, Routes } from 'react-router-dom'
 import { Offer } from '@pages/offer'
 import { useRefreshTokens } from '@widgets/credentials'
@@ -6,18 +7,31 @@ export const Main = (): JSX.Element => {
   useRefreshTokens({ withLoadingState: false })
 
   return (
+    <MainLayout>
+      <Outlet />
+      <Routes>
+        <Route path='/*' element={<Offer />} />
+        <Route path='dev' element={<div>dev</div>} />
+      </Routes>
+    </MainLayout>
+  )
+}
+
+type Props = {
+  children: ReactNode
+}
+
+function MainLayout ({ children }: Props): JSX.Element {
+  return (
     <main
       css={{
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         padding: '10px 0px 10px 10px',
-      }}>
-      <Outlet />
-      <Routes>
-        <Route path='/*' element={<Offer />} />
-        <Route path='dev' element={<div>dev</div>} />
-      </Routes>
+      }}
+    >
+      {children}
     </main>
   )
 }
