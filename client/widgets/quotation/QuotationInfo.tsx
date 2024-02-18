a15af990405c63d447a009f0a6344160df32ecda
Anton Arbus <anton.arbus@macbookpro>
2024-02-18 20:19:54 +0200

    QuotationInfoLayout

--- client/widgets/quotation/QuotationInfo.tsx
+++ client/widgets/quotation/QuotationInfo.tsx
@@ -1,50 +1,15 @@
 import { useSelectorTyped } from '@lib_instances/store'
-import { Box } from '@mui/material'
-import { FaInfoCircle } from 'react-icons/fa'
+import { QuotationInfoLayout } from './QuotationInfoLayout'
 
 export const QuotationInfo = (): JSX.Element => {
   const quotationId = useSelectorTyped(state => state.quotation.id)
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        fontWeight: 600,
-        color: 'grey',
-        paddingRight: '15px',
-        height: '20px',
-        translate: '0px 5px',
+    <QuotationInfoLayout
+      quotationId={quotationId}
+      onClick={() => {
+        alert('show popup with all info with proper route')
       }}
-    >
-      <Box
-        component='button'
-        onClick={() => {
-          alert('show popup with all info with proper route')
-        }}
-        sx={{
-          all: 'unset',
-          display: 'flex',
-          alignItems: 'center',
-          cursor: 'pointer',
-          gap: '5px',
-        }}
-      >
-        <FaInfoCircle
-          css={{
-            fill: '#6488cf',
-          }}
-        />
-        <span
-          css={{
-            fontWeight: 400,
-          }}
-        >
-          id:
-        </span>
-        {quotationId}
-      </Box>
-    </Box>
+    />
   )
 }
