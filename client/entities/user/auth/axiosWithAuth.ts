5ab31920ca9c7e6616e3329b9c34675a2c1ca851
Anton Arbus <anton.arbus@macbookpro>
2024-02-21 22:56:01 +0200

    comments at axiosWithAuth

--- client/entities/user/auth/axiosWithAuth.ts
+++ client/entities/user/auth/axiosWithAuth.ts
@@ -1,9 +1,16 @@
 import { dispatch, getState } from '@lib_instances/store'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { apiUrl } from 'server/consts/apiUrl'
 import { headerName } from 'server/consts/headerName'
 import { userSlice } from '../redux/userSlice'
 import { token } from './token'
+
+//* add headers to the request headers
+//* protected routes have a middleware which checks the token against db
+//* and throw 401 error response if token is not valid
+//* if error is thrown for the 1st time the interceptor bellow will try to refresh the token as it may be simply expired
+//* then it will try to make original call again with refreshed token (if it is refreshed)
+//* and if it has again 401 status then we need to show the login form
 
 export const axiosWithAuth = axios.create({ withCredentials: true })
 
@@ -27,34 +34,27 @@ axiosWithAuth.interceptors.response.use(
       originalRequest._isRetry = true
       try {
         const response = await axios.get(apiUrl.refresh, { withCredentials: true })
-        const { accessJwtToken, email } = response.data
+        const { accessJwtToken } = response.data
 
         if (accessJwtToken) {
           token.access = accessJwtToken
         }
 
-        // if (!accessJwtToken) {
-        //   console.log(777)
-        //   token.access = ''
-        //   dispatch(userSlice.actions.forgetLoggedUser())
-        // }
-
         // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
         return await axiosWithAuth.request(originalRequest)
-      } catch (err) {
-        token.access = null
-        if (getState().user.isLogged) {
-          dispatch(userSlice.actions.forgetLoggedUser())
+      } catch (err: unknown) {
+        if (err instanceof AxiosError && err.response?.status === 401) {
+          if (getState().user.isLogged) {
+            dispatch(userSlice.actions.forgetLoggedUser())
+          }
+          token.access = null
+          console.log('show login window')
+          console.warn('not authorized')
+          console.error(err)
+          // todo: show login window
         }
-        console.warn('not authorized')
-        console.error(err)
       }
     }
-
-    // if (error.response.status === 401) {
-    //   dispatch(userSlice.actions.forgetLoggedUser())
-    //   // todo: navigate to login route
-    // }
 
     throw error
   },
