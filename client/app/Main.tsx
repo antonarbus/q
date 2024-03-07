75f3c1f44d262caf5079cc7b613f3bb268695044
Anton Arbus <anton.arbus@macbookpro>
2024-03-07 06:59:21 +0200

    put back

--- client/app/Main.tsx
+++ client/app/Main.tsx
@@ -1,37 +1,8 @@
-import { dispatch } from '@lib_instances/store'
-import { useEffect, type ReactNode } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import { type ReactNode } from 'react'
+import { Outlet } from 'react-router-dom'
 import { Offer } from '@pages/offer'
-import { itemsSlice } from '@entities/items'
-import { type Item } from '@entities/items/types'
-import { type Quotation, quotationSignal } from '@entities/quotation'
-import { localStorageKey } from '@shared/consts/localStorageKey'
-import { jsonParseSafe } from '@shared/lib/jsonParseSafe'
 
 export const Main = (): JSX.Element => {
-  const { pathname } = useLocation()
-  useEffect(() => {
-    if (pathname !== '/') return
-    const itemsFromLocalStorage = localStorage.getItem(localStorageKey.items)
-    const quotationFromLocalStorage = localStorage.getItem(localStorageKey.quotation)
-
-    if (itemsFromLocalStorage !== null && quotationFromLocalStorage !== null) {
-      const items = jsonParseSafe<Item[]>(itemsFromLocalStorage)
-      if (items !== undefined) {
-        dispatch(itemsSlice.actions.loadItemsReducer({ items }))
-      }
-
-      const quotation = jsonParseSafe<Quotation>(quotationFromLocalStorage)
-      if (quotation !== undefined) {
-        quotationSignal.value = quotation
-      }
-
-      // if (quotation?.id !== id) {
-      //   setEnabled(true)
-      // }
-    }
-  }, [pathname])
-
   return (
     <MainLayout>
       <Offer />
