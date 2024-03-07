0550166101b9e32d4b89fefce6292b0c2a509659
Anton Arbus <anton.arbus@macbookpro>
2024-03-08 01:16:30 +0200

    tsc issue

--- client/pages/quotations/quotationsAgGridRef.ts
+++ client/pages/quotations/quotationsAgGridRef.ts
@@ -1,6 +1,7 @@
+import { type QuotationModelType } from '@server/db/models/quotationModel'
 import { type AgGridReact } from 'ag-grid-react'
 import { type RefObject } from 'react'
 
-export const quotationsAgGridRef: RefObject<AgGridReact<QuotationsTable>> = {
+export const quotationsAgGridRef: RefObject<AgGridReact<QuotationModelType>> = {
   current: null,
 }
