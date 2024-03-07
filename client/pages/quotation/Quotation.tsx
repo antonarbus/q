7beafb93d0a8effb2b8eaafa6ac08ccaeef059cf
Anton Arbus <anton.arbus@macbookpro>
2024-03-08 01:15:00 +0200

    loading logic

--- client/pages/quotation/Quotation.tsx
+++ client/pages/quotation/Quotation.tsx
@@ -1,7 +1,10 @@
 import { Items } from '@widgets/items'
 import { QuotationInfo } from '@widgets/quotation'
+import { useFetchQuotation } from '@entities/quotation'
 
 export const Quotation = (): JSX.Element => {
+  useFetchQuotation()
+
   return (
     <>
       <QuotationInfo />
