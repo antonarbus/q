1a81df353bb962bf6206a884d902b0abed883dc3
Anton Arbus <anton.arbus@macbookpro>
2024-03-07 06:57:47 +0200

    smth

--- client/app/Main.tsx
+++ client/app/Main.tsx
@@ -1,24 +1,48 @@
-import { type ReactNode } from 'react'
-import { Outlet, Route, Routes } from 'react-router-dom'
+import { dispatch } from '@lib_instances/store'
+import { useEffect, type ReactNode } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
+import { useEffectOnce } from 'react-use'
 import { Offer } from '@pages/offer'
+import { itemsSlice } from '@entities/items'
+import { type Item } from '@entities/items/types'
+import { type Quotation, quotationSignal } from '@entities/quotation'
+import { localStorageKey } from '@shared/consts/localStorageKey'
+import { jsonParseSafe } from '@shared/lib/jsonParseSafe'
 
 export const Main = (): JSX.Element => {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    if (pathname !== '/') return
+    console.log(666)
+    const itemsFromLocalStorage = localStorage.getItem(localStorageKey.items)
+    const quotationFromLocalStorage = localStorage.getItem(localStorageKey.quotation)
+
+    if (itemsFromLocalStorage !== null && quotationFromLocalStorage !== null) {
+      const items = jsonParseSafe<Item[]>(itemsFromLocalStorage)
+      if (items !== undefined) {
+        dispatch(itemsSlice.actions.loadItemsReducer({ items }))
+      }
+
+      const quotation = jsonParseSafe<Quotation>(quotationFromLocalStorage)
+      if (quotation !== undefined) {
+        quotationSignal.value = quotation
+      }
+
+      // if (quotation?.id !== id) {
+      //   setEnabled(true)
+      // }
+    }
+  }, [pathname])
+
   return (
     <MainLayout>
       <Offer />
       <Outlet />
-      <Routes>
-        <Route path='dev' element={<div>dev</div>} />
-      </Routes>
     </MainLayout>
   )
 }
 
-type Props = {
-  children: ReactNode
-}
-
-function MainLayout ({ children }: Props): JSX.Element {
+function MainLayout ({ children }: { children: ReactNode }): JSX.Element {
   return (
     <main
       css={{
