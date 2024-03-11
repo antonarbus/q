bafe2f23d7db68d6925e5011d7af400f4e0316f0
Anton Arbus <anton.arbus@macbookpro>
2024-03-11 02:44:22 +0200

    style

--- client/entities/quotation/signals/quotationSignal.ts
+++ client/entities/quotation/signals/quotationSignal.ts
@@ -1,6 +1,4 @@
 import { type Signal, signal } from '@preact/signals-react'
 import { type Quotation } from '../types'
 
-export const quotationSignal: Signal<Quotation> = signal({
-  id: 'local version',
-})
+export const quotationSignal: Signal<Quotation> = signal({ id: 'local version' })
