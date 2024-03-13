9e1390f9513c54f517452b3626af48ce6dce8090
Anton Arbus <anton.arbus@macbookpro>
2024-03-13 10:46:31 +0200

    console.trace

--- server/api/saveQuotationRouter.ts
+++ server/api/saveQuotationRouter.ts
@@ -1,5 +1,6 @@
 import { QuotationModel, type QuotationModelType } from '@server/db/models/quotationModel'
 import { verifyTokenMiddleware } from '@server/middleware/verifyTokenMiddleware'
+import { type JwtPayloadExtended, verifyRefreshToken } from '@server/services/jwt'
 import { bucket } from '@server/services/storage'
 import { Router } from 'express'
 import { type HydratedDocument } from 'mongoose'
@@ -14,7 +15,7 @@ export type ReqBody = {
 
 export type ResBody = {
   message: string
-  document: HydratedDocument<QuotationModelType> | null
+  document?: HydratedDocument<QuotationModelType> | null
 }
 
 type RouterHandler = (req: ReqWithBody<ReqBody>, res: ResWithBody<ResBody>, next: Next) => Promise<ResWithBody<ResBody> | undefined>
@@ -23,8 +24,23 @@ export const saveQuotationRouter = Router()
 
 const saveQuotation: RouterHandler = async (req, res, next) => {
   try {
+    const refreshJwtToken = req.cookies.refreshJwtToken
+
+    if (typeof refreshJwtToken !== 'string') {
+      return res.status(200).json({
+        message: 'not logged in',
+      })
+    }
+
+    const { email } = verifyRefreshToken(refreshJwtToken) as JwtPayloadExtended
+
+    if (!email) {
+      return res.status(200).json({
+        message: 'not logged in',
+      })
+    }
+
     const { items, quotation, id } = req.body
-    const email = req.headers.email
 
     const filter = { email, id }
     const update = { quotation }
