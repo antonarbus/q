8454b23812cacde20cc3a718ab86c9010b64f9ff
Anton Arbus <anton.arbus@macbookpro>
2024-03-16 00:17:00 +0200

    saveQuotationFn

--- client/features/save_quotation/saveQuotation.ts
+++ client/features/save_quotation/saveQuotation.ts
@@ -1,17 +1,12 @@
 import { reactQuery } from '@lib_instances/reactQuery'
 import { dispatch, getState } from '@lib_instances/store'
 import { type ResBody as ResBodyQuotations } from '@server/api/getQuotationsRouter'
-import { type AxiosResponse } from 'axios'
 import { produce } from 'immer'
-import { type ResBody, type ReqBody } from 'server/api/saveQuotationRouter'
-import { apiUrl } from 'server/consts/apiUrl'
-import { quotationSignal, saveQuotationLocally } from '@entities/quotation'
+import { quotationSignal, saveQuotationFn, saveQuotationLocally } from '@entities/quotation'
 import { queryKey } from '@shared/consts/queryKey'
-import { axiosWithAuth } from '@shared/lib/axios/axiosWithAuth'
 import { nanoid } from '@shared/lib/nanoid'
 import { navSlice, showErrorNavIcon, showLoadingNavIcon, showSuccessNavIcon } from '@shared/nav'
 
-// todo: make a mutation, just for the consistency sake
 // todo: ones we saved re-direct to id route
 // todo: rename Quotation --> Reset to default offer to NEW
 
@@ -23,19 +18,15 @@ export const saveQuotation = async (): Promise<void> => {
   }
 
   try {
-    const res = await axiosWithAuth<ResBody, AxiosResponse<ResBody>, ReqBody>({
-      method: 'POST',
-      url: apiUrl.saveQuotation,
-      data: {
-        quotation: quotationSignal.value,
-        items: getState().items,
-        id: quotationSignal.value.id,
-      },
+    const { document } = await saveQuotationFn({
+      quotation: quotationSignal.value,
+      items: getState().items,
+      id: quotationSignal.value.id,
     })
 
     quotationSignal.value = {
       ...quotationSignal.value,
-      ...res.data.document,
+      ...document,
     }
 
     saveQuotationLocally()
