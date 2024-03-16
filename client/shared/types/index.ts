fa95aed06ab0174ae9c300ec1f2879c661d0088f
Anton Arbus <anton.arbus@macbookpro>
2024-03-16 20:51:55 +0200

    higher froala version using own key

--- client/shared/types/index.ts
+++ client/shared/types/index.ts
@@ -1,9 +1,5 @@
-// import type FroalaEditor from 'froala-editor'
-
 import FroalaEditor from 'froala-editor'
 
 export type { OnItemResizeStop, OnItemResizeStart, OnItemResize } from './ResizablePaper'
-
 export { FroalaEditor }
-
 export type FroalaEditorRef = { current: FroalaEditor | null }
