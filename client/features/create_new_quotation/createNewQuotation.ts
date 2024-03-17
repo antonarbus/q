48650c7655139e60aa7983387466e96f7f194bae
Anton Arbus <anton.arbus@macbookpro>
2024-03-17 04:37:06 +0200

    createNewQuotation

--- client/features/create_new_quotation/createNewQuotation.ts
+++ client/features/create_new_quotation/createNewQuotation.ts
@@ -3,21 +3,28 @@ import { dispatch } from '@lib_instances/store'
 import { theme } from '@lib_instances/theme'
 import { defaultItems, isItemsFroalaSignal, itemsSlice, reRenderItemsSignal, saveItemsLocally } from '@entities/items'
 import { quotationSignal, saveQuotationLocally } from '@entities/quotation'
+import { localStorageKey } from '@shared/consts/localStorageKey'
 import { route } from '@shared/consts/route'
 import { nanoid } from '@shared/lib/nanoid'
+import { loadingDotsOverlayTextSignal } from '@shared/loading_dots_overlay'
 import { navSlice } from '@shared/nav'
 
 export const createNewQuotation = (): void => {
   isItemsFroalaSignal.value = false
-  dispatch(itemsSlice.actions.loadItemsReducer({ items: defaultItems }))
-  saveItemsLocally({ items: defaultItems })
-  quotationSignal.value = { id: 'local version', email: '' }
-  saveQuotationLocally()
-  dispatch(navSlice.actions.enableTopNavItem({ navMenuItemIdKey: 'save' }))
+  setTimeout(() => { isItemsFroalaSignal.value = true }, 1000 * theme.item.animationDuration)
+
   reRenderItemsSignal.value = nanoid(3)
+
   void router.navigate(route.root)
 
-  setTimeout(() => {
-    isItemsFroalaSignal.value = true
-  }, 1000 * theme.item.animationDuration)
+  loadingDotsOverlayTextSignal.value = 'Loading template...'
+  dispatch(itemsSlice.actions.loadItemsReducer({ items: defaultItems }))
+  quotationSignal.value = { id: 'local version', email: '' }
+  localStorage.setItem(localStorageKey.items, JSON.stringify(defaultItems))
+  localStorage.setItem(localStorageKey.quotation, JSON.stringify(quotationSignal.value))
+
+  dispatch(navSlice.actions.enableTopNavItem({ navMenuItemIdKey: 'save' }))
+  dispatch(navSlice.actions.enableTopNavItem({ navMenuItemIdKey: 'pdf' }))
+  dispatch(navSlice.actions.enableTopNavItem({ navMenuItemIdKey: 'share' }))
+  setTimeout(() => { loadingDotsOverlayTextSignal.value = null }, 2000)
 }
--- client/shared/utils/jsonParseSafe.ts
+++ client/shared/utils/jsonParseSafe.ts
@@ -1,5 +1,6 @@
-export const jsonParseSafe = <T>(str: string): T | undefined => {
+export const jsonParseSafe = <T>(str: unknown): T | undefined => {
   try {
+    if (typeof str !== 'string') return undefined
     const parsedJson: T = JSON.parse(str) as T
     return parsedJson
   } catch {
