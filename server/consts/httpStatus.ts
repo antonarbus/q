5a1dc7feb1250858bc8d6bbeb737390ffeaf77aa
Anton Arbus <anton.arbus@macbookpro>
2024-03-26 11:30:27 +0200

    added status code to loginRouter

--- server/consts/httpStatus.ts
+++ server/consts/httpStatus.ts
@@ -1,22 +1,9 @@
 export const httpStatus = {
-  // successful responses (2xx)
-  success: 200,
-  created: 201,
-  accepted: 202,
-  noContent: 204,
-  // redirection messages (3xx):
-  multipleChoices: 300,
-  movedPermanently: 301,
-  found: 302,
-  notModified: 304,
-  // client error responses (4xx):
-  badRequest: 400,
-  unauthorized: 401,
-  forbidden: 403,
-  notFound: 404,
-  // server error responses (5xx):
-  internalServerError: 500,
-  notImplemented: 501,
-  serviceUnavailable: 503,
-  gatewayTimeout: 504,
+  success_200: 200,
+  created_201: 201,
+  noContent_204: 204,
+  badRequest_400: 400,
+  unauthorized_401: 401,
+  forbidden_403: 403,
+  notFound_404: 404,
 } as const
