dc60560a5d748e2d11477c505ef874b4ce25d07b
Anton Arbus <anton.arbus@macbookpro>
2024-03-26 11:30:27 +0200

    added status code to loginRouter

--- server/api/loginRouter.ts
+++ server/api/loginRouter.ts
@@ -1,8 +1,9 @@
+import { httpStatus } from '@server/consts/httpStatus'
 import bcrypt from 'bcryptjs'
 import express from 'express'
 import { UserModel } from '../db/models/userModel'
 import { getNewAccessToken, getNewRefreshToken, thirtyDaysInSec } from '../services/jwt'
-import type { Next, Req, Res } from '../types'
+import type { Next, ReqWithBody, ResWithBody } from '../types'
 
 export type ReqBody = {
   email: string
@@ -17,59 +18,62 @@ export type ResBody = {
   roles: string[]
 }
 
+type RouterHandler = (req: ReqWithBody<ReqBody>, res: ResWithBody<ResBody>, next: Next) => Promise<ResWithBody<ResBody> | undefined>
+
 export const loginRouter = express.Router()
 
-loginRouter.post('/', async (req: Req, res: Res, next: Next) => {
+const checkCredentials: RouterHandler = async (req, res, next) => {
   try {
-    const password = (req.body as ReqBody).password
-    let email = (req.body as ReqBody).email
-    email = email.toLowerCase()
+    const password = req.body.password
+    const email = req.body.email.toLowerCase()
 
-    // check email & password
     const user = await UserModel.findOne({ email })
 
     if (!user) {
-      res.json({ status: 'error', message: 'no user data' })
-      return
+      return res
+        .status(httpStatus.unauthorized_401)
+        .json({ status: 'error', message: 'no user data' })
     }
 
     const passwordFromDB = user.password
 
     if (!passwordFromDB) {
-      res.json({ status: 'error', message: 'no password' })
-      return
+      return res
+        .status(httpStatus.unauthorized_401)
+        .json({ status: 'error', message: 'no password' })
     }
 
     const isPasswordValid = await bcrypt.compare(password, passwordFromDB)
 
     if (!isPasswordValid) {
-      res.status(401).json({
-        status: 'error',
-        message: 'invalid credentials',
-        accessJwtToken: 'no access token',
-        email: 'no email',
-        roles: ['no role'],
-      })
-      return
+      return res
+        .status(httpStatus.unauthorized_401)
+        .json({
+          status: 'error',
+          message: 'invalid credentials',
+          accessJwtToken: 'no access token',
+          email: 'no email',
+          roles: ['no role'],
+        })
     }
 
     // check if account is activated
     if (!user.isActivated) {
       // todo: send email with activation link
-      res.json({
-        status: 'error',
-        message: 'account is not activated',
-        accessJwtToken: 'no access token',
-        email: 'no email',
-        roles: ['no role'],
-      })
-      return
+      return res
+        .status(httpStatus.created_201)
+        .json({
+          status: 'error',
+          message: 'account is not activated',
+          accessJwtToken: 'no access token',
+          email: 'no email',
+          roles: ['no role'],
+        })
     }
 
     // generate jwt tokens
-    const { roles } = user
-    const accessJwtToken = getNewAccessToken({ email, roles })
-    const refreshJwtToken = getNewRefreshToken({ email, roles })
+    const accessJwtToken = getNewAccessToken({ email, roles: user.roles })
+    const refreshJwtToken = getNewRefreshToken({ email, roles: user.roles })
 
     // put refresh token in cookie
     res.cookie('refreshJwtToken', refreshJwtToken, {
@@ -83,16 +87,18 @@ loginRouter.post('/', async (req: Req, res: Res, next: Next) => {
     await UserModel.findOneAndUpdate(filter, update)
 
     // return data to the client
-    const response = {
-      status: 'ok',
-      message: `user with email: ${email} logged in and tokens are refreshed`,
-      accessJwtToken,
-      email,
-      roles,
-    }
-
-    res.json(response)
+    return res
+      .status(httpStatus.success_200)
+      .json({
+        status: 'ok',
+        message: `user with email: ${email} logged in and tokens are refreshed`,
+        accessJwtToken,
+        email,
+        roles: user.roles,
+      })
   } catch (error) {
     next(error)
   }
-})
+}
+
+loginRouter.post('/', checkCredentials)
