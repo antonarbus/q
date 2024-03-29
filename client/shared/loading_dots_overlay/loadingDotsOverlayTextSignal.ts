933689881595245723c86928717f456a4639ae84
Anton Arbus <anton.arbus@macbookpro>
2024-03-29 19:16:40 +0200

    loadingDotsOverlayTextSignal is null by default

--- client/shared/loading_dots_overlay/loadingDotsOverlayTextSignal.ts
+++ client/shared/loading_dots_overlay/loadingDotsOverlayTextSignal.ts
@@ -1,4 +1,4 @@
 import { signal } from '@preact/signals-react'
 
 // if null overlay not showing
-export const loadingDotsOverlayTextSignal = signal<string | null>('Loading')
+export const loadingDotsOverlayTextSignal = signal<string | null>(null)
