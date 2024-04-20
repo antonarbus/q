0148d23b0fd361f2582797bbc9f6fe4559ab7338
Anton Arbus <anton.arbus@macbookpro>
2024-04-20 20:17:22 +0300

    verifyAccessTokenMiddleware

--- server/middleware/verifyTokenMiddleware.ts
+++ server/middleware/verifyTokenMiddleware.ts
@@ -1,14 +1,21 @@
+import { httpStatus } from '@shared/consts/httpStatus'
 import { headerName } from '../consts/headerName'
 import { verifyAccessToken, type JwtPayloadExtended } from '../services/jwt'
-import type { Next, Req, Res } from '../types'
+import type { Next, Req, Res, ResWithBody } from '../types'
 
-export const verifyAccessTokenMiddleware = (req: Req, res: Res, next: Next): Res | undefined => {
+export type ResBody = {
+  message:
+  'something happened during jwt token validation' |
+  'accessJwtToken is not verified, user is not authorized'
+}
+
+export const verifyAccessTokenMiddleware = (req: Req, res: ResWithBody<ResBody>, next: Next): Res | undefined => {
   try {
     const accessJwtToken = req.headers[headerName.accessJwtToken]
 
     if (typeof accessJwtToken !== 'string') {
       return res
-        .status(401)
+        .status(httpStatus.unauthorized_401)
         .json({ message: 'something happened during jwt token validation' })
     }
 
@@ -16,14 +23,14 @@ export const verifyAccessTokenMiddleware = (req: Req, res: Res, next: Next): Res
 
     if (typeof email !== 'string') {
       return res
-        .status(401)
+        .status(httpStatus.unauthorized_401)
         .json({ message: 'accessJwtToken is not verified, user is not authorized' })
     }
 
     next()
   } catch (error) {
     return res
-      .status(401)
+      .status(httpStatus.unauthorized_401)
       .json({ message: 'accessJwtToken is not verified, user is not authorized' })
   }
 }
