07a24e9f8cc8500fef3f1a5e8cf8d50c938e62ef
Anton Arbus <anton.arbus@macbookpro>
2024-04-28 13:11:40 +0300

    slide away

--- client/pages/edit_item/EditItem.tsx
+++ client/pages/edit_item/EditItem.tsx
@@ -13,7 +13,9 @@ import { ButtonCustom } from '@shared/components/ButtonCustom'
 import { CardCustom } from '@shared/components/CardCustom'
 import { CategoryInput } from '@shared/components/CategoryInput'
 import { NameInput } from '@shared/components/NameInput'
+import { nanoid } from '@shared/lib/nanoid'
 import { notify } from '@shared/ui/top_msg'
+import { slideElement } from '@shared/utils/slideElement'
 
 export const EditItem = (): JSX.Element => {
   const navigate = useNavigate()
@@ -30,10 +32,17 @@ export const EditItem = (): JSX.Element => {
       if (data.message === 'inserted') {
         notify({ msg: 'Added new item', type: 'success', theme: 'dark', position: 'bottom-center' })
       } else if (data.message === 'updated') {
-        notify({ msg: 'Updated', type: 'success', theme: 'dark', position: 'bottom-center' })
+        notify({ msg: 'Updated', type: 'info', theme: 'dark', position: 'bottom-center' })
       }
 
-      navigate('..')
+      setTimeout(() => {
+        slideElement({
+          element: cardRef.current,
+          cb: () => {
+            navigate('..', { replace: true, state: nanoid() })
+          },
+        })
+      }, 1500)
     }
   }, [isSuccess])
 
@@ -41,8 +50,6 @@ export const EditItem = (): JSX.Element => {
     if (isError) {
       if (error.response?.data.message === 'not logged in') {
         notify({ msg: 'Not logged in', type: 'warn', theme: 'dark', position: 'bottom-center' })
-      } else if (error.response?.data.message === 'not owner') {
-        notify({ msg: 'Not owner', type: 'warn', theme: 'dark', position: 'bottom-center' })
       } else if (error.response?.data.message === 'not saved') {
         notify({ msg: 'Not saved', type: 'warn', theme: 'dark', position: 'bottom-center' })
       } else {
@@ -64,9 +71,7 @@ export const EditItem = (): JSX.Element => {
         reference={cardRef}
         title='Save item'
         logo={
-          <Avatar
-            sx={{ m: 1, bgcolor: theme.colors.darkBackground }}
-          >
+          <Avatar sx={{ m: 1, bgcolor: theme.colors.darkBackground }} >
             <BsBookmarkStar />
           </Avatar>
         }
@@ -74,7 +79,9 @@ export const EditItem = (): JSX.Element => {
         <form
           onSubmit={(e: FormEvent): void => {
             e.preventDefault()
+
             const itemToSave = location.state.itemToSave as Copyable | undefined
+
             if (!itemToSave) return
 
             const item = {
