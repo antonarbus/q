bce33277960ef9e36b5d4699c55ec2e938f14b48
Anton Arbus <anton.arbus@macbookpro>
2024-04-28 13:02:50 +0300

    clean icons

--- client/features/items/boq_row_actions/save_boq_row/SaveBoqRowIcon.tsx
+++ client/features/items/boq_row_actions/save_boq_row/SaveBoqRowIcon.tsx
@@ -2,78 +2,34 @@ import { getState } from '@lib_instances/store'
 import { type MouseEvent, type ReactNode } from 'react'
 import { BsBookmarks } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
-import { useUpdateEffect } from 'react-use'
-import { useSaveItemMutation } from '@entities/item'
 import { boqRowKey, getBoqRowFromStore, useItem, useRow } from '@entities/quotation'
-import { RotatingLoaderIcon } from '@shared/components'
 import { route } from '@shared/consts/route'
 import { notify } from '@shared/ui/top_msg'
 
 export const SaveBoqRowIcon = (): ReactNode => {
   const navigate = useNavigate()
-  const { mutate: saveBoqRow, data, isPending, isSuccess, isError, error } = useSaveItemMutation()
   const { itemIndex } = useItem()
   const { rowIndex } = useRow()
 
-  useUpdateEffect(() => {
-    if (isSuccess) {
-      if (data.message === 'inserted') {
-        notify({ msg: 'Saved', type: 'success', position: 'bottom-center' })
-      }
+  return (
+    <BsBookmarks
+      tabIndex={-1}
+      onClick={(e: MouseEvent): void => {
+        const email = getState().user.email
 
-      if (data.message === 'updated') {
-        notify({ msg: 'Updated', type: 'success', position: 'bottom-center' })
-      }
-    }
-  }, [isSuccess])
+        if (!email) {
+          notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
+          navigate('./login')
+          return
+        }
 
-  useUpdateEffect(() => {
-    if (isError) {
-      if (error.response?.data.message === 'not logged in') {
-        notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
-      }
+        const boqRow = getBoqRowFromStore({ rowIndex, itemIndex })
 
-      if (error.response?.data.message === 'not owner') {
-        notify({ msg: 'Now owner', type: 'warn', theme: 'light' })
-      }
+        if (!boqRow) return
+        if (boqRow.type === boqRowKey.paste) return
 
-      if (error.response?.data.message === 'not saved') {
-        notify({ msg: 'Now saved', type: 'error', theme: 'light' })
-      }
-    }
-  }, [isError])
-
-  if (isPending) {
-    return (
-      <RotatingLoaderIcon />
-    )
-  }
-
-  if (!isPending) {
-    return (
-      <BsBookmarks
-        tabIndex={-1}
-        onClick={(e: MouseEvent): void => {
-          const email = getState().user.email
-
-          if (!email) {
-            notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
-            navigate('./login')
-            return
-          }
-
-          const boqRow = getBoqRowFromStore({ rowIndex, itemIndex })
-
-          if (!boqRow) return
-          if (boqRow.type === boqRowKey.paste) return
-
-          navigate(`./${route.editItem}`, { state: { itemToSave: boqRow } })
-
-          // saveBoqRow({ item: boqRow })
-        }}
-      />
-    )
-  }
-
-  return null
+        navigate(`./${route.editItem}`, { state: { itemToSave: boqRow } })
+      }}
+    />
+  )
 }
--- client/features/items/item_actions/save_item/SaveItemIcon.tsx
+++ client/features/items/item_actions/save_item/SaveItemIcon.tsx
@@ -2,77 +2,33 @@ import { getState } from '@lib_instances/store'
 import { type ReactNode, type MouseEvent } from 'react'
 import { BsBookmarks } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
-import { useUpdateEffect } from 'react-use'
-import { useSaveItemMutation } from '@entities/item'
 import { getItemFromStore, itemKey, useItem } from '@entities/quotation'
-import { RotatingLoaderIcon } from '@shared/components'
 import { route } from '@shared/consts/route'
 import { notify } from '@shared/ui/top_msg'
 
 export const SaveItemIcon = (): ReactNode => {
   const navigate = useNavigate()
-  const { mutate: saveItem, data, isPending, isSuccess, isError, error } = useSaveItemMutation()
   const { itemIndex } = useItem()
 
-  useUpdateEffect(() => {
-    if (isSuccess) {
-      if (data.message === 'inserted') {
-        notify({ msg: 'Saved', type: 'success', position: 'bottom-center' })
-      }
+  return (
+    <BsBookmarks
+      tabIndex={-1}
+      onClick={(e: MouseEvent): void => {
+        const email = getState().user.email
 
-      if (data.message === 'updated') {
-        notify({ msg: 'Updated', type: 'success', position: 'bottom-center' })
-      }
-    }
-  }, [isSuccess])
+        if (!email) {
+          notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
+          navigate('./login')
+          return
+        }
 
-  useUpdateEffect(() => {
-    if (isError) {
-      if (error.response?.data.message === 'not logged in') {
-        notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
-      }
+        const item = getItemFromStore({ itemIndex })
 
-      if (error.response?.data.message === 'not owner') {
-        notify({ msg: 'Now owner', type: 'warn', theme: 'light' })
-      }
+        if (!item) return
+        if (item.type === itemKey.paste) return
 
-      if (error.response?.data.message === 'not saved') {
-        notify({ msg: 'Now saved', type: 'error', theme: 'light' })
-      }
-    }
-  }, [isError])
-
-  if (isPending) {
-    return (
-      <RotatingLoaderIcon />
-    )
-  }
-
-  if (!isPending) {
-    return (
-      <BsBookmarks
-        tabIndex={-1}
-        onClick={(e: MouseEvent): void => {
-          const email = getState().user.email
-
-          if (!email) {
-            notify({ msg: 'Not logged in', type: 'warn', theme: 'light' })
-            navigate('./login')
-            return
-          }
-
-          const item = getItemFromStore({ itemIndex })
-
-          if (!item) return
-          if (item.type === itemKey.paste) return
-
-          navigate(`./${route.editItem}`, { state: { itemToSave: item } })
-
-          // saveItem({ item })
-        }}
-      />
-    )
-  }
-
-  return null
+        navigate(`./${route.editItem}`, { state: { itemToSave: item } })
+      }}
+    />
+  )
 }
