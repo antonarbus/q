c60c10ee5ba18c490d26a929f9e788f8e2130458
Anton Arbus <anton.arbus@macbookpro>
2024-05-06 09:29:18 +0300

    useGetItemMutation

---
+++ client/pages/items_table/CopyItemButton.tsx
@@ -0,0 +1,33 @@
+import { IconButton } from '@mui/material'
+import type { ReqBody } from '@server/api/deleteItemRouter'
+import { MdCopyAll } from 'react-icons/md'
+import { useUpdateEffect } from 'react-use'
+import { useGetItemMutation } from '@entities/item'
+import { RotatingLoaderIcon } from '@shared/components'
+import { notify } from '@shared/ui/top_msg'
+
+export const CopyItemButton = ({ id }: ReqBody): JSX.Element => {
+  const { mutate: loadItem, isPending, isSuccess, isError, error } = useGetItemMutation()
+  console.log('🚀 ~ isPending:', isPending)
+
+  useUpdateEffect(() => {
+    if (isError) {
+      notify({ msg: error.response?.data.message, type: 'error', theme: 'light' })
+    }
+  }, [isError])
+
+  return (
+    <IconButton
+      size='small'
+      onClick={() => {
+        loadItem({ id })
+      }}
+      sx={{
+        translate: '0px 1px',
+      }}
+    >
+      {!isPending && <MdCopyAll />}
+      {isPending && <RotatingLoaderIcon />}
+    </IconButton>
+  )
+}
