f5b1bf7fd728bab55ebb63b0320a97afc931b3d8
Anton Arbus <anton.arbus@macbookpro>
2024-05-06 21:53:35 +0300

    disable froala

--- client/pages/items_table/GetCopiedItemButton.tsx
+++ client/pages/items_table/GetCopiedItemButton.tsx
@@ -5,15 +5,22 @@ import { MdCopyAll } from 'react-icons/md'
 import { useUpdateEffect } from 'react-use'
 import { copySlice } from '@entities/copy'
 import { useGetItemMutation } from '@entities/item'
+import { isFroalaSignal } from '@entities/quotation'
 import { RotatingLoaderIcon } from '@shared/components'
 import { notify } from '@shared/ui/top_msg'
 
-export const CopyItemButton = ({ id }: ReqBody): JSX.Element => {
+// todo: do not disable new nav item
+// todo: after multiple copies ui slows down a lot
+
+export const GetCopiedItemButton = ({ id }: ReqBody): JSX.Element => {
   const { mutate: loadItem, isPending, isSuccess, isError, error, data } = useGetItemMutation()
 
   useUpdateEffect(() => {
     if (isSuccess) {
       if (!data.item) return
+
+      // todo: copyItem should already have preview
+      isFroalaSignal.value = false
       dispatch(copySlice.actions.addItemIntoCopyContainer({ copyItem: data.item, preview: data.item.preview ?? '' }))
       dispatch(copySlice.actions.allowToPaste())
       dispatch(copySlice.actions.showCopyContainer())
