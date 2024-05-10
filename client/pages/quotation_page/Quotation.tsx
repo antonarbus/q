176dfda65acb3dba1a547b28f0bcfd68aed8e8df
Anton Arbus <anton.arbus@macbookpro>
2024-05-10 21:50:21 +0300

    search input field

--- client/pages/quotation_page/Quotation.tsx
+++ client/pages/quotation_page/Quotation.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import { Items } from '@widgets/items'
-import { QuotationInfo } from '@widgets/quotation'
+import { Info } from '@widgets/quotation/info'
+import { Search } from '@widgets/quotation/search'
 import { useLoadQuotation } from '@features/quotation/load_quotation'
+import { InfoAndSearchLayout } from './InfoAndSearchLayout'
 
 export const Quotation = (): JSX.Element => {
   useLoadQuotation()
 
   return (
     <>
-      <QuotationInfo />
+      <InfoAndSearchLayout>
+        <Info />
+        <Search />
+      </InfoAndSearchLayout>
       <Items />
       <Outlet />
     </>
---
+++ client/widgets/quotation/info/Info.tsx
@@ -0,0 +1,37 @@
+import { useSelectorTyped } from '@lib_instances/store'
+import { Box } from '@mui/material'
+import { type ReactNode } from 'react'
+import { FaInfoCircle } from 'react-icons/fa'
+import { openQuotationInfoModal } from '@features/quotation/open_info_quotation_modal'
+
+export const Info = (): ReactNode => {
+  const quotationId = useSelectorTyped(state => state.quotation.id)
+
+  if (quotationId === '' || quotationId === undefined) return null
+
+  return (
+    <Box
+      component='button'
+      onClick={() => {
+        openQuotationInfoModal()
+      }}
+      sx={{
+        all: 'unset',
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer',
+        gap: '5px',
+        ':hover svg': {
+          fill: '#3c5588 !important',
+        },
+      }}
+    >
+      <FaInfoCircle
+        style={{
+          fill: '#6488cf',
+        }}
+      />
+      {quotationId}
+    </Box>
+  )
+}
