495070548152b2db96ea23a23ed9db6b69aa2af7
Anton Arbus <anton.arbus@macbookpro>
2024-05-14 11:44:58 +0300

    remove margin bottom from input fields

--- client/entities/quotation/redux/getters/getBoqCellHtmlFromStore.ts
+++ client/entities/quotation/redux/getters/getBoqCellHtmlFromStore.ts
@@ -12,6 +12,7 @@ export const getBoqCellHtmlFromStore = ({ itemIndex, rowIndex, boqRowCellKey }: 
   const item = getState().quotation.items[itemIndex]
   if (!item) return ''
 
+  // todo: move item for edit modal into a different slice
   // special case for when the item is a row for item edit modal
   if (item.type === itemKey.boq) {
     const row = item.boq.rows[rowIndex]
--- client/shared/components/FormModal.tsx
+++ client/shared/components/FormModal.tsx
@@ -116,7 +116,7 @@ export const FormModal = ({
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            gap: '20px',
+            gap: '30px',
             padding: paddingContent ?? '40px 50px',
             backgroundColor: '#8080800f',
             overflowY: 'auto',
