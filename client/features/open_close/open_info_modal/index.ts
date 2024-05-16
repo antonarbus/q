0f56c102c3d5574ee3652ba0a8b16caaa26105e1
Anton Arbus <anton.arbus@macbookpro>
2024-05-16 20:02:56 +0300

    openSaveQuotationModal

--- client/features/open_close/open_info_modal/index.ts
+++ client/features/open_close/open_info_modal/index.ts
@@ -1,2 +1,3 @@
+export { openQuotationInfoModal } from './openQuotationInfoModal'
 export { OpenInfoBoqRowModalIcon } from './OpenInfoBoqRowModalIcon'
 export { OpenInfoItemModalIcon } from './OpenInfoItemModalIcon'
