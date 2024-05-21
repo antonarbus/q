4467985f265fb0847110ea7f72ded4513059beb1
Anton Arbus <anton.arbus@macbookpro>
2024-05-22 02:02:14 +0300

    improve share to form

--- client/pages/quotation/save_quotation_modal/SaveQuotationModal.tsx
+++ client/pages/quotation/save_quotation_modal/SaveQuotationModal.tsx
@@ -1,17 +1,22 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { getState, useSelectorTyped } from '@lib_instances/store'
-import { Box, Chip, FormControlLabel, Radio, RadioGroup } from '@mui/material'
-import { useSignal } from '@preact/signals-react'
+import { theme } from '@lib_instances/theme'
+import { Box, Chip, FormControlLabel, IconButton, InputAdornment, Radio, RadioGroup, TextField } from '@mui/material'
+import { useSignal, useSignalEffect } from '@preact/signals-react'
+import { AnimatePresence, motion } from 'framer-motion'
 import uniq from 'lodash.uniq'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { BsFillPersonPlusFill } from 'react-icons/bs'
-import { FiShare2 } from 'react-icons/fi'
+import { IoAddCircleOutline } from 'react-icons/io5'
 import { MdSaveAlt, MdGroups, MdGroupOff } from 'react-icons/md'
 import { useSaveQuotation } from '@features/quotation/save_quotation'
-import { EmailField, FormModal, OutlinedDivWithLabel } from '@shared/components'
+import { FormModal, OutlinedDivWithLabel } from '@shared/components'
 import { CategoryField } from './CategoryField'
 import { DescriptionField } from './DescriptionField'
 import { NameField } from './NameField'
+
+const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const isEmailPatternOk = (email: string): boolean => emailRegExp.test(email)
 
 export const SaveQuotationModal = (): JSX.Element => {
   const modalRef = useRef<HTMLDivElement>(null)
@@ -30,15 +35,21 @@ export const SaveQuotationModal = (): JSX.Element => {
 
   type SharedOptions = (typeof sharedToOptions)[keyof typeof sharedToOptions]
 
-  const sharedToSignal = useSignal<SharedOptions>(sharedToOptions.none)
-  const emailToShareSignal = useSignal('')
-  const isEmailOkSignal = useSignal(false)
-  const emailsToShareSignal = useSignal<string[]>([])
+  const shareToOptionSignal = useSignal<SharedOptions>(sharedToOptions.none)
+  const emailToSignal = useSignal('')
+  const emailsToSignal = useSignal<string[]>([])
+  const labelSignal = useSignal('Share')
 
-  const [emailParent] = useAutoAnimate()
-  const [emailChipsParent] = useAutoAnimate()
+  const shouldMonitorIfEmailIsOkRef = useRef(false)
 
-  console.log('🚀 ~ emailsToShareSignal.value:', emailsToShareSignal.value)
+  const [chipsParent] = useAutoAnimate()
+
+  useEffect(() => {
+    if (shouldMonitorIfEmailIsOkRef.current) {
+      const isEmailOk = isEmailPatternOk(emailToSignal.value)
+      labelSignal.value = isEmailOk ? 'Share' : 'Check email'
+    }
+  }, [emailToSignal.value])
 
   return (
     <FormModal
@@ -59,125 +70,181 @@ export const SaveQuotationModal = (): JSX.Element => {
       <CategoryField categorySignal={categorySignal}/>
       <DescriptionField descSignal={descSignal} />
 
-      <OutlinedDivWithLabel label='Share'>
+      <OutlinedDivWithLabel
+        label={labelSignal.value}
+        sx={{
+          '& .MuiInputLabel-shrink': {
+            color: labelSignal.value !== 'Share' ? theme.colors.red : '',
+          },
+        }}
+      >
         <Box
           sx={{
-            display: 'flex',
-            padding: '5px',
+            padding: '10px 10px 10px 20px',
           }}
         >
-          <Box
-            sx={{
-              width: '50px',
-              flexShrink: 0,
-            }}
-          >
-            <FiShare2 style={{ height: '22px', width: '22px', translate: '6px 10px', opacity: 0.5 }}/>
-          </Box>
-          <RadioGroup
-            name='controlled-radio-buttons-group'
-            value={sharedToSignal.value}
-            onChange={(event): void => {
-              const value = event.target.value as SharedOptions
-              sharedToSignal.value = value
-            }}
-            sx={{
-              '.MuiRadio-root': {
-                padding: '5px',
-              },
-            }}
-          >
+          <AnimatePresence initial={false}>
             <Box
+              key='radio-buttons'
               sx={{
                 display: 'flex',
-                alignItems: 'center',
-                gap: '15px',
               }}
             >
-              <MdGroupOff style={{ opacity: 0.7 }}/>
-              <FormControlLabel
-                value={sharedToOptions.none}
-                label={sharedToOptions.none}
-                disabled={sharedToSignal.value === sharedToOptions.none}
-                control={<Radio size='small' />}
-              />
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '15px',
-              }}
-            >
-              <MdGroups style={{ opacity: 0.7, scale: '1.2', translate: '-1px 0px' }}/>
-              <FormControlLabel
-                value={sharedToOptions.anyone}
-                label={sharedToOptions.anyone}
-                disabled={sharedToSignal.value === sharedToOptions.anyone}
-                control={<Radio size='small' />}
-              />
-            </Box>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '15px',
-              }}
-            >
-              <BsFillPersonPlusFill style={{ opacity: 0.7 }}/>
-              <FormControlLabel
-                value={sharedToOptions.persons}
-                label={sharedToOptions.persons}
-                disabled={sharedToSignal.value === sharedToOptions.persons}
-                control={<Radio size='small' />}
-              />
-            </Box>
-          </RadioGroup>
-          {sharedToSignal.value === sharedToOptions.persons && (
-            <EmailField
-              emailSignal={emailToShareSignal}
-              isEmailOkSignal={isEmailOkSignal}
-              label='Share with'
-              onClickAway={() => {
-                if (isEmailOkSignal.value) {
-                  emailsToShareSignal.value = uniq([...emailsToShareSignal.value, emailToShareSignal.value])
-                  emailToShareSignal.value = ''
-                }
-              }}
-            />
-          )}
-        </Box>
-        {sharedToSignal.value === sharedToOptions.persons && emailsToShareSignal.value.length > 0 && (
-          <Box
-            ref={emailChipsParent}
-            key='emails-chips'
-            sx={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              // marginTop: '5px',
-              // top: '-15px',
-              margin: '10px',
-            }}
-          >
-            {emailsToShareSignal.value.map(email => {
-              return (
-                <Chip
-                  key={email}
-                  label={email}
-                  onDelete={() => {
-                    emailsToShareSignal.value = emailsToShareSignal.value
-                      .filter(emailInArray => emailInArray !== email)
+              <RadioGroup
+                name='controlled-radio-buttons-group'
+                value={shareToOptionSignal.value}
+                onChange={(event): void => {
+                  const value = event.target.value as SharedOptions
+                  shareToOptionSignal.value = value
+                }}
+                sx={{
+                  '.MuiRadio-root': {
+                    padding: '5px',
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '15px',
+                  }}
+                >
+                  <MdGroupOff style={{ opacity: 0.7 }}/>
+                  <FormControlLabel
+                    value={sharedToOptions.none}
+                    label={sharedToOptions.none}
+                    disabled={shareToOptionSignal.value === sharedToOptions.none}
+                    control={<Radio size='small' />}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '15px',
+                  }}
+                >
+                  <MdGroups style={{ opacity: 0.7, scale: '1.2', translate: '-1px 0px' }}/>
+                  <FormControlLabel
+                    value={sharedToOptions.anyone}
+                    label={sharedToOptions.anyone}
+                    disabled={shareToOptionSignal.value === sharedToOptions.anyone}
+                    control={<Radio size='small' />}
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    position: 'relative',
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '15px',
+                  }}
+                >
+                  <BsFillPersonPlusFill style={{ opacity: 0.7 }}/>
+                  <FormControlLabel
+                    value={sharedToOptions.persons}
+                    label={sharedToOptions.persons}
+                    disabled={shareToOptionSignal.value === sharedToOptions.persons}
+                    control={<Radio size='small' />}
+                  />
+                </Box>
+              </RadioGroup>
+              {shareToOptionSignal.value === sharedToOptions.persons && (
+                <TextField
+                  autoFocus
+                  focused
+                  variant='standard'
+                  placeholder='Email'
+                  value={emailToSignal.value}
+                  onChange={e => {
+                    emailToSignal.value = e.target.value
                   }}
                   sx={{
-                    width: 'min-content',
-                    m: '2px',
-                    fontSize: '12px',
+                    position: 'relative',
+                    bottom: '5px',
+                    flexGrow: 1,
+                    alignSelf: 'self-end',
+                    input: {
+                      fontSize: '12px',
+                      padding: '4px',
+                    },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton
+                          size='small'
+                          onClick={() => {
+                            if (emailToSignal.value === '') return
+                            const isEmailOk = isEmailPatternOk(emailToSignal.value)
+
+                            if (isEmailOk) {
+                              shouldMonitorIfEmailIsOkRef.current = false
+                              labelSignal.value = 'Share'
+                              emailsToSignal.value = uniq([...emailsToSignal.value, emailToSignal.value])
+                              emailToSignal.value = ''
+                              return
+                            }
+
+                            if (!isEmailOk) {
+                              shouldMonitorIfEmailIsOkRef.current = true
+                              labelSignal.value = 'Check email'
+                            }
+                          }}
+                        >
+                          <IoAddCircleOutline />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
                   }}
                 />
-              )
-            })}
-          </Box>
-        )}
+              )}
+            </Box>
+            {shareToOptionSignal.value === sharedToOptions.persons && emailsToSignal.value.length > 0 && (
+              <Box
+                ref={chipsParent}
+                key='emails-chips'
+                component={motion.div}
+                initial={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                animate={{
+                  height: 'auto',
+                  opacity: 1,
+                }}
+                exit={{
+                  height: 0,
+                  opacity: 0,
+                }}
+                sx={{
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  flexDirection: 'row-reverse',
+                }}
+              >
+                {emailsToSignal.value.map(email => {
+                  return (
+                    <Chip
+                      key={email}
+                      label={email}
+                      onDelete={() => {
+                        emailsToSignal.value = emailsToSignal.value
+                          .filter(emailInArray => emailInArray !== email)
+                      }}
+                      sx={{
+                        width: 'min-content',
+                        margin: '2px',
+                        fontSize: '12px',
+                      }}
+                    />
+                  )
+                })}
+              </Box>
+            )}
+          </AnimatePresence>
+        </Box>
       </OutlinedDivWithLabel>
 
     </FormModal>
