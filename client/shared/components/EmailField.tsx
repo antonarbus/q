9197213f7ef3ffd9b48263a060bb828117c9b658
Anton Arbus <anton.arbus@macbookpro>
2024-05-22 09:17:39 +0300

    ShareField

--- client/shared/components/EmailField.tsx
+++ client/shared/components/EmailField.tsx
@@ -4,9 +4,7 @@ import { InputAdornment, TextField } from '@mui/material'
 import { useSignal, type Signal, useSignalEffect } from '@preact/signals-react'
 import mailcheck from 'mailcheck'
 import type { RefObject } from 'react'
-
-const emailRegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const isEmailPatternOk = (email: string): boolean => emailRegExp.test(email)
+import { isEmailPatternOk } from '../utils/isEmailPatternOk'
 
 type Props = {
   emailSignal: Signal<string>
