5a8d555597968a13f1b5e244557612dea1d07145
Anton Arbus <anton.arbus@macbookpro>
2024-05-26 10:38:45 +0300

    go to login route on save click if no logged

--- client/features/open_close/open_save_quotation_modal/openSaveQuotationModal.ts
+++ client/features/open_close/open_save_quotation_modal/openSaveQuotationModal.ts
@@ -1,6 +1,12 @@
 import { router } from '@lib_instances/Router'
+import { getState } from '@lib_instances/store'
 import { route } from '@shared/consts/route'
 
 export const openSaveQuotationModal = (): void => {
+  if (!getState().user.email) {
+    void router.navigate(`./${route.login}`)
+    return
+  }
+
   void router.navigate(`./${route.saveQuotation}`)
 }
