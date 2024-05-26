3d771b5155b21254d71a7183a1005e1d64c40955
Anton Arbus <anton.arbus@macbookpro>
2024-05-26 17:37:38 +0300

    lint

--- client/entities/quotation/refs/previousQuotationRef.ts
+++ client/entities/quotation/refs/previousQuotationRef.ts
@@ -1,5 +1,5 @@
 import { type MutableRefObject } from 'react'
-import { type Quotation } from '@entities/quotation'
+import { type Quotation } from '../types'
 
 export const previousQuotationRef: MutableRefObject<Quotation | null> = {
   current: null,
