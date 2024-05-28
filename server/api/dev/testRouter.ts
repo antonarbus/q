9af80aaefca2744134419c5e089cdb1c88d5a525
Anton Arbus <anton.arbus@macbookpro>
2024-05-28 08:41:53 +0300

    sendEmail function

--- server/api/dev/testRouter.ts
+++ server/api/dev/testRouter.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { QuotationModel } from '@server/db/models/quotationModel'
-import { UserModel } from '@server/db/models/userModel'
+import { sendEmail } from '@server/services/mail'
 import { Router } from 'express'
-import { BookmarkModel } from '../../db/models/bookmarkModel'
 import type { Next, Req, Res } from '../../types'
 
 // todo: delete, it is temp file not related to the project
@@ -11,9 +9,17 @@ export const testRouter = Router()
 export async function test(req: Req, res: Res, next: Next): Promise<void> {
   try {
     // await QuotationModel.deleteOne({ version: 2 })
-    const dbRes = await QuotationModel.find()
     // const dbRes = await UserModel.find({ email: 'anton.arbus@gmail.com' })
     // const dbRes = await ItemModel.find().distinct('category', { email: 'anton.arbus@gmail.com' })
+    const dbRes = await QuotationModel.find()
+
+    const mailRes = await sendEmail({
+      to: 'anton.arbus@gmail.com',
+      subject: 'Activate account',
+      htmlBody: 'Activate your account',
+    })
+
+    console.log('🚀 ~ mailRes:', mailRes)
 
     res.status(200).json({ dbRes })
   } catch (error) {
