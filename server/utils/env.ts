4d0bcfb5c5deeac2c96d14f91ff2c477ad5b5a01
Anton Arbus <anton.arbus@macbookpro>
2024-05-30 11:14:30 +0300

    requestPasswordResetRouter

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -1,3 +1,6 @@
 export const isDev = process.env.NODE_ENV === 'development'
 export const isProd = process.env.NODE_ENV !== 'development'
-export const env = process.env.NODE_ENV === 'development' ? 'dev' : 'prod'
+export const env = isDev ? 'dev' : 'prod'
+export const domain = isProd
+  ? 'https://quotation.app'
+  : 'https://local.quotation.app:3005'
