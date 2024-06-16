073c433d0b465bd75656a12125eed1022e82ae5c
Anton Arbus <anton.arbus@macbookpro>
2024-06-16 08:58:03 +0300

    pass scroll position to save modal

--- client/shared/nav/ui/NavList/NavItem/NavItem.tsx
+++ client/shared/nav/ui/NavList/NavItem/NavItem.tsx
@@ -153,10 +153,16 @@ export const NavItem = ({ children, id }: Props): JSX.Element => {
         {icon && isSuccess && <SuccessIcon />}
         {icon && isError && <ErrorIcon />}
         {icon && !isLoading && !isSuccess && !isError && (
-          <Icon icon={icon} disabled={disabled} />
+          <Icon
+            icon={icon}
+            disabled={disabled}
+          />
         )}
         {!icon && shouldDisplayIcon && (
-          <Icon icon={name?.[0]} disabled={disabled} />
+          <Icon
+            icon={name?.[0]}
+            disabled={disabled}
+          />
         )}
         {name && <span className='nav-item-name'>{name}</span>}
         {isNestedMenu && !disabled && (
