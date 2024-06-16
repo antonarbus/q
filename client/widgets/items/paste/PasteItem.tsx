3c30902ee9bca228b4097183580d1f70622a80a7
Anton Arbus <anton.arbus@macbookpro>
2024-06-16 17:43:43 +0300

    Only id for paste item

--- client/widgets/items/paste/PasteItem.tsx
+++ client/widgets/items/paste/PasteItem.tsx
@@ -1,44 +1,38 @@
 import type { EmotionJSX } from '@emotion/react/types/jsx-namespace'
 import { theme } from '@lib_instances/theme'
 import { motion } from 'framer-motion'
-import { useItem } from '@entities/quotation'
 
-export const PasteItem = (): EmotionJSX.Element => {
-  const { itemId } = useItem()
-
-  return (
-    <motion.div
-      id={itemId}
-      className='paste-item'
-      initial={{
-        height: 0,
-        marginBottom: 0,
-        opacity: 0,
-      }}
-      animate={{
-        height: 20,
-        marginBottom: 20,
-        opacity: 1,
-      }}
-      exit={{
-        height: 0,
-        marginBottom: 0,
-        opacity: 0,
-      }}
-      transition={{
-        height: { duration: theme.item.animationDuration },
-        marginBottom: { duration: theme.item.animationDuration },
-        opacity: { duration: 0 },
-      }}
-      style={{
-        display: 'grid',
-        placeItems: 'center',
-        fontWeight: 600,
-        color: theme.copy.pasteTextColor,
-        userSelect: 'none',
-      }}
-    >
-      Paste here
-    </motion.div>
-  )
-}
+export const PasteItem = (): EmotionJSX.Element => (
+  <motion.div
+    className='paste-item'
+    initial={{
+      height: 0,
+      marginBottom: 0,
+      opacity: 0,
+    }}
+    animate={{
+      height: 20,
+      marginBottom: 20,
+      opacity: 1,
+    }}
+    exit={{
+      height: 0,
+      marginBottom: 0,
+      opacity: 0,
+    }}
+    transition={{
+      height: { duration: theme.item.animationDuration },
+      marginBottom: { duration: theme.item.animationDuration },
+      opacity: { duration: 0 },
+    }}
+    style={{
+      display: 'grid',
+      placeItems: 'center',
+      fontWeight: 600,
+      color: theme.copy.pasteTextColor,
+      userSelect: 'none',
+    }}
+  >
+    Paste here
+  </motion.div>
+)
