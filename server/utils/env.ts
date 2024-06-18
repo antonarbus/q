143e6231ef997b58564fb9714ea2df2db2fdfc97
Anton Arbus <anton.arbus@macbookpro>
2024-06-18 10:57:03 +0300

    centralized server host and port

--- server/utils/env.ts
+++ server/utils/env.ts
@@ -3,7 +3,9 @@
 export const isDev = process.env.NODE_ENV === 'development'
 export const isProd = process.env.NODE_ENV !== 'development'
 export const env = isDev ? 'dev' : 'prod'
-export const portServer = 3006
+
+export const portServer = isDev ? 3006 : 3333
+export const hostServer = 'http://localhost'
 
 export const domainClient = isProd
   ? 'https://quotation.app'
