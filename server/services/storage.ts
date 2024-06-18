2c4eb07e2c8fe376751e28db0d4ce896d0993eb0
Anton Arbus <anton.arbus@macbookpro>
2024-06-18 05:19:12 +0300

    removed 04cff2d88d5b file

--- server/services/storage.ts
+++ server/services/storage.ts
@@ -4,8 +4,17 @@ import { Storage } from '@google-cloud/storage'
 //* https://cloud.google.com/nodejs/docs/reference/storage/latest
 
 const storage = new Storage({
-  keyFilename: process.env.GOOGLE_CLOUD_PROJECT_FILE_KEY,
-  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  // keyFilename: process.env.GOOGLE_CLOUD_PROJECT_FILE_KEY,
+  // projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  credentials: {
+    type: process.env.GOOGLE_CLOUD_PROJECT_TYPE,
+    project_id: process.env.GOOGLE_CLOUD_PROJECT_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY,
+    client_email: process.env.GOOGLE_CLOUD_PROJECT_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLOUD_PROJECT_CLIENT_ID,
+    token_url: process.env.GOOGLE_CLOUD_PROJECT_TOKEN_URL,
+  },
 })
 
 export const bucket = storage.bucket(process.env.BUCKET_NAME!)
