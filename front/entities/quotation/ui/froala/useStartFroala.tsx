d05ba2017d7646ecfdf8e827eac9f596e317f6d1
Anton Arbus <anton.arbus@macbookpro>
2024-06-19 02:53:23 +0300

    moved froala into public

--- front/entities/quotation/ui/froala/useStartFroala.tsx
+++ front/entities/quotation/ui/froala/useStartFroala.tsx
@@ -1,7 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import './froalaPkgd.js'
-import './froalaPkgd.css'
 
 import type { MouseEvent } from 'react'
 import { useEffectOnce } from 'react-use'
