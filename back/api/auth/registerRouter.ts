45450aa3173107b683149b294c84b578f94ca480
Anton Arbus <anton.arbus@macbookpro>
2024-06-19 03:11:21 +0300

    domain

--- back/api/auth/registerRouter.ts
+++ back/api/auth/registerRouter.ts
@@ -12,7 +12,7 @@ import { UserModel } from '../../db/models/userModel'
 import { nanoid } from '../../lib/nanoid'
 import { sendEmail } from '../../services/email'
 import type { Next, ReqWithBody, ResWithBody } from '../../types'
-import { domainClient } from '../../utils/env'
+import { domain } from '../../utils/env'
 
 export type ReqBody = {
   email: User['email']
@@ -85,9 +85,9 @@ const register: RouterHandler = async (req, res, next) => {
         <p>
           <a
             clicktracking="off"
-            href="${domainClient}/activate/${activationKey}"
+            href="${domain}/activate/${activationKey}"
           >
-            ${domainClient}/activate/${activationKey}
+            ${domain}/activate/${activationKey}
           </a>
         </p>
       `,
