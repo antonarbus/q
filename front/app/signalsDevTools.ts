51067ea5d2b96118bbb42b55c3a0b591c1915781
Anton Arbus <anton.arbus@macbookpro>
2024-06-20 07:55:15 +0300

    removed disable react dev tools

--- front/app/signalsDevTools.ts
+++ front/app/signalsDevTools.ts
@@ -34,6 +34,6 @@ function signalsOn(): void {
   })
 }
 
-signalsOn()
+// signalsOn()
 
 window.signalsOn = signalsOn
--- index.tsx
+++ index.tsx
@@ -1,8 +1,5 @@
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import ReactDOM from 'react-dom/client'
 import { App } from './front/app/App'
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const rootElement = document.getElementById('root') as Element
 const root = ReactDOM.createRoot(rootElement)
