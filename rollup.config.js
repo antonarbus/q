cf664aa105875bac5c1b1a07372dd79777a0d9c8
Anton Arbus <anton.arbus@macbookpro>
2024-06-21 21:46:09 +0300

    normalized packages

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,23 @@ import typescript from '@rollup/plugin-typescript'
 export default {
   input: './back/index.ts',
   output: {
-    dir: 'build/back',
+    dir: './back/build/',
     format: 'es',
   },
   plugins: [typescript()],
+  external: [
+    '@google-cloud/storage',
+    '@sendgrid/mail',
+    'bcryptjs',
+    'cookie-parser',
+    'cors',
+    'dotenv',
+    'express',
+    'express-validator',
+    'jsonwebtoken',
+    'mongoose',
+    'morgan',
+    'multer',
+    'nanoid',
+  ],
 }
--- vite.config.mts
+++ vite.config.mts
@@ -80,7 +80,7 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     build: {
-      outDir: './build/front',
+      outDir: '../build/front',
       rollupOptions: {
         output: {
           // https://rollupjs.org/configuration-options/#output-manualchunks
