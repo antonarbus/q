8d75a5ed6eb73f663a07fc4ef0a14491cb329ca8
Anton Arbus <anton.arbus@macbookpro>
2024-06-23 04:33:58 +0300

    docker-compose for dev & prod

--- back/utils/env.ts
+++ back/utils/env.ts
@@ -5,7 +5,7 @@ export const isProd = process.env.NODE_ENV !== 'development'
 export const env = isDev ? 'dev' : 'prod'
 
 export const portFront = isDev ? 3000 : 3001
-export const hostFront = isDev ? 'local.quotation.app' : 'localhost'
+export const hostFront = isDev ? 'localhost' : 'localhost'
 
 export const portBack = isDev ? 4000 : 4001
 export const hostBack = 'http://localhost'
