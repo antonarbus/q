f3e0ca81343f25ae5a932ae4138914713fd68e55
Anton Arbus <anton.arbus@macbookpro>
2024-06-25 00:01:38 +0300

    smth

--- back/utils/env.ts
+++ back/utils/env.ts
@@ -4,12 +4,12 @@ export const isDev = process.env.NODE_ENV === 'development'
 export const isProd = process.env.NODE_ENV !== 'development'
 export const env = isDev ? 'dev' : 'prod'
 
-export const portFront = isDev ? 3000 : 3001
+export const portFront = 3000
+// export const portFront = 3000 : 3001
 export const hostFront = isDev ? 'localhost' : 'localhost'
 
-export const portBack = isDev ? 4000 : 4001
+export const portBack = 4000
+// export const portBack = isDev ? 4000 : 4001
 export const hostBack = 'http://localhost'
 
-export const domain = isProd
-  ? 'https://quotation.app'
-  : 'https://local.quotation.app:3005'
+export const domain = isProd ? 'https://quotation.app' : 'http://localhost:3000'
