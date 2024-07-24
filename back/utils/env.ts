2f4e7c10dadd7e79bda63ed2ed85608f1e3d2dc8
Anton Arbus <anton.arbus@macbookpro>
2024-07-25 00:26:57 +0300

    docker cli changed

--- back/utils/env.ts
+++ back/utils/env.ts
@@ -5,12 +5,9 @@ export const isProd = process.env.NODE_ENV !== 'development'
 export const env = isDev ? 'dev' : 'prod'
 
 export const portFront = 3000
-// export const portFront = 3000 : 3001
 export const hostFront = 'localhost'
-// export const hostFront = isDev ? 'localhost' : 'localhost'
 
 export const portBack = 4000
-// export const portBack = isDev ? 4000 : 4001
 export const hostBack = 'http://localhost'
 
 export const domain = isProd
