179e496a84528265a385357c1465caa5a763efe9
Anton Arbus <anton.arbus@macbookpro>
2024-07-30 23:31:13 +0300

    smth

--- front/features/open_close/open_bookmarks_page/openBookmarksPage.ts
+++ front/features/open_close/open_bookmarks_page/openBookmarksPage.ts
@@ -1,8 +1,8 @@
-import { router } from '@lib_instances/router'
 import { dispatch, getState } from '@lib_instances/store'
 import { previousQuotationRef } from '@entities/quotation'
 import { route } from '@shared/consts/route'
 import { navSlice } from '@shared/nav'
+import { router } from '@lib_instances/router'
 
 export const openBookmarksPage = (): void => {
   // save current quotation into previousQuotationRef to be able to return to it with back button
