3b0c98ac6505ff5cd0f06fb8971c261f6ff179c4
Anton Arbus <anton.arbus@macbookpro>
2024-08-03 19:59:16 +0300

    render bookmark info only in bookmarks

--- front/pages/bookmark/bookmark_modal/BookmarkModal.tsx
+++ front/pages/bookmark/bookmark_modal/BookmarkModal.tsx
@@ -12,8 +12,11 @@ import {
   useLoadInitValuesIntoBookmarkModal,
   useLoadBookmarkModalOpenedWithDirectLink,
 } from '@features/open_close/open_bookmark_modal'
+import { useLocation } from 'react-router-dom'
+import { route } from '@shared/consts/route'
 
 export const BookmarkModal = (): JSX.Element => {
+  const { pathname } = useLocation()
   const modalRef = useRef<HTMLDivElement>(null)
 
   const nameSignal = useSignal('')
@@ -47,8 +50,8 @@ export const BookmarkModal = (): JSX.Element => {
     <FormModal
       width='500px'
       headerIcon={<FiEdit3 />}
-      headerText='Edit bookmark'
-      buttonText='UPDATE'
+      headerText='Save bookmark'
+      buttonText='SAVE'
       isButtonDisabled={nameSignal.value === '' || categorySignal.value === ''}
       isButtonLoading={isPending}
       isButtonSuccess={isSuccess}
@@ -61,7 +64,7 @@ export const BookmarkModal = (): JSX.Element => {
       <CategoryField categorySignal={categorySignal} />
       <DescriptionField descSignal={descSignal} />
       <InfoField infoSignal={infoSignal} />
-      <BookmarkField />
+      {pathname.includes(route.bookmarks) && <BookmarkField />}
     </FormModal>
   )
 }
