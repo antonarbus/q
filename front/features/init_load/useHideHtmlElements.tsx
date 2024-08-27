052019ee84712a943a08a100148f13940a112916
Anton Arbus <anton.arbus@macbookpro>
2024-08-27 20:31:19 +0300

    loading instead of wait

--- front/features/init_load/useHideHtmlElements.tsx
+++ front/features/init_load/useHideHtmlElements.tsx
@@ -3,7 +3,7 @@ import { useEffectOnce } from 'react-use'
 export const useHideHtmlElements = (): void => {
   useEffectOnce(() => {
     const seoElement = document.getElementById('seo')
-    const waitElement = document.getElementById('wait')
+    const waitElement = document.getElementById('loading')
 
     if (seoElement instanceof Element) {
       seoElement.style.display = 'none'
