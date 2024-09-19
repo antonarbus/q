db9b0b63f41a9b54738869f22b9a24123aaa0ed6
Anton Arbus <anton.arbus@macbookpro>
2024-09-19 08:42:38 +0300

    FooterLayout

--- front/widgets/footer/Footer.tsx
+++ front/widgets/footer/Footer.tsx
@@ -1,25 +1,10 @@
 import { EmailIcon } from './EmailIcon'
+import { FooterLayout } from './FooterLayout'
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer
-      style={{
-        position: 'fixed',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '24px',
-        bottom: '0',
-        left: '0',
-        right: '0',
-        padding: '2px',
-        backgroundColor: 'rgba(250, 250, 250, 0.9)',
-        borderTop: '1px solid #e0e0e0',
-        zIndex: '666',
-      }}
-    >
+    <FooterLayout>
       <EmailIcon />
-    </footer>
+    </FooterLayout>
   )
 }
---
+++ front/widgets/footer/FooterLayout.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  children: React.ReactNode
+}
+
+export const FooterLayout = (props: Props): JSX.Element => {
+  return (
+    <footer
+      style={{
+        position: 'fixed',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '24px',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        padding: '2px',
+        backgroundColor: 'rgba(250, 250, 250, 0.9)',
+        borderTop: '1px solid #e0e0e0',
+        zIndex: '666',
+      }}
+    >
+      {props.children}
+    </footer>
+  )
+}
