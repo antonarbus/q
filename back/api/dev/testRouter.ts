ef8e10603d30014c579a851bb4a45b416b4b3396
Anton Arbus <anton.arbus@macbookpro>
2024-09-24 12:37:40 +0300

    serverless cluster

--- back/api/dev/testRouter.ts
+++ back/api/dev/testRouter.ts
@@ -7,9 +7,10 @@ export const testRouter = Router()
 
 async function test(req: Req, res: Res, next: Next): Promise<void> {
   try {
+    const dbRes = await UserModel.find({ email: 'some random guy' })
     // const dbRes = await UserModel.find({ email: 'anton.arbus@gmail.com' })
-    await UserModel.deleteOne({ email: 'info@sendmequotation.today' })
-    const dbRes = await UserModel.find()
+    // await UserModel.deleteOne({ email: 'info@sendmequotation.today' })
+    // const dbRes = await UserModel.find()
     // const dbRes = await ItemModel.find().distinct('category', { email: 'anton.arbus@gmail.com' })
     // const dbRes = await QuotationModel.find()
 
