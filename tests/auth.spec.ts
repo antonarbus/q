d76e1ab073f2c6ef2eb9223cb80f28e99637cf02
Anton Arbus <anton.arbus@macbookpro>
2024-09-26 00:18:43 +0300

    log in & out playwright

---
+++ tests/auth.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+
+const url = 'https://localhost:3000'
+
+test('log in & out', async ({ page }) => {
+  const email = 'anton.arbus@gmail.com'
+  const password = 'xxx'
+  const sixtySec = 60000
+
+  test.setTimeout(sixtySec)
+
+  await page.goto(url)
+
+  await page.getByRole('link', { name: 'Log in' }).click()
+  await page.getByPlaceholder('Email').fill(email)
+  await page.getByPlaceholder('Password').fill(password)
+  await page.getByRole('button', { name: 'LOG IN' }).click()
+
+  const nav = page.locator('nav')
+  await expect(nav).toHaveText(/Profile/u)
+
+  await page.getByRole('link', { name: 'Profile' }).click()
+  await page.getByRole('link', { name: 'Log out' }).nth(0).click()
+
+  await expect(nav).toHaveText(/Log in/u)
+})
--- tests/general.spec.ts
+++ tests/general.spec.ts
@@ -43,23 +43,3 @@ test('has main elements & texts on index page', async ({ page }) => {
   await expect(body).toHaveText(/Quotations/u)
   await expect(body).toHaveText(/Log in/u)
 })
-
-test.only('log in & out', async ({ page }) => {
-  const email = 'anton.arbus@gmail.com'
-  const password = 'xxx'
-
-  await page.goto(url)
-
-  await page.getByRole('link', { name: 'Log in' }).click()
-  await page.getByPlaceholder('Email').fill(email)
-  await page.getByPlaceholder('Password').fill(password)
-  await page.getByRole('button', { name: 'LOG IN' }).click()
-
-  const nav = page.locator('nav')
-  await expect(nav).toHaveText(/Profile/u)
-
-  await page.getByRole('link', { name: 'Profile' }).click()
-  await page.getByRole('link', { name: 'Log out' }).nth(0).click()
-
-  await expect(nav).toHaveText(/Log in/u)
-})
