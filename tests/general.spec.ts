5d60a4337777de2568d14733059df57b70a64b7e
Anton Arbus <anton.arbus@macbookpro>
2024-09-25 19:13:36 +0300

    playwright_record

--- tests/general.spec.ts
+++ tests/general.spec.ts
@@ -1,16 +1,44 @@
 import { test, expect } from '@playwright/test'
 
-test('check if webpage is accessible', async ({ page }) => {
-  const url = 'https://localhost:3000'
+const url = 'https://localhost:3000'
 
+test('page is accessible', async ({ page }) => {
   const response = await page.goto(url)
   expect(response?.status()).toBe(200)
+})
 
-  const title = await page.title()
-  expect(title).toBe(
-    'SendMeQuotation.today - Build and Manage Your Commercial Quotations',
-  )
+test('has title', async ({ page }) => {
+  await page.goto(url)
+  await expect(page).toHaveTitle(/SendMeQuotation.today/u)
+})
+
+test('has main elements & texts on index page', async ({ page }) => {
+  await page.goto(url)
 
   const mainElement = await page.$('main')
   expect(mainElement).not.toBeNull()
+
+  const navElement = await page.$('nav')
+  expect(navElement).not.toBeNull()
+
+  const footerElement = await page.$('footer')
+  expect(footerElement).not.toBeNull()
+
+  const body = page.locator('body')
+  await expect(body).toHaveText(/Cover letter/u)
+  await expect(body).toHaveText(/Title/u)
+  await expect(body).toHaveText(/Subtotal/u)
+  await expect(body).toHaveText(/Description/u)
+  await expect(body).toHaveText(/Item price/u)
+  await expect(body).toHaveText(/Qty/u)
+  await expect(body).toHaveText(/Price/u)
+  await expect(body).toHaveText(/Total price/u)
+  await expect(body).toHaveText(/Terms & Conditions/u)
+  await expect(body).toHaveText(/New/u)
+  await expect(body).toHaveText(/Save/u)
+  await expect(body).toHaveText(/Pdf/u)
+  await expect(body).toHaveText(/Insert/u)
+  await expect(body).toHaveText(/Bookmarks/u)
+  await expect(body).toHaveText(/Quotations/u)
+  await expect(body).toHaveText(/Log in/u)
 })
