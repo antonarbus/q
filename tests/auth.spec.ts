f626ed244dbdfa2a5f7411642272b7081c4d5ccb
Anton Arbus <anton.arbus@macbookpro>
2024-09-26 22:31:26 +0300

    bit better log in & out test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -4,22 +4,20 @@ import { route } from '@shared/consts/route'
 test('log in & out', async ({ page }) => {
   const email = 'anton.arbus@gmail.com'
   const password = 'xxx'
-  const sixtySec = 60000
-
-  test.setTimeout(sixtySec)
 
   await page.goto(route.root)
-
   await page.getByRole('link', { name: 'Log in' }).click()
+  const button = page.locator('button').filter({ hasText: /LOG IN/u })
+  await expect(button).toBeDisabled()
+  await page.getByPlaceholder('Email').focus()
   await page.getByPlaceholder('Email').fill(email)
+  await page.getByPlaceholder('Password').focus()
   await page.getByPlaceholder('Password').fill(password)
-  await page.getByRole('button', { name: 'LOG IN' }).click()
-
+  await expect(button).not.toBeDisabled()
+  await button.click()
   const nav = page.locator('nav')
   await expect(nav).toHaveText(/Profile/u)
-
   await page.getByRole('link', { name: 'Profile' }).click()
   await page.getByRole('link', { name: 'Log out' }).nth(0).click()
-
   await expect(nav).toHaveText(/Log in/u)
 })
