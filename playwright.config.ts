97a8b3a4852986d6b3cc32813e3536955836c3d6
Anton Arbus <anton.arbus@macbookpro>
2024-09-26 22:01:50 +0300

    no manual start of dev server for tests on ci cd pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from '@playwright/test'
 
-// See https://playwright.dev/docs/test-configuration
+// https://playwright.dev/docs/test-configuration
 
 export default defineConfig({
   testDir: './tests',
