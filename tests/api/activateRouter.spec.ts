67be5494f77c76ddd5887be6e049b63c6da4e24d
Anton Arbus <anton.arbus@macbookpro>
2024-10-06 01:27:22 +0300

    activateRouter api test

--- tests/api/activateRouter.spec.ts
+++ tests/api/activateRouter.spec.ts
@@ -1,14 +1,14 @@
 import { apiUrl } from '@back/consts/apiUrl'
 import { baseUrlBack } from '@back/utils/env'
-import { test, expect, type Locator } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.describe('activateRouter', () => {
-  test.only('should not return successful status if key is missing', async ({
+  test('should not return successful status if key is missing', async ({
     request,
   }) => {
     const res = await request.post(`${baseUrlBack}${apiUrl.activate}`, {
       data: {
-        activationKey: 'some non existing activation key',
+        activationKey: 'bad activation key',
       },
     })
 
