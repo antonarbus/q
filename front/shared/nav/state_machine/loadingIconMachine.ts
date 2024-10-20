b14fd722036a2cecb088fd544bba61ba9d5c9946
Anton Arbus <anton.arbus@macbookpro>
2024-10-20 18:57:28 +0300

    not type issues at state machine

--- front/shared/nav/state_machine/loadingIconMachine.ts
+++ front/shared/nav/state_machine/loadingIconMachine.ts
@@ -1,24 +1,68 @@
-import { createMachine } from 'xstate'
+import { setup } from 'xstate'
 
-export const loadingIconMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBsD2BDCBLAdlAkgMao4B0A7ulgC65QDEsAFquQARqZ1tbE4DaABgC6iUAAdUsGlhJiQAD0QAWABwBOUgHYATMoBsOgMyr9Jo8oCsAGhABPRAEZVO0jvXKtR-Y-eD9-joAvkG2nNh4RCSk4XSMLOxgAE5JqEk8fEKiSCCS0rRyOUoI3q6qlqrO+lpaglqWRka2DiWqpOqqqkaCyo4BlpYe+iFhGBEEfDFjccysbLAAroSEcBkkWfJ5MoWgxYaWpEYDRzqD3YZN9ogm7Z3dvf2DViMgsZGTyalJ9Aqw1OjUMCkdAAM0BSQAFDpBDCAJT0N4TaKfNIbHJbAo4eTFUqkVQw-SqLQ+dSOdTqHRaZqIdyaRyCdx6HT6fSDDovRFRMiLZZwWA-P4AoGg8FQmGCeGcyY8lawWBoiRSbZYoqIfTqIykBqkqwGdQs7zUkr+LXqQTmZR6AxHEKhEA4VAQODyKU7XJKzHYmnudoBNTQ5QGQQDI0AWjUpBZLOUwZc1S8wztrrIlBkeE2HtkKt2iAaB30lvUWl6pME+scRvcWjctSZZg6Vi0HOm72iiIz+SzXoQlh9+qJZlUylJWl8RpurMMulJFW8+ubXFbZBRSQ7yu7lOr+p6Lh6QZDVwQzkcvspO6HjUqC-GXNIMr5a89qoQ9TaGk8o7JPj6RuPp9qagXiYji2kEQA */
-  id: 'loadingIcon',
+/** @xstate-layout N4IgpgJg5mDOIC5QBsD2BDCBLAdlABFgMao4B0A7ulgC65QDEsN6ATjfmpvQNoAMAXUSgADqli0spYSAAeiAKwAWAJxkVAZg0AOBQEYNqvSoBM2gDQgAnoj3aTZEyqUB2JcqV8VANhNuAvv6WXNh4hCTkIfQMMBxgrKyorPxCSCBiEnTSafIIGr5k9nx6JnZ22oYu3pY2CHreZAoKKnza2i4mfFqaLgqBwRihBMSkZFF4MWAcsACuRERwsCkyGZLZoLkuenrqLXoK+Qbe3u01tg1NLW0dXRo9fUEg48MRZPGJrAyyzOg0YGToABmf1YAApOnw+ABKBjPcKjd5JZZpVZZHAyXJONQtJoHJQGLQuCzWRB+BraPhE-EdBTHbwPAbcMIjcizeaLL4-P4A4HxcGQ6Gwwb0eGsuYLWBLQQrcRrdE5RDafGNZQuFSmWl6XpKM4IEzeNQaUqubYqPSqFSBR44VAQOAyOEsmWZKTyjak-TqPi4wwEjREjS6gC0Lj4ZCqOhMTXcTj4rn6T2FzNeVEkeGdcoxik6ZH9xwUcZUWwpel1ZhcZD0cfqBdrReUCcdr2eGbRWYQClD4YpJhMd229lOJLy2nU+mOLjV+xKGj0jaTLwRCSSrdd7dnGnUMcn3js7m01WHZXUvlDSjaSi02nN86Zi7F7Mlq-WckVl9zRpKX+arh1R+vJ5+HGF5XjeVpAA */
+export const loadingIconMachine = setup({
+  types: {
+    events: {} as
+      | { type: 'start loading' }
+      | { type: 'get error' }
+      | { type: 'get success' },
+  },
+  actions: {
+    'disable click': () => {
+      console.info('disable click')
+    },
+    'show error icon': () => {
+      console.info('show error icon')
+    },
+    'show success icon': () => {
+      console.info('show success icon')
+    },
+    'show spinner': () => {
+      console.info('show spinner')
+    },
+    'show initial icon': () => {
+      console.info('show initial icon')
+    },
+  },
+}).createMachine({
+  id: 'loading icon',
   initial: 'waiting',
   states: {
     waiting: {
       on: {
-        'show loading icon': {
+        'start loading': {
           target: 'loading',
         },
       },
+      entry: [
+        {
+          type: 'show initial icon',
+        },
+      ],
+      exit: [
+        {
+          type: 'show spinner',
+        },
+      ],
     },
     loading: {
       on: {
-        'show error icon': {
+        'get error': {
           target: 'error',
+          actions: [
+            {
+              type: 'show error icon',
+            },
+          ],
         },
-        'show succes icon': {
+        'get success': {
           target: 'success',
+          actions: [
+            {
+              type: 'show success icon',
+            },
+          ],
         },
       },
     },
@@ -28,6 +72,11 @@ export const loadingIconMachine = createMachine({
           target: 'waiting',
         },
       },
+      // exit: [
+      //   {
+      //     type: 'show initial icon',
+      //   },
+      // ],
     },
     success: {
       after: {
