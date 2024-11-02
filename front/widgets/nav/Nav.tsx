eda523d246d92aab68078ee827df20cd45400d79
Anton Arbus <anton.arbus@macbookpro>
2024-11-02 21:12:14 +0200

    NavLayout

--- front/widgets/nav/Nav.tsx
+++ front/widgets/nav/Nav.tsx
@@ -1,16 +1,14 @@
-import { dispatch, useSelectorTyped } from '@lib_instances/store'
-import { theme } from '@lib_instances/theme'
+import { dispatch } from '@lib_instances/store'
 import { useRef } from 'react'
 import { useEffectOnce } from 'react-use'
 import { navSlice, useMediaQueryValues, Logo, NavList } from '@shared/nav'
 import { navStructure } from './navStructure'
 import { usePressNavShortcut } from '@features/nav/press_shortcut'
+import { NavLayout } from './NavLayout'
 
 export const Nav = (): React.JSX.Element => {
   const navRef = useRef<HTMLDivElement>(null)
   const logoRef = useRef<HTMLDivElement>(null)
-  const mediaQueryWidth = useSelectorTyped((state) => state.nav.mediaQueryWidth)
-  const mediaEnabled = useSelectorTyped((state) => state.nav.mediaEnabled)
   usePressNavShortcut({ navStructure })
   useMediaQueryValues({ navRef, logoRef })
   useEffectOnce(() => {
@@ -18,55 +16,9 @@ export const Nav = (): React.JSX.Element => {
   })
 
   return (
-    <nav
-      ref={navRef}
-      css={{
-        display: 'flex',
-        flexWrap: 'nowrap',
-        justifyContent: 'space-between',
-        alignItems: 'stretch',
-        position: 'sticky',
-        top: '5px',
-        marginBottom: `${String(theme.nav.marginBottom)}px`,
-        marginLeft: '10px',
-        marginRight: '10px',
-        height: `${String(theme.nav.height)}px`,
-        borderRadius: '4px',
-        background: theme.colors.darkBackground,
-        boxShadow: '0 0px 5px 0 #0000005c',
-        zIndex: 6,
-        contain: 'layout inline-size',
-        fontWeight: 300,
-        '& > ul > li > a .icon-round-wrapper': mediaEnabled && {
-          [`@media (max-width: ${String(mediaQueryWidth.icon)}px) and (min-width: ${String(mediaQueryWidth.name)}px)`]:
-            {
-              display: 'none',
-            },
-          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]: {
-            display: 'none',
-          },
-        },
-        '& .nav-item-name': mediaEnabled && {
-          [`@media (max-width: ${String(mediaQueryWidth.name)}px)`]: {
-            display: 'none',
-          },
-        },
-        '& li:not(:last-child)': mediaEnabled && {
-          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]: {
-            display: 'none',
-          },
-        },
-        '& li:last-child': {
-          display: 'none',
-          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]:
-            mediaEnabled && {
-              display: 'flex',
-            },
-        },
-      }}
-    >
+    <NavLayout navRef={navRef}>
       <Logo logoRef={logoRef} />
       <NavList />
-    </nav>
+    </NavLayout>
   )
 }
---
+++ front/widgets/nav/NavLayout.tsx
@@ -0,0 +1,65 @@
+import { useSelectorTyped } from '@lib_instances/store'
+import { theme } from '@lib_instances/theme'
+import type { RefObject } from 'react'
+
+type Props = {
+  children: React.ReactNode
+  navRef: RefObject<HTMLDivElement>
+}
+
+export const NavLayout = (props: Props): React.JSX.Element => {
+  const mediaQueryWidth = useSelectorTyped((state) => state.nav.mediaQueryWidth)
+  const mediaEnabled = useSelectorTyped((state) => state.nav.mediaEnabled)
+
+  return (
+    <nav
+      ref={props.navRef}
+      css={{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+        position: 'sticky',
+        top: '5px',
+        marginBottom: `${String(theme.nav.marginBottom)}px`,
+        marginLeft: '10px',
+        marginRight: '10px',
+        height: `${String(theme.nav.height)}px`,
+        borderRadius: '4px',
+        background: theme.colors.darkBackground,
+        boxShadow: '0 0px 5px 0 #0000005c',
+        zIndex: 6,
+        contain: 'layout inline-size',
+        fontWeight: 300,
+        '& > ul > li > a .icon-round-wrapper': mediaEnabled && {
+          [`@media (max-width: ${String(mediaQueryWidth.icon)}px) and (min-width: ${String(mediaQueryWidth.name)}px)`]:
+            {
+              display: 'none',
+            },
+          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]: {
+            display: 'none',
+          },
+        },
+        '& .nav-item-name': mediaEnabled && {
+          [`@media (max-width: ${String(mediaQueryWidth.name)}px)`]: {
+            display: 'none',
+          },
+        },
+        '& li:not(:last-child)': mediaEnabled && {
+          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]: {
+            display: 'none',
+          },
+        },
+        '& li:last-child': {
+          display: 'none',
+          [`@media (max-width: ${String(mediaQueryWidth.burger)}px)`]:
+            mediaEnabled && {
+              display: 'flex',
+            },
+        },
+      }}
+    >
+      {props.children}
+    </nav>
+  )
+}
