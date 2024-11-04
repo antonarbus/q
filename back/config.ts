629d91bd49eaeae7dfbdbd51bbca744712984ff5
Anton Arbus <anton.arbus@macbookpro>
2024-11-04 09:53:50 +0200

    comments

--- back/config.ts
+++ back/config.ts
@@ -2,10 +2,18 @@ import { getEnvVar } from './utils/getEnvVar'
 
 export const config = {
   get env() {
+    // https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production#why-is-node_env-considered-an-antipattern
+    // NODE_ENV usage is anti-pattern
+    // we set NODE_ENV=development at package.json & Dockerfile.prod & deployment.yaml when launch a server
+    // we set NODE_ENV only for potential usage by other packages for their own reasons
+
     if (getEnvVar('NODE_ENV') === 'development') return 'development'
     return 'production'
   },
   get installation() {
+    // our code logic is not dependent on NODE_ENV, but on INSTALLATION env variable
+    // INSTALLATION is also set at package.json & Dockerfile.prod & deployment.yaml
+
     if (getEnvVar('INSTALLATION') === 'local') return 'local'
     return 'production'
   },
@@ -28,9 +36,9 @@ export const config = {
       return 443 // default, not specified at browser
     },
     get baseUrl() {
-      if (config.installation === 'local')
+      if (config.installation === 'local') {
         return `${this.protocol}://${this.hostname}:${this.port}` as const
-
+      }
       return `${this.protocol}://${this.hostname}` as const
     },
   },
