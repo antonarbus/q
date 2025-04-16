b4239afe5b5d36761d69b01955e17bcc529ad8dd
Anton Arbus <anton.arbus@macbookpro>
2025-04-16 17:54:35 +0300

    api description

--- back/shared/consts/api.ts
+++ back/shared/consts/api.ts
@@ -48,13 +48,17 @@ export const api = {
     url: '/',
     method: 'get',
     handler: root,
-    description: 'Root endpoint, no real purpose',
+    description: `
+      Root endpoint, no real purpose, good to show some info for dev purpose
+    `,
   },
   rootApi: {
     url: '/api',
     method: 'get',
     handler: rootApi,
-    description: 'description',
+    description: `
+      Root api endpoint, no real purpose, good to show some info for dev purpose
+    `,
   },
   health: {
     url: '/api/health',
@@ -62,175 +66,189 @@ export const api = {
     handler: health,
     description: `
       Google Cloud Run checks this endpoint to see if the app is alive. 
-      It runs ExpressJs server and checks the db connection.
+      It checks if ExpressJS and DB connection are OK.
     `,
   },
   test: {
     url: '/api/test',
     method: 'get',
     handler: test,
-    description: 'description',
+    description: 'Test playground for dev purpose',
   },
   setBucketCors: {
     url: '/api/set-bucket-cors',
     method: 'get',
     handler: setBucketCors,
-    description: 'description',
+    description: 'Set CORS for bucket & visit the endpoint to apply',
   },
   getBucketCors: {
     url: '/api/get-bucket-cors',
     method: 'get',
     handler: getBucketCors,
-    description: 'description',
+    description: 'Get CORS for bucket. Visit the endpoint to apply.',
   },
   // auth
   register: {
     url: '/api/register',
     method: 'post',
     handler: register,
-    description: 'description',
+    description: 'When user clicks on register button',
   },
   logIn: {
     url: '/api/login',
     method: 'post',
     handler: logIn,
-    description: 'description',
+    description: 'When user clicks on login button',
   },
   logOut: {
     url: '/api/logout',
     method: 'get',
     handler: logOut,
-    description: 'description',
+    description: 'When user clicks on logout button',
   },
   activate: {
     url: '/api/activate',
     method: 'post',
     handler: activate,
-    description: 'description',
+    description: 'When user clicks on activate link in email',
   },
   getAccessToken: {
     url: '/api/get-access-token',
     method: 'get',
     handler: getAccessToken,
-    description: 'description',
+    description: `
+      Calls initially on app load (<AccessToken />) to get access token
+      and puts it into memory (state.user.accessToken).
+      Then for every protected request we add this token to the request header ("access-jwt-token").
+      If access token is expired, backend responds with 401 and we
+      a) save initial request config to resend it later
+      b) call this endpoint to get new access token and save it in memory
+      c) resend initial request with new access token
+      d) if access token is still invalid, we log out user
+    `,
   },
   requestPasswordReset: {
     url: '/api/request-password-reset',
     method: 'post',
     handler: requestPasswordReset,
-    description: 'description',
+    description: `
+      When user clicks on forgot password link to get an email with reset link
+    `,
   },
   resetPassword: {
     url: '/api/reset-password',
     method: 'post',
     handler: resetPassword,
-    description: 'description',
+    description: 'When user clicks on reset password link in email',
   },
   // user
   getUsers: {
     url: '/api/get-users',
     method: 'get',
     handler: getUsers,
-    description: 'description',
+    description: 'Users list for admin page',
   },
   deleteUser: {
     url: '/api/delete-user',
     method: 'delete',
     handler: deleteUser,
-    description: 'description',
+    description: 'When admin deletes a user with button in table',
   },
   // quotation
   saveQuotation: {
     url: '/api/save-quotation',
     method: 'post',
     handler: saveQuotation,
-    description: 'description',
+    description: 'When user clicks on save button in the modal',
   },
   getQuotation: {
     url: '/api/get-quotation',
     method: 'post',
     handler: getQuotation,
-    description: 'description',
+    description: 'When user opens the quotation page',
   },
   getQuotations: {
     url: '/api/get-quotations',
     method: 'get',
     handler: getQuotations,
-    description: 'description',
+    description: 'For quotations table',
   },
   getQuotationCategories: {
     url: '/api/get-quotation-categories',
     method: 'get',
     handler: getQuotationCategories,
-    description: 'description',
+    description: 'For the category field on save quotation modal',
   },
   deleteQuotation: {
     url: '/api/delete-quotation',
     method: 'delete',
     handler: deleteQuotation,
-    description: 'description',
+    description: 'When user clicks on delete button in quotations table',
   },
   // bookmark
   saveBookmark: {
     url: '/api/save-bookmark',
     method: 'post',
     handler: saveBookmark,
-    description: 'description',
+    description: 'When user clicks on save button in the modal',
   },
   getBookmark: {
     url: '/api/get-bookmark',
     method: 'post',
     handler: getBookmark,
-    description: 'description',
+    description: 'When user clicks on bookmark in search or on bookmarks page',
   },
   getBookmarks: {
     url: '/api/get-bookmarks',
     method: 'get',
     handler: getBookmarks,
-    description: 'description',
+    description: 'For bookmarks table',
   },
   getBookmarkCategories: {
     url: '/api/get-bookmark-categories',
     method: 'get',
     handler: getBookmarkCategories,
-    description: 'description',
+    description: 'For the category field on save bookmark modal',
   },
   deleteBookmark: {
     url: '/api/delete-bookmark',
     method: 'delete',
     handler: deleteBookmark,
-    description: 'description',
+    description: 'When user clicks on delete button in bookmarks table',
   },
   // files
   fileUploadSignedUrl: {
     url: '/api/file-upload-signed-url',
     method: 'get',
     handler: fileUploadSignedUrl,
-    description: 'description',
+    description: `
+      On file upload we send light request for Google Cloud Storage url
+      where file is uploaded on client side
+    `,
   },
   makeFilePublic: {
     url: '/api/make-file-public',
     method: 'patch',
     handler: makeFilePublic,
-    description: 'description',
+    description: 'After file is uploaded we make it public',
   },
   getFilesStats: {
     url: '/api/get-files-stats',
     method: 'get',
     handler: getFilesStats,
-    description: 'description',
+    description: 'Gets total files size and count for settings modal',
   },
   // visitors
   countUniqueDailyVisitors: {
     url: '/api/count-unique-daily-visitors',
     method: 'post',
     handler: countUniqueDailyVisitors,
-    description: 'description',
+    description: 'Records unique daily visitors per day',
   },
   getUniqueDailyVisitors: {
     url: '/api/get-unique-daily-visitors',
     method: 'get',
     handler: getUniqueDailyVisitors,
-    description: 'description',
+    description: 'Gets unique daily visitors for admin page',
   },
 } as const satisfies Record<string, Api>
