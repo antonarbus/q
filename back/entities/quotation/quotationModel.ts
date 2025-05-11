a725e61be0933bb1435ee4d6569794deba773839
Anton Arbus <anton.arbus@macbookpro>
2025-05-12 00:10:31 +0300

    refactor: simplify quotationSchema definition by removing unnecessary line breaks

--- back/entities/quotation/quotationModel.ts
+++ back/entities/quotation/quotationModel.ts
@@ -1,66 +1,63 @@
 import { model, Schema } from 'mongoose'
 import type { Quotation } from '@entities/quotation'
 
-const quotationSchema = new Schema<Quotation>(
-  {
-    id: {
-      type: String,
-      // default: () => generateId(),
-      required: true,
-      unique: true,
-      index: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      index: true,
-      lowercase: true,
-      trim: true,
-    },
-    name: {
-      type: String,
-      trim: true,
-    },
-    category: {
-      type: String,
-      trim: true,
-    },
-    desc: {
-      type: String,
-      trim: true,
-    },
-    info: {
-      type: String,
-      trim: true,
-    },
-    createdAt: Date,
-    updatedAt: Date,
-    openedAt: Date,
-    viewedAt: Date,
-    access: {
-      type: {
-        level: String,
-        userList: [String],
-      },
-      default: (): Quotation['access'] => ({
-        level: 'nobody',
-        userList: [],
-      }),
-      required: true,
-    },
-    from: {
-      email: String,
-      name: String,
-      company: String,
-    },
-    to: {
-      email: String,
-      name: String,
-      company: String,
-    },
+const quotationSchema = new Schema<Quotation>({
+  id: {
+    type: String,
+    // default: () => generateId(),
+    required: true,
+    unique: true,
+    index: true,
   },
-  { _id: false }, // disables _id for this subdocument schema
-)
+  email: {
+    type: String,
+    required: true,
+    index: true,
+    lowercase: true,
+    trim: true,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: String,
+    trim: true,
+  },
+  desc: {
+    type: String,
+    trim: true,
+  },
+  info: {
+    type: String,
+    trim: true,
+  },
+  createdAt: Date,
+  updatedAt: Date,
+  openedAt: Date,
+  viewedAt: Date,
+  access: {
+    type: {
+      level: String,
+      userList: [String],
+    },
+    default: (): Quotation['access'] => ({
+      level: 'nobody',
+      userList: [],
+    }),
+    required: true,
+  },
+  from: {
+    email: String,
+    name: String,
+    company: String,
+  },
+  to: {
+    email: String,
+    name: String,
+    company: String,
+  },
+})
 
 // remove _id and __v after converting to object
 quotationSchema.set('toObject', {
