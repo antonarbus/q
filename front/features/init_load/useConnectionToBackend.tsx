e68d8b300c45099746dd4fb58fd0ca02772cb277
Anton Arbus <anton.arbus@macbookpro>
2025-05-13 07:38:49 +0300

    fix health check msg

--- front/features/init_load/useConnectionToBackend.tsx
+++ front/features/init_load/useConnectionToBackend.tsx
@@ -1,11 +1,11 @@
 import { useHealthCheck } from '@entities/dev'
-import { useEffect } from 'react'
+import { useUpdateEffect } from 'react-use'
 import { toast } from 'sonner'
 
 export const useConnectionToBackendCheck = (): void => {
   const { data, isError, isSuccess, error } = useHealthCheck()
 
-  useEffect(() => {
+  useUpdateEffect(() => {
     if (isSuccess) {
       if (data.message === 'disconnected') {
         toast.warning('Ups, looks like we have problems with database')
@@ -13,10 +13,10 @@ export const useConnectionToBackendCheck = (): void => {
     }
   }, [isSuccess])
 
-  useEffect(() => {
+  useUpdateEffect(() => {
     if (isError) {
       console.error(error)
       toast.warning('Ups, looks like we have problems')
     }
-  }, [isSuccess])
+  }, [isError])
 }
