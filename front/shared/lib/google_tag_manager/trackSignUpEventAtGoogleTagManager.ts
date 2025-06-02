3a4a92569a975a180345c7ba38624ed019f1c596
Anton Arbus <anton.arbus@macbookpro>
2025-06-02 16:41:01 +0300

    better comment about google tag manager event function

--- front/shared/lib/google_tag_manager/trackSignUpEventAtGoogleTagManager.ts
+++ front/shared/lib/google_tag_manager/trackSignUpEventAtGoogleTagManager.ts
@@ -1,7 +1,13 @@
 // https://tagmanager.google.com/#/container/accounts/6250651246/containers/196002691/workspaces/5/tags
 
+/**
+ * We have a Custom Event Trigger for "sendmequotation_today_sign_up"
+ * That trigger fires a GA4 Event Tag
+ * That GA4 Event sends event_name: "conversion_event_subscribe_paid_2"
+ * This event "conversion_event_subscribe_paid_2" is tracked as conversion at Google Ads
+ **/
+
 export const trackSignUpEventAtGoogleTagManager = (): void => {
-  // fires a Custom Event in Google Tag Manager.
   window.dataLayer.push({
     event: 'sendmequotation_today_sign_up',
   })
