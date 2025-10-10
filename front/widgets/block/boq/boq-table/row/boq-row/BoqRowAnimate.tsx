fdb0e489fc72c3563756bfbd43b8d159118e1847
Anton Arbus <anton.arbus@macbookpro>
2025-10-10 03:19:28 +0300

    fix: comment out overflow properties in BoqRowAnimate for improved animation handling

--- front/widgets/block/boq/boq-table/row/boq-row/BoqRowAnimate.tsx
+++ front/widgets/block/boq/boq-table/row/boq-row/BoqRowAnimate.tsx
@@ -9,25 +9,26 @@ type Props = {
 export const BoqRowAnimate = ({ children }: Props): JSX.Element => {
   return (
     <motion.div
+      className='boq-row-animate'
       animate={{
         height: 'auto', // height is being stored on copy/cut icon click
         opacity: 1,
         transitionEnd: {
           height: 'auto',
-          overflow: 'visible',
+          // overflow: 'visible',
         },
         y: 0,
       }}
       exit={{
         height: 0,
         opacity: 0,
-        overflow: 'hidden',
+        // overflow: 'visible',
         x: '150vw',
       }}
       initial={{
         height: 0,
         opacity: 0,
-        overflow: 'hidden',
+        // overflow: 'visible',
         y: '100vh',
       }}
       transition={{
