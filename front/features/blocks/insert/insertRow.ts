7400ae742f8b0d2653f6aa9a486cea934e659e9b
Anton Arbus <anton.arbus@macbookpro>
2025-10-21 22:49:34 +0300

    feat: Add row description and price templates; update item values in existing templates

--- front/features/blocks/insert/insertRow.ts
+++ front/features/blocks/insert/insertRow.ts
@@ -1,8 +1,9 @@
 import { copySlice } from '@entities/copy/copySlice'
 import { rowTypeKey } from '@entities/quotation/const/rowTypeKey'
-import rowItemHtml from '@entities/quotation/templates/rowItem.html?raw' // assets can be imported as strings using the ?raw suffix
+import rowDescriptionHtml from '@entities/quotation/templates/rowDescription.html?raw' // assets can be imported as strings using the ?raw suffix
 import rowItemPriceHtml from '@entities/quotation/templates/rowItemPrice.html?raw'
 import rowPreviewHtml from '@entities/quotation/templates/rowPreview.html?raw'
+import rowPriceHtml from '@entities/quotation/templates/rowPrice.html?raw'
 import rowQtyHtml from '@entities/quotation/templates/rowQty.html?raw'
 import type { Row } from '@entities/quotation/type'
 import { textSlice } from '@shared/lib/froala/textSlice'
@@ -19,7 +20,7 @@ export const insertRow = (event?: MouseEvent): void => {
     width: 570,
     preview: rowPreviewHtml,
     description: {
-      html: rowItemHtml,
+      html: rowDescriptionHtml,
       value: 0,
       pin: {
         isPinned: false,
@@ -28,7 +29,7 @@ export const insertRow = (event?: MouseEvent): void => {
     },
     itemPrice: {
       html: rowItemPriceHtml,
-      value: 10,
+      value: 111,
       pin: {
         isPinned: false,
         isShown: false,
@@ -36,15 +37,15 @@ export const insertRow = (event?: MouseEvent): void => {
     },
     qty: {
       html: rowQtyHtml,
-      value: 1,
+      value: 6,
       pin: {
         isPinned: true,
         isShown: false,
       },
     },
     price: {
-      html: rowItemPriceHtml,
-      value: 10,
+      html: rowPriceHtml,
+      value: 666,
       pin: {
         isPinned: false,
         isShown: false,
