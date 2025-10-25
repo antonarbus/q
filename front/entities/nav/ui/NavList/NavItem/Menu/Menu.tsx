0e92c9e545341567232b5a1ba808144167bd1e24
Anton Arbus <anton.arbus@macbookpro>
2025-10-25 18:08:40 +0300

    Portal jsx

--- front/entities/nav/ui/NavList/NavItem/Menu/Menu.tsx
+++ front/entities/nav/ui/NavList/NavItem/Menu/Menu.tsx
@@ -1,9 +1,16 @@
 import { navItemId } from '@entities/nav/navItemId'
-import { Box } from '@mui/material'
+import { Box, Portal } from '@mui/material'
 import { dispatch, useSelector } from '@shared/lib/redux'
 import { theme } from '@shared/theme'
 import { useKeysForMenuNavigation } from '@widgets/nav/handlers/useKeysForMenuNavigation'
-import { type ComponentRef, type JSX, useEffect, useRef, useState } from 'react'
+import {
+  type ComponentRef,
+  type JSX,
+  type ReactNode,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
 import { createPortal } from 'react-dom'
 import { navSlice } from '../../../../navSlice'
 import { EmailAtBottomOfMenu } from './EmailAtBottomOfMenu'
@@ -17,7 +24,7 @@ type Props = {
   navItemRef?: { current: HTMLElement | null }
 }
 
-export const Menu = (props?: Props): JSX.Element => {
+export const Menu = (props?: Props): ReactNode => {
   const menuContainerRef = useRef<ComponentRef<'div'> | null>(null)
   const currentMenuRef = useRef<ComponentRef<'div'> | null>(null)
   const nextMenuRef = useRef<ComponentRef<'div'> | null>(null)
@@ -69,79 +76,83 @@ export const Menu = (props?: Props): JSX.Element => {
     }
   }, [isMenuOutsideWindow, props?.navItemRef])
 
-  const menuContent = (
-    <Box
-      className='drop-down-nav-menu'
-      sx={{
-        position: 'fixed',
-        top: menuPosition ? `${menuPosition.top}px` : 'calc(100% + 5px)',
-        left:
-          isMenuOutsideWindow && menuPosition
-            ? `${menuPosition.left}px`
-            : 'auto',
-        right:
-          !isMenuOutsideWindow && menuPosition ? `${menuPosition.right}px` : 0,
-        width: `${theme.menu.width}px`,
-        paddingTop: `${theme.menu.paddingTop}px`,
-        paddingBottom: `${theme.menu.paddingBottom}px`,
-        overflow: 'hidden',
-
-        // liquid glass
-        background: 'rgba(0, 0, 0, 0.75)',
-        backdropFilter: 'blur(4px) saturate(180%)',
-        border: '1px solid rgba(255, 255, 255, 0.1)',
-        borderRadius: '8px',
-        boxShadow:
-          '0 8px 32px 0 rgba(0, 0, 0, 0.37), inset 0 1px 1px 0 rgba(255, 255, 255, 0.05)',
-
-        '@media screen and (width <= 480px) ': {
-          left: '0px',
-          right: '0px',
-          width: 'auto',
-        },
-        '.slidable': {
-          position: 'absolute',
-          right: '0px',
-          left: '0px',
-          height: 'auto',
-        },
-        '.next': {
-          transform: 'translateX(100%)',
-        },
-        '.measurable-div': {
-          transform: 'translateX(9999px)',
-        },
-      }}
-      onMouseLeave={(): void => {
-        dispatch(
-          navSlice.actions.setMenuItemHoverIndex({ menuItemHoverIndex: -1 }),
-        )
-      }}
-      ref={menuContainerRef}
-    >
-      <TopMenuItemsContainer />
-      <SlidableMenuItemsContainer
-        className='slidable current'
-        menuNavItemId={currentMenuNavItemId}
-        reference={currentMenuRef}
-      />
-      <SlidableMenuItemsContainer
-        className='slidable next'
-        menuNavItemId={nextMenuNavItemId}
-        reference={nextMenuRef}
-      />
-      <SlidableMenuItemsContainer
-        className='measurable-div'
-        menuNavItemId={nextMenuNavItemId}
-        reference={fakeMenuRef}
-      />
-      {isProfileMenu === true ? <EmailAtBottomOfMenu /> : null}
-    </Box>
-  )
-
   if (!menuPosition) {
-    return menuContent // Fallback before position is calculated
+    return null
   }
 
-  return createPortal(menuContent, document.body)
+  // return createPortal(menuContent, document.body)
+
+  return (
+    <Portal>
+      <Box
+        className='drop-down-nav-menu'
+        sx={{
+          position: 'fixed',
+          top: menuPosition ? `${menuPosition.top}px` : 'calc(100% + 5px)',
+          left:
+            isMenuOutsideWindow && menuPosition
+              ? `${menuPosition.left}px`
+              : 'auto',
+          right:
+            !isMenuOutsideWindow && menuPosition
+              ? `${menuPosition.right}px`
+              : 0,
+          width: `${theme.menu.width}px`,
+          paddingTop: `${theme.menu.paddingTop}px`,
+          paddingBottom: `${theme.menu.paddingBottom}px`,
+          overflow: 'hidden',
+
+          // liquid glass
+          background: 'rgba(0, 0, 0, 0.75)',
+          backdropFilter: 'blur(4px) saturate(180%)',
+          border: '1px solid rgba(255, 255, 255, 0.1)',
+          borderRadius: '8px',
+          boxShadow:
+            '0 8px 32px 0 rgba(0, 0, 0, 0.37), inset 0 1px 1px 0 rgba(255, 255, 255, 0.05)',
+
+          '@media screen and (width <= 480px) ': {
+            left: '0px',
+            right: '0px',
+            width: 'auto',
+          },
+          '.slidable': {
+            position: 'absolute',
+            right: '0px',
+            left: '0px',
+            height: 'auto',
+          },
+          '.next': {
+            transform: 'translateX(100%)',
+          },
+          '.measurable-div': {
+            transform: 'translateX(9999px)',
+          },
+        }}
+        onMouseLeave={(): void => {
+          dispatch(
+            navSlice.actions.setMenuItemHoverIndex({ menuItemHoverIndex: -1 }),
+          )
+        }}
+        ref={menuContainerRef}
+      >
+        <TopMenuItemsContainer />
+        <SlidableMenuItemsContainer
+          className='slidable current'
+          menuNavItemId={currentMenuNavItemId}
+          reference={currentMenuRef}
+        />
+        <SlidableMenuItemsContainer
+          className='slidable next'
+          menuNavItemId={nextMenuNavItemId}
+          reference={nextMenuRef}
+        />
+        <SlidableMenuItemsContainer
+          className='measurable-div'
+          menuNavItemId={nextMenuNavItemId}
+          reference={fakeMenuRef}
+        />
+        {isProfileMenu === true ? <EmailAtBottomOfMenu /> : null}
+      </Box>
+    </Portal>
+  )
 }
