96584f7c866fb4b212b6a5f7c4fe1e36c492c583
Anton Arbus <anton.arbus@macbookpro>
2025-10-25 18:36:30 +0300

    refactor: Adjust styles in PaperComponent for improved visual consistency

--- front/widgets/quotation/search/PaperComponent.tsx
+++ front/widgets/quotation/search/PaperComponent.tsx
@@ -13,12 +13,19 @@ export const PaperComponent = (
       sx={{
         width: '300px',
         translate: '0px 10px',
-        borderRadius: '8px',
         padding: '30px 6px 8px 6px',
         '.MuiAutocomplete-listbox': {
           maxHeight: '315px',
           pt: '0px !important',
         },
+
+        // liquid glass
+        background: 'rgba(255, 255, 255, 0.7)',
+        backdropFilter: 'blur(4px) saturate(180%)',
+        border: '1px solid rgba(255, 255, 255, 0.1)',
+        borderRadius: '8px',
+        boxShadow:
+          '0 8px 32px 0 rgba(0, 0, 0, 0.37), inset 0 1px 1px 0 rgba(255, 255, 255, 0.05)',
       }}
       {...props}
     >
