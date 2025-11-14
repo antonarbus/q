df65e52791218e20eb9b7ffbe082844fdb91ff67
Anton Arbus <anton.arbus@macbookpro>
2025-11-15 01:42:26 +0200

    refactor: Enhance MenuNavigationProvider with improved state management and menu animation integration

--- front/entities/nav/provider/MenuNavigationProvider.tsx
+++ front/entities/nav/provider/MenuNavigationProvider.tsx
@@ -1,18 +1,31 @@
-import type { NavItemId } from '@entities/nav/navItemId'
+import { navItemId, type NavItemId } from '@entities/nav/navItemId'
+import { useMenuAnimation } from '@entities/nav/ui/NavList/NavItem/Menu/functions/useMenuAnimation'
+import { useSelector } from '@shared/lib/redux'
 import {
+  type ComponentRef,
   type Context,
   createContext,
   type JSX,
   type ReactNode,
+  type RefObject,
   useContext,
+  useRef,
 } from 'react'
 
 type MenuNavigation = {
   goUp: () => Promise<void>
   goDown: (args: { navItemId: NavItemId }) => Promise<void>
+  menuContainerRef: RefObject<ComponentRef<'div'> | null>
+  currentMenuRef: RefObject<ComponentRef<'div'> | null>
+  nextMenuRef: RefObject<ComponentRef<'div'> | null>
+  fakeMenuRef: RefObject<ComponentRef<'div'> | null>
+  currentMenuNavItemId: NavItemId | null
+  nextMenuNavItemId: NavItemId | null
+  idsToCurrentMenuItems: NavItemId[]
+  isProfileMenu: boolean
 }
 
-type Props = MenuNavigation & {
+type Props = {
   children: ReactNode
 }
 
@@ -20,9 +33,39 @@ const MenuNavigationContext: Context<MenuNavigation | null> =
   createContext<MenuNavigation | null>(null)
 
 export const MenuNavigationProvider = (props: Props): JSX.Element => {
+  const menuContainerRef = useRef<ComponentRef<'div'> | null>(null)
+  const currentMenuRef = useRef<ComponentRef<'div'> | null>(null)
+  const nextMenuRef = useRef<ComponentRef<'div'> | null>(null)
+  const fakeMenuRef = useRef<ComponentRef<'div'> | null>(null)
+
+  const currentMenuNavItemId = useSelector(
+    (state) => state.nav.currentMenuNavItemId,
+  )
+
+  const nextMenuNavItemId = useSelector((state) => state.nav.nextMenuNavItemId)
+
+  const idsToCurrentMenuItems = useSelector(
+    (state) => state.nav.idsToCurrentMenuItems,
+  )
+
+  const menuAnimation = useMenuAnimation({
+    currentMenuRef,
+    nextMenuRef,
+    menuContainerRef,
+    fakeMenuRef,
+  })
+
   const navigationContextData: MenuNavigation = {
-    goUp: props.goUp,
-    goDown: props.goDown,
+    goUp: menuAnimation.goUp,
+    goDown: menuAnimation.goDown,
+    menuContainerRef,
+    currentMenuRef,
+    nextMenuRef,
+    fakeMenuRef,
+    currentMenuNavItemId,
+    nextMenuNavItemId,
+    idsToCurrentMenuItems,
+    isProfileMenu: idsToCurrentMenuItems.includes(navItemId.profile),
   }
 
   return (
--- front/entities/nav/ui/NavList/NavItem/Menu/Menu.tsx
+++ front/entities/nav/ui/NavList/NavItem/Menu/Menu.tsx
@@ -1,13 +1,14 @@
-import { navItemId, type NavItemId } from '@entities/nav/navItemId'
-import { MenuNavigationProvider } from '@entities/nav/provider/MenuNavigationProvider'
+import {
+  MenuNavigationProvider,
+  useMenuNavigation,
+} from '@entities/nav/provider/MenuNavigationProvider'
 import { Box } from '@mui/material'
-import { dispatch, useSelector } from '@shared/lib/redux'
+import { dispatch } from '@shared/lib/redux'
 import { useKeysForMenuNavigation } from '@widgets/nav/handlers/useKeysForMenuNavigation'
-import { type ComponentRef, type ReactNode, useRef } from 'react'
+import type { ReactNode } from 'react'
 import { navSlice } from '../../../../navSlice'
 import { EmailAtBottomOfMenu } from './EmailAtBottomOfMenu'
 import { useCloseMenuOnClickOutside } from './functions/useCloseMenuOnClickOutside'
-import { useMenuAnimation } from './functions/useMenuAnimation'
 import { MenuLayout } from './MenuLayout'
 import { SlidableMenuItemsContainer } from './SlidableMenuItemsContainer'
 import { TopMenuItemsContainer } from './TopMenuItemsContainer'
@@ -16,29 +17,19 @@ type Props = {
   navItemRef?: { current: HTMLElement | null }
 }
 
-type MenuContentProps = {
-  currentMenuNavItemId: NavItemId | null
-  nextMenuNavItemId: NavItemId | null
-  currentMenuRef: React.RefObject<ComponentRef<'div'> | null>
-  nextMenuRef: React.RefObject<ComponentRef<'div'> | null>
-  menuContainerRef: React.RefObject<ComponentRef<'div'> | null>
-  fakeMenuRef: React.RefObject<ComponentRef<'div'> | null>
-  navItemRef?: { current: HTMLElement | null }
-  isProfileMenu: boolean
-}
-
-const MenuContent = (props: MenuContentProps): ReactNode => {
+const MenuContent = (props: Props): ReactNode => {
+  const menuNavigation = useMenuNavigation()
   useKeysForMenuNavigation()
 
   useCloseMenuOnClickOutside({
-    menuContainerRef: props.menuContainerRef,
+    menuContainerRef: menuNavigation.menuContainerRef,
     navItemRef: props.navItemRef,
   })
 
   return (
     <MenuLayout navItemRef={props.navItemRef}>
       <Box
-        ref={props.menuContainerRef}
+        ref={menuNavigation.menuContainerRef}
         className='drop-down-nav-menu'
         onMouseLeave={(): void => {
           dispatch(
@@ -51,65 +42,29 @@ const MenuContent = (props: MenuContentProps): ReactNode => {
         <TopMenuItemsContainer />
         <SlidableMenuItemsContainer
           className='slidable current'
-          menuNavItemId={props.currentMenuNavItemId}
-          reference={props.currentMenuRef}
+          menuNavItemId={menuNavigation.currentMenuNavItemId}
+          reference={menuNavigation.currentMenuRef}
         />
         <SlidableMenuItemsContainer
           className='slidable next'
-          menuNavItemId={props.nextMenuNavItemId}
-          reference={props.nextMenuRef}
+          menuNavItemId={menuNavigation.nextMenuNavItemId}
+          reference={menuNavigation.nextMenuRef}
         />
         <SlidableMenuItemsContainer
           className='measurable-div'
-          menuNavItemId={props.nextMenuNavItemId}
-          reference={props.fakeMenuRef}
+          menuNavItemId={menuNavigation.nextMenuNavItemId}
+          reference={menuNavigation.fakeMenuRef}
         />
-        {props.isProfileMenu === true ? <EmailAtBottomOfMenu /> : null}
+        {menuNavigation.isProfileMenu === true ? <EmailAtBottomOfMenu /> : null}
       </Box>
     </MenuLayout>
   )
 }
 
 export const Menu = (props?: Props): ReactNode => {
-  const menuContainerRef = useRef<ComponentRef<'div'> | null>(null)
-  const currentMenuRef = useRef<ComponentRef<'div'> | null>(null)
-  const nextMenuRef = useRef<ComponentRef<'div'> | null>(null)
-  const fakeMenuRef = useRef<ComponentRef<'div'> | null>(null)
-
-  const currentMenuNavItemId = useSelector(
-    (state) => state.nav.currentMenuNavItemId,
-  )
-
-  const nextMenuNavItemId = useSelector((state) => state.nav.nextMenuNavItemId)
-
-  const idsToCurrentMenuItems = useSelector(
-    (state) => state.nav.idsToCurrentMenuItems,
-  )
-
-  const menuNavigation = useMenuAnimation({
-    currentMenuRef,
-    nextMenuRef,
-    menuContainerRef,
-    fakeMenuRef,
-  })
-
-  const isProfileMenu = idsToCurrentMenuItems.includes(navItemId.profile)
-
   return (
-    <MenuNavigationProvider
-      goUp={menuNavigation.goUp}
-      goDown={menuNavigation.goDown}
-    >
-      <MenuContent
-        currentMenuNavItemId={currentMenuNavItemId}
-        nextMenuNavItemId={nextMenuNavItemId}
-        currentMenuRef={currentMenuRef}
-        nextMenuRef={nextMenuRef}
-        menuContainerRef={menuContainerRef}
-        fakeMenuRef={fakeMenuRef}
-        navItemRef={props?.navItemRef}
-        isProfileMenu={isProfileMenu}
-      />
+    <MenuNavigationProvider>
+      <MenuContent navItemRef={props?.navItemRef} />
     </MenuNavigationProvider>
   )
 }
