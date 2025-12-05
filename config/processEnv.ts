412744c0101d265cf1a80e57b68aedee4fdd8e2d
Anton Arbus <anton.arbus@macbookpro>
2025-12-05 10:50:15 +0200

    feat: improve comments for clarity in process environment configuration

--- config/processEnv.ts
+++ config/processEnv.ts
@@ -2,18 +2,15 @@ import { z } from 'zod'
 
 const processEnvSchema = z.object({
   // NODE_ENV=development is set at package.json scripts
-  // NODE_ENV=production is set at Dockerfile.prod.front & Dockerfile.prod.back
+  // NODE_ENV=production is set at Dockerfile.prod.front (not really useful) & Dockerfile.prod.back
   // NODE_ENV=test is set by vitest
   NODE_ENV: z.enum(['production', 'development', 'test']),
-  // CI is set by GitHub Actions (and other CI platforms)
+  // CI=true is set by GitHub Actions (and other CI platforms)
   CI: z
     .string()
     .optional()
-    .transform((val) => val === 'true'),
-  // ENVIRONMENT indicates which deployed environment we're running against
-  // Optional during build (defaults to 'unknown')
-  // Set to 'local' for local development in package.json scripts
-  // Set to 'dev' | 'test' | 'pilot' | 'prod' in Cloud Run deployments (via updateCloudRunService.ts by --set-env-vars gcloud CLI flag)
+    .transform((val) => val === 'true'), // ENVIRONMENT indicates which deployed environment we're running against// ENVIRONMENT=unknown optional during build (defaults to 'unknown')// ENVIRONMENT=local in package.json scripts for local development
+  // ENVIRONMENT=dev (test | pilot | prod) in Cloud Run deployments via updateCloudRunService.ts (by --set-env-vars gcloud CLI flag)
   ENVIRONMENT: z
     .enum(['local', 'dev', 'test', 'pilot', 'prod', 'unknown'])
     .optional()
