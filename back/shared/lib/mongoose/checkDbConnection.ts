c9779aed8993eb532420dfc8dae5b1fc129f45c9
Anton Arbus <anton.arbus@macbookpro>
2025-12-07 17:58:54 +0200

    refactor: update checkDbConnection to use mongoose.ConnectionStates for clarity

--- back/shared/lib/mongoose/checkDbConnection.ts
+++ back/shared/lib/mongoose/checkDbConnection.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 export const checkDbConnection = (): boolean => {
-  const isConnected = mongoose.connection.readyState === 1
+  const isConnected =
+    mongoose.connection.readyState === mongoose.ConnectionStates.connected
 
   return isConnected
 }
