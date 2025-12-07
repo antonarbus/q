a404f6765465a7ef57b05470b40fae77079c02f5
Anton Arbus <anton.arbus@macbookpro>
2025-12-07 20:33:32 +0200

    refactor: define EqualityFn type for improved type safety in arrayShapesEqualityFn

--- front/shared/util/arrayShapesEqualityFn.ts
+++ front/shared/util/arrayShapesEqualityFn.ts
@@ -1,12 +1,18 @@
 import { BOOKMARK_POS_AT_BLOCKS } from '@entities/quotation/const/bookmarkPosAtBlocks'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type EqualityFn = (a: any, b: any) => boolean
+
 type Obj = {
   [x: string]: unknown
   id: string
 }
 
 // re-render the list only if item is replaced or new item is added
-export const arrayShapesEqualityFn = (prev: Obj[], current: Obj[]): boolean => {
+export const arrayShapesEqualityFn: EqualityFn = (
+  prev: Obj[],
+  current: Obj[],
+): boolean => {
   // remove the bookmark block item which is placed at pos 1000
   // after slice() there going to be empty items, filter them out
   // otherwise blocks will be re-rendered with visual distortion on bookmark modal open
