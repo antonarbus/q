7dd890fcfdf4b0da58912c76bf137147c1bf4bdb
Anton Arbus <anton.arbus@macbookpro>
2025-12-07 18:27:49 +0200

    refactor: restructure Google Cloud Storage credentials for clarity and type safety

--- back/shared/lib/google-cloud-storage/storage.ts
+++ back/shared/lib/google-cloud-storage/storage.ts
@@ -1,20 +1,33 @@
+/* eslint-disable camelcase */
 import { Storage } from '@google-cloud/storage'
 import { secret } from '../../../../config/secrets'
 
 //* https://console.cloud.google.com/storage/browser/quotation-app-bucket
 //* https://cloud.google.com/nodejs/docs/reference/storage/latest
 
-const storageInstance = new Storage({
-  credentials: {
-    type: secret.GOOGLE_CLOUD_PROJECT_TYPE,
-    project_id: secret.GOOGLE_CLOUD_PROJECT_ID,
-    private_key_id: secret.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY_ID,
-    private_key: secret.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY,
-    client_email: secret.GOOGLE_CLOUD_PROJECT_CLIENT_EMAIL,
-    client_id: secret.GOOGLE_CLOUD_PROJECT_CLIENT_ID,
-    token_url: secret.GOOGLE_CLOUD_PROJECT_TOKEN_URL,
-  },
-})
+// Google Auth Library expects snake_case properties in the credentials object
+// despite TypeScript types suggesting camelCase
+type ServiceAccountCredentials = {
+  type: string
+  project_id: string
+  private_key_id: string
+  private_key: string
+  client_email: string
+  client_id: string
+  token_uri: string
+}
+
+const credentials: ServiceAccountCredentials = {
+  type: secret.GOOGLE_CLOUD_PROJECT_TYPE,
+  project_id: secret.GOOGLE_CLOUD_PROJECT_ID,
+  private_key_id: secret.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY_ID,
+  private_key: secret.GOOGLE_CLOUD_PROJECT_PRIVATE_KEY,
+  client_email: secret.GOOGLE_CLOUD_PROJECT_CLIENT_EMAIL,
+  client_id: secret.GOOGLE_CLOUD_PROJECT_CLIENT_ID,
+  token_uri: secret.GOOGLE_CLOUD_PROJECT_TOKEN_URL,
+}
+
+const storageInstance = new Storage({ credentials })
 
 export const bucket = storageInstance.bucket(secret.BUCKET_NAME)
 
