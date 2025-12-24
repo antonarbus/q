51d4a560baa303793fc188686f40dcf177cb673f
Anton Arbus <anton.arbus@macbookpro>
2025-12-24 21:39:44 +0200

    refactor: add missing newline for improved readability in user index exports

--- back/entities/user/index.ts
+++ back/entities/user/index.ts
@@ -2,6 +2,7 @@ export { getUserFromAccessTokenOrNull } from './getUserFromAccessTokenOrNull'
 export { getUserFromAccessTokenOrThrowUnauthorized } from './getUserFromAccessTokenOrThrowUnauthorized'
 export { getUserFromRefreshTokenOrJohn } from './getUserFromRefreshTokenOrJohn'
 export { getUserFromRefreshTokenOrNull } from './getUserFromRefreshTokenOrNull'
+
 export {
   usersTable,
   type SelectUser,
