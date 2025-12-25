ce1cb8e3890a03ace86c2bc1000d71df2cfc7543
Anton Arbus <anton.arbus@macbookpro>
2025-12-25 17:23:09 +0200

    refactor: simplify error message handling by removing unnecessary object structure

--- back/shared/const/errorMessageCommon.ts
+++ back/shared/const/errorMessageCommon.ts
@@ -1,8 +1,2 @@
 // todo: check if we need it
-export const errorMessageCommon = {
-  internalError: 'Internal error',
-} as const
-
-// todo: check if we need it
-export type ErrorMessageCommon =
-  (typeof errorMessageCommon)[keyof typeof errorMessageCommon]
+export type ErrorMessageCommon = 'Internal error'
