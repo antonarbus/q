6dafa184976adb039b2336e8bc2c5145a8a58701
Anton Arbus <anton.arbus@macbookpro>
2026-02-24 11:21:37 +0200

    refactor: format import statements for better readability in useExtensions.ts

--- front/shared/lib/tiptap/extension/useExtensions.ts
+++ front/shared/lib/tiptap/extension/useExtensions.ts
@@ -22,7 +22,12 @@ import Strike from '@tiptap/extension-strike'
 import Code from '@tiptap/extension-code'
 import Underline from '@tiptap/extension-underline'
 import Link from '@tiptap/extension-link'
-import { TextStyle, FontSize, FontFamily, LineHeight } from '@tiptap/extension-text-style'
+import {
+  TextStyle,
+  FontSize,
+  FontFamily,
+  LineHeight,
+} from '@tiptap/extension-text-style'
 import Color from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import TextAlign from '@tiptap/extension-text-align'
