84a6e8b03e51ba1581ff8a976eb90b5f1011fb10
Anton Arbus <anton.arbus@macbookpro>
2026-03-23 00:10:13 +0200

    feat: add Steiger configuration with feature-sliced rules and update dependencies

---
+++ steiger.config.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { defineConfig } from 'steiger'
+import fsd from '@feature-sliced/steiger-plugin'
+
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    rules: {
+      'fsd/ambiguous-slice-names': 'warn',
+      'fsd/excessive-slicing': 'warn',
+      'fsd/forbidden-imports': 'warn',
+      'fsd/inconsistent-naming': 'warn',
+      'fsd/insignificant-slice': 'warn',
+      'fsd/no-cross-imports': 'warn',
+      'fsd/no-higher-level-imports': 'warn',
+      'fsd/no-layer-public-api': 'warn',
+      'fsd/no-processes': 'warn',
+      'fsd/no-public-api-sidestep': 'warn',
+      'fsd/no-reserved-folder-names': 'warn',
+      'fsd/no-segmentless-slices': 'warn',
+      'fsd/no-segments-on-sliced-layers': 'warn',
+      'fsd/no-ui-in-app': 'warn',
+      'fsd/public-api': 'warn',
+      'fsd/repetitive-naming': 'warn',
+      'fsd/segments-by-purpose': 'warn',
+      'fsd/shared-lib-grouping': 'warn',
+      'fsd/typo-in-layer-name': 'warn',
+    },
+  },
+])
