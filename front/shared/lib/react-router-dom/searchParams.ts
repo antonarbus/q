7e6a962fc8a920b8fd590376d29ad44fed28b422
Anton Arbus <anton.arbus@macbookpro>
2026-05-03 01:54:49 +0300

    feat: add searchParams utility functions for managing URL search parameters

---
+++ front/shared/lib/react-router-dom/searchParams.ts
@@ -0,0 +1,32 @@
+import type { route } from './route'
+
+type SearchParamsInput = {
+  shouldSlide?: 'true'
+  redirect?: typeof route.save | typeof route.share | typeof route.stripeConnect
+  prefilledEmail?: string
+}
+
+type SearchParamsKeys = keyof SearchParamsInput
+
+export const getSearchParam = (key: SearchParamsKeys): string | null =>
+  new URLSearchParams(window.location.search).get(key)
+
+export const buildSearchParams = (params: SearchParamsInput): '' | `?${string}` => {
+  const searchParams = new URLSearchParams()
+
+  if (params.shouldSlide !== undefined) {
+    searchParams.set('shouldSlide', params.shouldSlide)
+  }
+
+  if (params.redirect !== undefined) {
+    searchParams.set('redirect', params.redirect)
+  }
+
+  if (params.prefilledEmail !== undefined) {
+    searchParams.set('prefilledEmail', params.prefilledEmail)
+  }
+
+  const result = searchParams.toString()
+
+  return result ? `?${result}` : ''
+}
