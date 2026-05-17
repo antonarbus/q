066111d93c26510cbc45e1082934303a3232d1dd
Anton Arbus <anton.arbus@macbookpro>
2026-05-18 00:05:39 +0300

    feat: simplify useSubscriptionCheckoutMutation by removing payload handling

--- front/entities/user/api/useSubscriptionCheckoutMutation.tsx
+++ front/entities/user/api/useSubscriptionCheckoutMutation.tsx
@@ -1,25 +1,20 @@
 import { route } from '@back/api/route'
-import type {
-  ErrorResBody,
-  ReqBody as Payload,
-  ResBody,
-} from '@back/api/stripe/subscriptionCheckoutHandler'
+import type { ErrorResBody, ResBody } from '@back/api/stripe/subscriptionCheckoutHandler'
 import { axiosHolder } from '@front/shared/lib/axios/axiosHolder'
 import { queryKey } from '@front/shared/lib/tanstack-query/queryKey'
 import { useMutation } from '@tanstack/react-query'
 import type { UseMutationResult } from '@tanstack/react-query'
 import type { AxiosError, AxiosResponse } from 'axios'
 
-type Res = UseMutationResult<ResBody, AxiosError<ErrorResBody>, Payload>
+type Res = UseMutationResult<ResBody, AxiosError<ErrorResBody>, void>
 
 export const useSubscriptionCheckoutMutation = (): Res => {
-  const mutation = useMutation<ResBody, AxiosError<ErrorResBody>, Payload>({
+  const mutation = useMutation<ResBody, AxiosError<ErrorResBody>>({
     mutationKey: [queryKey.stripeSubscriptionCheckout],
-    mutationFn: async (payload) => {
-      const response = await axiosHolder.axiosWithAuth<ResBody, AxiosResponse<ResBody>, Payload>({
+    mutationFn: async () => {
+      const response = await axiosHolder.axiosWithAuth<ResBody, AxiosResponse<ResBody>>({
         url: route.stripeSubscriptionCheckout.url,
         method: route.stripeSubscriptionCheckout.method,
-        data: payload,
       })
 
       return response.data
