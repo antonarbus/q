000fc476d88feca0e82939589a8227fa50c8f0a8
Anton Arbus <anton.arbus@macbookpro>
2026-05-25 23:53:26 +0300

    refactor: update SubscriptionPage layout for improved readability and clarity

--- front/pages/subscription-page/SubscriptionPage.tsx
+++ front/pages/subscription-page/SubscriptionPage.tsx
@@ -1,7 +1,7 @@
 import { SubscribeButtons } from '@front/features/user/subscribe/SubscribeButtons'
 import { FormModal } from '@front/shared/component/FormModal'
 import { routerHolder } from '@front/shared/lib/react-router-dom/routerHolder'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { MdLockOutline } from 'react-icons/md'
 import { useRef } from 'react'
 
@@ -11,7 +11,7 @@ export const SubscriptionPage = (): React.JSX.Element => {
   return (
     <FormModal
       headerIcon={<MdLockOutline />}
-      headerText='Quota limit reached'
+      headerText=''
       modalRef={modalRef}
       onCloseClick={(): void => {
         routerHolder.router.navigate('..')
@@ -23,10 +23,10 @@ export const SubscriptionPage = (): React.JSX.Element => {
       shouldUnmountOnEsc={true}
       width='350px'
     >
-      <Typography sx={{ mb: 3 }}>
-        You&apos;ve used all 100 free quotations. Get unlimited access.
-      </Typography>
-      <SubscribeButtons />
+      <Typography sx={{ mb: 3, textAlign: 'center' }}>Quota limit reached</Typography>
+      <Box sx={{ textAlign: 'center' }}>
+        <SubscribeButtons />
+      </Box>
     </FormModal>
   )
 }
