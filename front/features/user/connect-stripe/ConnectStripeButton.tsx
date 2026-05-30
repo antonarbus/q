c91fa8af79ab5d01ee91ac5b654d284a2d3f524c
Anton Arbus <anton.arbus@macbookpro>
2026-05-30 22:40:46 +0300

    refactor: simplify ConnectStripeButton by removing unused queries and loading states

--- front/features/user/connect-stripe/ConnectStripeButton.tsx
+++ front/features/user/connect-stripe/ConnectStripeButton.tsx
@@ -1,30 +1,9 @@
-import { useStripeAccountStatusQuery } from '@front/entities/payment/api/useStripeAccountStatusQuery'
 import { useStripeConnectUrlQuery } from '@front/entities/payment/api/useStripeConnectUrlQuery'
-import { RotatingLoaderIcon } from '@front/shared/component/RotatingLoaderIcon'
-import { Box, Button } from '@mui/material'
+import { Button } from '@mui/material'
 import { SiStripe } from 'react-icons/si'
 
 export const ConnectStripeButton = (): React.JSX.Element => {
-  const stripeStatusQuery = useStripeAccountStatusQuery()
   const stripeConnectUrlQuery = useStripeConnectUrlQuery()
-
-  if (stripeStatusQuery.isPending === true) {
-    return <RotatingLoaderIcon style={{ height: '16px', width: '16px' }} />
-  }
-
-  if (stripeStatusQuery.data?.connected === true) {
-    return (
-      <Box
-        component='a'
-        href={`https://dashboard.stripe.com/${stripeStatusQuery.data.stripeAccountId}`}
-        target='_blank'
-        rel='noopener noreferrer'
-        sx={{ fontSize: '13px', color: 'text.secondary' }}
-      >
-        Stripe connected ({stripeStatusQuery.data.stripeAccountId})
-      </Box>
-    )
-  }
 
   return (
     <Button
